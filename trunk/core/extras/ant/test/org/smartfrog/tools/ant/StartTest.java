91f1f017b5506101aa32b6f3ce68024ba3ea8ffc
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-03-01 17:42:44 +0000

    Tests are working. All I need to do now is test deploy/undeploy

--- trunk/core/extras/ant/test/org/smartfrog/tools/ant/StartTest.java
+++ trunk/core/extras/ant/test/org/smartfrog/tools/ant/StartTest.java
@@ -20,13 +20,21 @@
 package org.smartfrog.tools.ant;
 
 /**
+ * This test tests the daemon starting.
+ * This is quite a functional test, as it runs new processes and things, which
+ * is why there is complexity in cleanup. Even so, there is non-zero
+ * risk that bad things are happening, so we test for that, too.
  * @author steve loughran
- *         created 27-Feb-2004 16:37:55
+ *         created 01-Mar-2004 16:19:21
  */
 
-public class RunTest extends TaskTestBase {
+public class StartTest extends TaskTestBase {
 
-    public RunTest(String s) {
+    /**
+     * constructor
+     * @param s
+     */
+    public StartTest(String s) {
         super(s);
     }
 
@@ -36,21 +44,58 @@ public class RunTest extends TaskTestBase {
      * @return the path (from the test files base dir) to the build file
      */
     protected String getBuildFile() {
-        return "run.xml";
+        return "start.xml" ;
     }
 
-    public void testNoParams() {
-        expectBuildExceptionContaining("testNoParams", "no parameters",
-                "No applications declared");
-    }
     /**
-     * failonerror is not deemed to affect the no apps configuration,
-     * as that is a fundamental configuration error
+     * shut down anything running
      */
-    public void testNoFailure() {
-        expectBuildExceptionContaining("testNoFailure", "no parameters",
-                "No applications declared");
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        executeTarget("teardown");
     }
+
+    /**
+     * test for bad settings
+     */
+    public void testIncompatibleSettings() {
+        expectBuildExceptionContaining("testIncompatibleSettings",
+                "spawn and timeout",
+                "have used an attribute which is not compatible with spawn");
+    }
+
+    /**
+     * test for a bad hostname raising an error
+     */
+    public void testBadHost() {
+        expectBuildExceptionContaining("testBadHost", "host parameter",
+                "failed to start the smartfrog daemon");
+        assertLogContaining("java.net.UnknownHostException: no-such-hostname");
+    }
+
+    public void testDefaults() {
+        expectBuildExceptionContaining("testDefaults", "timeout",
+                "Timeout:");
+        assertRootProcessInLog();
+    }
+
+    /**
+     * Look for this trace
+     * [sf-startdaemon] Standalone SmartFrog daemon started
+     [sf-stopdaemon] SmartFrog 3.01.003_alpha
+     [sf-stopdaemon] (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+     [sf-stopdaemon] Warning: SmartFrog security is NOT active
+     [sf-stopdaemon] SmartFrog daemon terminated
+
+
+     */
+    public void testSpawn() {
+        executeTarget("testSpawn");
+        assertLogContaining("Standalone SmartFrog daemon started");
+        assertLogContaining("SmartFrog daemon terminated");
+    }
+
+
 
     public void testEmptyApplication() {
         expectBuildExceptionContaining("testEmptyApplication", "anon app",
@@ -72,25 +117,24 @@ public class RunTest extends TaskTestBase {
                 "missing-file.sf does not exist");
     }
 
-
-    public void testBadHost() {
-        expectBuildExceptionContaining("testBadHost", "host parameter",
-                "host cannot be set on this task");
-    }
-
-    public void testRunFile(){
-        expectBuildExceptionContaining("testRunFile","deploy failure","Could not run");
+    public void testRunFile() {
+        executeTarget("testRunFile");
+        assertLogContaining("failed to deploy 'app' component");
         assertLogContaining("Reference not found, Unresolved Reference: sfClass");
+        assertLogContaining("SmartFrog [rootProcess] dead");
     }
 
     public void testResource() {
         executeTarget("testResource");
-        assertLogContaining("Successfully deployed components: [app]");
         assertLogContaining("COUNTER: hello - here is a constructed message");
         assertLogContaining("value is 99");
         assertLogContaining("goodbye");
         assertLogContaining("[[elementA, elementB], Message from outerVector, [value is , 99]]");
         assertLogContaining("1");
+        assertTerminationInLog();
     }
 
+    private void assertTerminationInLog() {
+        assertLogContaining("SmartFrog [rootProcess] dead");
+    }
 }
