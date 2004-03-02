138e6001d6985a46435b78e855bd4bb8b1769903
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-03-02 07:19:01 +0000

    sfDetachAndTerminate script for security

---
+++ core/smartfrog/bin/security/sfDetachAndTerminate.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+if defined SFHOME goto continue1
+  if exist "%cd%\sfDetachAndTerminate.bat" cd ..
+  set SFHOME=%cd%
+  cd bin
+:continue1
+
+if defined SFPRIVATE goto continue2
+  set SFPRIVATE=%SFHOME%\private
+:continue2
+
+if (%1) == () goto usage
+if (%2) == () goto usage
+
+call %SFHOME%\bin\security\setClassPath
+java -Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFPRIVATE%\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFPRIVATE%\SFSecurity.properties org.smartfrog.SFSystem -h %1 -d %2 -e
+
+GOTO end
+:usage
+echo Insufficient arguments to use sfDetachAndTerminate
+echo Usage: sfDetachAndTerminate HostName ComponentName
+:end
+endlocal
