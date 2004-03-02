ccefca81af6bde1885fcee09b4ce69ae9e3ba9a1
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-03-02 07:18:05 +0000

    sfDetachAndTerminate script for classloading

---
+++ trunk/core/smartfrog/bin/classloader/sfDetachAndTerminate.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+if defined SFHOME goto continue1
+  if exist "%cd%\sfDetachAndTerminate.bat" cd ..
+  set SFHOME=%cd%
+  cd bin
+:continue1
+if (%1) == () goto usage
+if (%2) == () goto usage
+rem call %SFHOME%\bin\setClassPath
+
+set CLASSPATH=%SFHOME%\lib\smartfrog.jar;%SFHOME%\lib\sfServices.jar;%SFHOME%\lib\sfTestCases.jar;%CLASSPATH%  
+set SERVER=localhost:8080
+rem Please edit codebase if you have any other jar file in webserver 
+set CODEBASE="http://%SERVER%/sfExamples.jar" 
+
+java -Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini -Dorg.smartfrog.codebase=%CODEBASE% org.smartfrog.SFSystem -h %1 -d %2 -e
+GOTO end
+:usage
+echo Insufficient arguments to use sfDetachAndTerminate
+echo Usage: sfDetachAndTerminate HostName ComponentName
+:end
+endlocal
