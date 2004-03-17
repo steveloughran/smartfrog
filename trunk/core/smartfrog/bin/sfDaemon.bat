cbe508cd175cd1556f7a075ef2d30f3186c4e3bb
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-03-17 17:07:13 +0000

    Updated to use new options and properties for sfDefault descriptions.

--- trunk/core/smartfrog/bin/sfDaemon.bat
+++ trunk/core/smartfrog/bin/sfDaemon.bat
@@ -9,7 +9,7 @@ if defined SFHOME goto continue1
 if NOT (%1)==() GOTO usage
 if exist "%SFHOME%\jre\bin\java.exe" set path=%SFHOME%\jre\bin
 call %SFHOME%\bin\setClassPath
-java -Dorg.smartfrog.sfcore.processcompound.sfProcessName=rootProcess -Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini -Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault1=%SFHOME%\bin\default.sf org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dorg.smartfrog.sfcore.processcompound.sfProcessName=rootProcess -Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini -Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=%SFHOME%\bin\default.sf org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO end
 :usage
 echo Insufficient argument(s) to use sfDaemon
