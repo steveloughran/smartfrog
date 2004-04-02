0a2e38dfa88002ef75fe2ac36cf9f9b8e0892607
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-04-02 06:41:18 +0000

    Re-added sfStop, saying it is obsolete and replaced by sfTerminate.

---
+++ trunk/core/smartfrog/bin/security/classloader/sfStop.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+echo ---------------------------------------------------
+echo sfStop is now obsolete and replaced by sfTerminate. 
+echo Please use sfTerminate from next time.
+echo ---------------------------------------------------
+sfTerminate %1 %2 
+
+endlocal
