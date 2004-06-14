e4aa73373ea070faa32abced25dc096e159bdfc2
aawasthi <aawasthi@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-06-14 09:55:14 +0000

    Adding SSH Execution component. It executes commands on a remote machine over SSH. Tested with Red Hat Linux ver 9.

---
+++ trunk/core/components/ssh/src/org/smartfrog/services/ssh/SSHExecImpl.java
@@ -0,0 +1,244 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.ssh;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Vector;
+import java.rmi.RemoteException;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.UserInfo;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.common.SmartFrogLifecycleException;
+import org.smartfrog.sfcore.common.SmartFrogResolutionException;
+import org.smartfrog.sfcore.common.TerminatorThread;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+import org.smartfrog.sfcore.reference.Reference;
+
+/**
+ * SmartFrog component to executes a command on a remote machine via ssh. 
+ * It is a wrapper around jsch-0.1.14
+ * @author Ashish Awasthi
+ * @see http://www.jcraft.com/jsch/
+ * 
+ */
+public class SSHExecImpl extends PrimImpl implements SSHExec{
+
+    private static final String TIMEOUT_MESSAGE = "Connection timed out";
+    private static final int SSH_PORT = 22;
+    private long timeout = 0;
+    private File outputFile = null;   
+    private boolean append = false;   
+    private String host;
+    private String userName;
+    private String password;
+    private String keyfile;
+    private int port = SSH_PORT;
+    private boolean failOnError = true;
+    private UserInfoImpl userInfo;
+    private Vector commandsList;
+    private Reference pwdProviderRef = new Reference("passwordProvider");
+    private PasswordProvider pwdProvider;
+    private boolean trustAllCerts = true ; 
+    private boolean  shouldTerminate = true;
+    private Thread waitThread = null;
+    private Session session = null;
+    /**
+     * Constructs SSHExecImpl object.
+     */
+    public SSHExecImpl() throws RemoteException{
+        super();
+    }
+    
+
+    /**----------------SmartFrog Life Cycle Methods Begin--------------------*/
+
+    /**
+     * Reads SmartFrog attributes and deploys SSHExecImpl component.
+     *
+     * @throws SmartFrogException in case of error in deploying or reading the 
+     * attributes
+     * @throws RemoteException in case of network/emi error
+     */ 
+    public synchronized void sfDeploy() throws SmartFrogException, 
+                                                            RemoteException {
+        super.sfDeploy();
+        readSFAttributes();
+        userInfo = new UserInfoImpl(trustAllCerts);
+        userInfo.setName(userName);
+        userInfo.setPassword(password);
+    }
+    /**
+     * Connects to remote host over SSH and executes commands.
+     *
+     * @throws SmartFrogException in case of error while connecting to remote 
+     * host or executing commands
+     * @throws RemoteException in case of network/emi error
+     */ 
+    public synchronized void sfStart() throws SmartFrogException, 
+                                                          RemoteException {
+                                                                      
+        super.sfStart();
+        try {
+            // open ssh session
+            session = openSession();
+            session.setTimeout((int) timeout);
+
+            // Execute commands
+            for (int i = 0 ; i <commandsList.size() ; i++ ) {
+                final ChannelExec channel = (ChannelExec) session.
+                                            openChannel("exec");
+                String cmd = (String) commandsList.get(i);
+                channel.setCommand(cmd);
+                channel.connect();
+
+                // wait for it to finish
+                waitThread = new Thread() {
+                            public void run() {
+                                while (!channel.isEOF()) {
+                                    if (waitThread == null) {
+                                        return;
+                                    }
+                                    try {
+                                        sleep(500);
+                                    } catch (Exception e) {
+                                        // ignored
+                                    }
+                                }
+                            }
+                };
+
+                waitThread.start();
+                waitThread.join(timeout);
+
+                if (waitThread.isAlive()) {
+                    // ran out of time
+                    waitThread = null;
+                    if (failOnError) {
+                        throw new SmartFrogException(TIMEOUT_MESSAGE);
+                    } else {
+                        // TODO: use logger
+                        System.out.println(TIMEOUT_MESSAGE);
+                    }
+                } else {
+                    int exitStat = channel.getExitStatus();
+                    if (exitStat != 0) {
+                        String msg = "Remote command: "+ cmd+
+                                        " failed with exit status " + exitStat;
+                        if (failOnError) {
+                            throw new SmartFrogException(msg);
+                        }
+                    }
+                }
+                waitThread = null;
+            }
+            // check if it should terminate by itself
+            if(shouldTerminate) {
+                TerminationRecord termR = new TerminationRecord("normal",
+                "SSH Session finished: ",sfCompleteName());
+                TerminatorThread terminator = new TerminatorThread(this,termR);
+                terminator.start();
+            }
+        }catch (SmartFrogException sfe){
+            throw sfe;
+        }catch (JSchException e) {
+            if (e.getMessage().indexOf("session is down") >= 0) {
+                if (getFailOnError()) {
+                    throw new SmartFrogLifecycleException(TIMEOUT_MESSAGE, e);
+                } else {
+                    System.out.println(TIMEOUT_MESSAGE);
+                }
+            } else {
+                if (getFailOnError()) {
+                    throw new SmartFrogLifecycleException(e);
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new SmartFrogLifecycleException(e);
+        }
+    }
+    
+    /**
+     * Life cycle method for terminating the SmartFrog component.
+     *@param tr Termination record
+     *
+     */ 
+    public synchronized void sfTerminateWith(TerminationRecord tr) {
+        super.sfTerminateWith(tr);
+        if (session != null) {
+            session.disconnect();
+        }
+    }
+    /**----------------SmartFrog Life Cycle Methods End ---------------------*/
+
+    /**
+     * Reads SmartFrog attributes.
+     * @throws SmartFrogResolutionException if failed to read any 
+     * attribute or a mandatory attribute is not defined.
+     * @throws RemoteException in case of network/rmi error
+     */ 
+    private void readSFAttributes() throws SmartFrogException, RemoteException{
+        // Mandatory attributes
+        host = sfResolve(HOST, host, true);
+        userName = sfResolve(USER, userName, true);
+        pwdProvider = (PasswordProvider) sfResolve(pwdProviderRef);
+        password = pwdProvider.getPassword();
+        commandsList = sfResolve(COMMANDS, commandsList, true);
+
+        //optional attributes
+        port = sfResolve(PORT, port, false);
+        timeout = sfResolve(TIMEOUT, timeout, false);
+        failOnError = sfResolve(FAIL_ON_ERROR, failOnError, false);
+        shouldTerminate = sfResolve(TERMINATE, shouldTerminate, false);
+        // TODO: trust onle pre-configured hosts
+        //trustAllCerts = sfResolve(TRUST_ALL_CERTIFICATES, trustAllCerts, false);
+    }
+
+    /**
+     * Opens a SSH session.
+     * @return SSH Session
+     * @thows JSchException if unable to open SSH session
+     * @see com.jcraft.jsch.Session
+     */
+    private Session openSession() throws JSchException {
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(userInfo.getName(), host, port);
+        session.setUserInfo(userInfo);
+        session.setPassword(password);
+        System.out.println("Connecting to " + host + ":" + port);
+        session.connect();
+        return session;
+    }
+    private boolean getFailOnError() {
+        return failOnError;
+    }
+}
+
