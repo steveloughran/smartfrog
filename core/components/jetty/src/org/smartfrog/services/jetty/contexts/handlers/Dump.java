2162cb2529257f61a23f0c62df63d93da3b83625
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-06-16 12:17:14 +0000

    Handlers for jetty server

---
+++ core/components/jetty/src/org/smartfrog/services/jetty/contexts/handlers/Dump.java
@@ -0,0 +1,38 @@
+package org.smartfrog.services.jetty.contexts.handlers;
+
+import java.rmi.RemoteException;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.mortbay.http.handler.DumpHandler;
+import org.mortbay.jetty.servlet.ServletHttpContext;
+
+/**
+ * A Dump handler class for jetty server
+ * @author Ritu Sabharwal
+ */
+
+
+public class Dump extends PrimImpl implements Prim {
+     
+   /** Standard RMI constructor */
+   public Dump() throws RemoteException {
+	   super();
+   }
+   
+   /** 
+   * sfDeploy: adds the Dump Handler to ServetletHttpContext of jetty server 
+   * @exception  SmartFrogException In case of error while deploying  
+   * @exception  RemoteException In case of network/rmi error  
+   */  
+   public void sfDeploy() throws SmartFrogException, RemoteException {
+       super.sfDeploy();   
+       Prim parent = this.sfParent();
+       Prim grandParent = parent.sfParent(); 
+       ServletHttpContext cxt = (ServletHttpContext)grandParent.
+       				sfResolveId("Context");   
+       cxt.addHandler(new DumpHandler());
+   }
+
+}
