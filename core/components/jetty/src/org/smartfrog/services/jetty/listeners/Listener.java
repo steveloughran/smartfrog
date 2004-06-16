10af5a47f573cc69f22b7b00ba6207071dfe1557
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-06-16 12:16:13 +0000

    Files for adding listeners to the jetty server

---
+++ core/components/jetty/src/org/smartfrog/services/jetty/listeners/Listener.java
@@ -0,0 +1,20 @@
+package org.smartfrog.services.jetty.listeners;
+
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+/**
+ * An interface for listeners for jetty server 
+ * @author Ritu Sabharwal
+ */
+
+public interface Listener extends Remote {
+	
+	/**
+	 * Add the listener to the http server
+	 * @exception  RemoteException In case of network/rmi error 
+	 */ 
+	public void addlistener(int listenerPort, String serverHost) throws 
+	SmartFrogException, RemoteException; 
+}
