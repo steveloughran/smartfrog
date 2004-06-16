434a4b1dd3d22954afacd047d3eaadbdc68569da
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-06-16 12:19:54 +0000

    Servlet for Contexts

---
+++ core/components/jetty/src/org/smartfrog/services/jetty/contexts/servlets/Servlet.java
@@ -0,0 +1,67 @@
+package org.smartfrog.services.jetty.contexts.servlets;
+
+import java.rmi.RemoteException;
+import java.util.Vector;
+import java.util.Enumeration;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.mortbay.jetty.servlet.ServletHttpContext;
+import org.mortbay.jetty.servlet.ServletHolder;
+/**
+ * A Servlet class for a Jetty http server.
+ * @author Ritu Sabharwal
+ */
+
+
+public class Servlet extends PrimImpl implements Prim {
+  
+  Reference nameRef = new Reference("name"); 
+  Reference pathSpecRef = new Reference("pathSpec"); 
+  Reference classNameRef = new Reference("className"); 
+  Reference initParamsRef = new Reference("initParams"); 
+	
+  String name = null;
+  String pathSpec = null;
+  String className = null;
+  Vector initParams = null;
+
+  ServletHolder holder = new ServletHolder();
+ 
+  /** Standard RMI constructor */
+  public Servlet() throws RemoteException {
+	  super();
+  }
+   
+  /** 
+   * sfDeploy: adds a servlet to ServetletHttpContext of jetty server 
+   * @exception  SmartFrogException In case of error while deploying  
+   * @exception  RemoteException In case of network/rmi error  
+   */ 
+  public void sfDeploy() throws SmartFrogException, RemoteException {
+	  try {
+		  super.sfDeploy();  
+                  name = sfResolve(nameRef, name, true); 
+                  pathSpec = sfResolve(pathSpecRef, pathSpec, true); 
+                  className = sfResolve(classNameRef,className,true); 
+                  initParams = sfResolve(initParamsRef, initParams, false); 
+                  Prim parent = this.sfParent();
+                  Prim grandParent = parent.sfParent(); 
+                  ServletHttpContext cxt = (ServletHttpContext)grandParent.
+			  sfResolveId("Context");   
+                  holder = cxt.addServlet(name, pathSpec, className);
+                  if (initParams != null) {
+                  for (Enumeration en = initParams.elements();
+				  en.hasMoreElements(); ) {
+			  Vector element = (Vector)en.nextElement();
+	                  String key = element.firstElement().toString();
+	                  String value = element.lastElement().toString();
+	                  holder.setInitParameter(key,value);
+				  }
+		  }
+	  } catch ( Exception ex) {
+		  throw SmartFrogException.forward(ex);
+	  }
+  }
+}
