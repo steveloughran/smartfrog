9d1c66ce28b1ea4e9a646b0f63f6b1931e5c26dd
julgui <julgui@users.noreply.github.com>
2004-06-24 16:03:15 +0000

    Silent local logging.

---
+++ core/smartfrog/src/org/smartfrog/sfcore/logging/LogToNothingImpl.java
@@ -0,0 +1,337 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+
+package org.smartfrog.sfcore.logging;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ * <p>Only out and err output are printed. </p>
+ */
+public class LogToNothingImpl implements LogToErr, Log, LogMessage, LogLevel, Serializable  {
+
+    /** The name of this simple log instance */
+    protected String logName = null;
+
+    /** output stream to print to. Bonded at construct time, and usually system.err unless
+     * otherwise chosen
+     */
+    protected PrintStream outstream;
+
+    /**
+     * Construct a simple log with given name and log level
+     * and log to output level
+
+     */
+    protected LogToNothingImpl() {
+    }
+
+    /**
+     * Construct a simple log with given name and log level
+     * and log to output level
+     * @param name log name
+     * @param intialLogLevel level to log at
+     */
+    public LogToNothingImpl(String name, Integer initialLogLevel) {
+       this(name,initialLogLevel,System.err);
+    }
+
+    /**
+     * Construct a simple log with given name and log level
+     * and log to output level
+     * @param name log name
+     * @param intialLogLevel level to log at. It will be ignored.
+     * @param out output stream to log to
+     */
+
+    public LogToNothingImpl(String name, Integer initialLogLevel,PrintStream out) {
+        assert name != null;
+        logName = name;
+        setOutstream(out);
+    }
+
+
+    /**
+     * set the output stream for logging. must not be null
+     * @param outstream
+     */
+    public void setOutstream(PrintStream outstream) {
+        assert(outstream != null);
+        this.outstream = outstream;
+    }
+
+
+    /**
+     * <p> Set logging level. </p>
+     *
+     * @param currentLogLevel new logging level
+     */
+    public void setLevel(int currentLogLevel) {
+        return;
+    }
+
+
+    /**
+     * <p> Get logging level. </p>
+     */
+    public int getLevel() {
+        return LOG_LEVEL_OFF;
+    }
+
+
+    // -------------------------------------------------------- Logging Methods
+
+    /**
+     * Is the given log level currently enabled?
+     *
+     * @param logLevel is this level enabled?
+     */
+    protected boolean isLevelEnabled(int logLevel) {
+        // log level are numerically ordered so can use simple numeric
+        // comparison
+        return false;
+    }
+
+
+    // -------------------------------------------------------- Log Implementation
+
+
+    /**
+     * <p> Log a message with debug log level.</p>
+     */
+    public final void debug(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with debug log level.</p>
+     */
+    public final void debug(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Log a message with trace log level.</p>
+     */
+    public final void trace(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with trace log level.</p>
+     */
+    public final void trace(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Log a message with info log level.</p>
+     */
+    public final void info(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with info log level.</p>
+     */
+    public final void info(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Log a message with warn log level.</p>
+     */
+    public final void warn(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with warn log level.</p>
+     */
+    public final void warn(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Log a message with error log level.</p>
+     */
+    public final void error(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with error log level.</p>
+     */
+    public final void error(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Log a message with fatal log level.</p>
+     */
+    public final void fatal(Object message) {
+        return;
+    }
+
+
+    /**
+     * <p> Log an error with fatal log level.</p>
+     */
+    public final void fatal(Object message, Throwable t) {
+        return;
+    }
+
+
+    /**
+     * <p> Are debug messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isDebugEnabled() {
+        return false;
+    }
+
+
+    /**
+     * <p> Are error messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isErrorEnabled() {
+        return false;
+    }
+
+
+    /**
+     * <p> Are fatal messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isFatalEnabled() {
+        return false;
+    }
+
+
+    /**
+     * <p> Are info messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isInfoEnabled() {
+        return false;
+    }
+
+    /**
+     * <p> Are trace messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isTraceEnabled() {
+        return false;
+    }
+
+    /**
+     * <p> Are warn messages currently enabled? </p>
+     *
+     * <p> This allows expensive operations such as <code>String</code>
+     * concatenation to be avoided when the message will be ignored by the
+     * logger. </p>
+     */
+    public final boolean isWarnEnabled() {
+        return false;
+    }
+
+
+    // Special LogMessages interface to produce output.
+    /**
+     * <p> Log an error with message log level. </p>
+     * <p> Same as info messages but without Labels.</p>
+     *
+     * @param message log this message
+     */
+    public void out(Object message) {
+         outstream.println(message.toString());
+    }
+
+    /**
+     * <p> Log an error with message log level. </p>
+     * <p> Same as info messages but without Labels.</p>
+     *
+     * @param message log this message
+     * @param t log this cause
+     */
+    public void err(Object message, Throwable t) {
+          outstream.println(message.toString());
+          t.printStackTrace(outstream);
+    }
+
+    /**
+     * <p> Log a message with message log level. </p>
+     * <p> Same as info messages but without Labels.</p>
+     *
+     * @param message log this message
+     */
+    public void err(Object message, SmartFrogException t, TerminationRecord tr) {
+        err(message, t);
+        outstream.println(tr.toString());
+    }
+
+    /**
+     * <p> Log an error with message log level. </p>
+     * <p> Same as info messages but without Labels.</p>
+     *
+     * @param message log this message
+     * @param t log this cause
+     */
+    public void err(Object message, SmartFrogException t) {
+        err(message, t);
+    }
+
+}
+
