03f77e78b8318f1717fde6a4c036c49c366ab8c3
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-07-12 15:23:02 +0000

    Added a missing message, MSG_OBJECT_REGISTRATION_FAILED

--- trunk/core/smartfrog/src/org/smartfrog/sfcore/common/ErrorMessageBundle.java
+++ trunk/core/smartfrog/src/org/smartfrog/sfcore/common/ErrorMessageBundle.java
@@ -95,8 +95,9 @@ public class ErrorMessageBundle extends ListResourceBundle
         { ILLEGAL_VECTOR_PARAMETER  , "Illegal vector parameter"},
         { ILLEGAL_BOOLEAN_PARAMETER  , "Illegal boolean parameter"},
         { MISSING_PARAMETER  , "Missing Parameter {0}"},
-	{ COPY_SERIALIZE_FAILED , "Attempt to copy data failed, exception during serialize/deserialize {0}"},
-	{ COPY_FAILED , "Attempt to copy data failed, data not serializable {0}"}
+        { COPY_SERIALIZE_FAILED , "Attempt to copy data failed, exception during serialize/deserialize {0}"},
+        { COPY_FAILED , "Attempt to copy data failed, data not serializable {0}"},
+        { MSG_OBJECT_REGISTRATION_FAILED, "Failed to register/export the object {0}"}
     };
 
     /**
