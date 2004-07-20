4f2e968b786eaa25588715e59e60c0ae26a19996
aawasthi <aawasthi@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-07-20 12:30:53 +0000

    added feature to download test reports from test nodes in one place

--- trunk/core/components/releasetest/bin/releasetest.sh
+++ trunk/core/components/releasetest/bin/releasetest.sh
@@ -12,6 +12,8 @@ echo SF_RELEASE
 echo $SF_RELEASE
 echo JUNIT_RELEASE
 echo $JUNIT_RELEASE
+echo HOST_NAME
+echo $HOST_NAME
 echo ==============================
 if [ $INSTALL_DIR == " " ] ; then 
     echo Unable to get environment variables
@@ -28,5 +30,10 @@ mv common.xml $INSTALL_DIR
 #################################source  $ANT_HOME/bin/ant package
 echo Changing to dir $INSTALL_DIR/$JUNIT_RELEASE
 cd $INSTALL_DIR/$JUNIT_RELEASE
-source  $ANT_HOME/bin/ant
-echo Executed Junit Test Cases Exiting...
+# Override exit value of ant
+! $ANT_HOME/bin/ant
+echo "Archiving test reports ..."
+mkdir $HOST_NAME
+mv -f build/test/reports ebnt171/reports
+tar -zcf $HOST_NAME.tar.gz ebnt171
+mv $HOST_NAME.tar.gz $INSTALL_DIR/.
