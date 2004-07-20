e5e458137c18e6f829affe3bf583552ca5f19fce
aawasthi <aawasthi@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-07-20 12:30:53 +0000

    added feature to download test reports from test nodes in one place

--- core/components/releasetest/bin/releasetest.bat
+++ core/components/releasetest/bin/releasetest.bat
@@ -18,6 +18,10 @@ echo "SF_RELEASE"
 echo %SF_RELEASE%
 echo "JUNIT_RELEASE"
 echo %JUNIT_RELEASE%
+echo "HOST_NAME"
+echo %HOST_NAME%
+echo "TEST_HOME"
+echo %TEST_HOME%
 echo "========================================"
 if %INSTALL_DIR% == "" goto error
 echo "Unzipping sf release file"
@@ -29,6 +33,7 @@ echo %INSTALL_DIR%
 cd %INSTALL_DIR%
 echo "Moving SF_RELEASE to smartfrog dir"
 mv %SF_RELEASE% smartfrog
+rem call %INSTALL_DIR%\smartfrog\dist\bin\sfDaemon.bat
 echo "Chaging dir to jnit dir"
 cd %INSTALL_DIR%\%JUNIT_RELEASE%
 mv extras %INSTALL_DIR%
@@ -36,6 +41,18 @@ mv common.xml %INSTALL_DIR%
 cd %INSTALL_DIR%\extras\ant
 call %ANT_HOME%\bin\ant.bat
 cd %INSTALL_DIR%\%JUNIT_RELEASE%
-call %ANT_HOME%\bin\ant.bat
+echo "Going to execute testcases in test harness"
+call ant.bat
+if not errorlevel 0 goto continue
+:continue
+echo "After executing testcases in test harness"
+rem move test reports to appropriate folder
+echo "Changing dir to %TEST_HOME%"
+cd %TEST_HOME%
+echo "Creating dir %HOST_NAME%"
+mkdir %HOST_NAME%
+echo "Moving junit reports to test home"
+cd %INSTALL_DIR%\%JUNIT_RELEASE%\build\test
+mv -f reports %TEST_HOME%\%HOST_NAME%
 :exit
-:error "Unable to get environment variables"
+:error "Unable to get env vars"
