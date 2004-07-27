50fbd6a6a671f5203d25df4fe5cb5050fe5a6ee8
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-07-27 13:29:15 +0000

    Configuration file for LogToErrImpl
    Needs to be completed with the real configuration attributes.
    
    Julio

--- trunk/core/smartfrog/bin/setSFProperties.bat
+++ trunk/core/smartfrog/bin/setSFProperties.bat
@@ -5,15 +5,16 @@ set SERVER=localhost:8080
 rem Please edit codebase if you have any other jar file in webserver 
 set CODEBASE="http://%SERVER%/sfExamples.jar" 
 
-
+rem ------------------------------------------------------
 rem SF ENV PROPERTIES  - Please edit with your preferences
+rem ------------------------------------------------------
 
 if not defined SFDEFAULTINI set SFDEFAULTINI="-Dorg.smartfrog.iniFile=org/smartfrog/default.ini"
 if not defined SFDEFAULTSF set SFDEFAULTSF="-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=org/smartfrog/default.sf"
-rem if not SFSECURITY defined set SFSECURITY="-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFPRIVATE%\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFPRIVATE%\SFSecurity.properties"
-rem if not SFCODEBASE defined set SFCODEBASE="-Dorg.smartfrog.codebase=%CODEBASE%"
+rem if not defined SFSECURITY set SFSECURITY="-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFPRIVATE%\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFPRIVATE%\SFSecurity.properties"
+rem if not defined SFCODEBASE set SFCODEBASE="-Dorg.smartfrog.codebase=%CODEBASE%"
 
-rem end SF ENV PROPERTIES
+rem ------------------------------------------------------
 
 set SFCMDPARAMETERS=
 
