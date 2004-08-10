1aa707ca6b3a02152f7a420e674c53a22683cb3c
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-08-10 20:34:02 +0000

    Updated to make more clear what the user can edit

--- trunk/core/smartfrog/bin/setSFProperties.bat
+++ trunk/core/smartfrog/bin/setSFProperties.bat
@@ -1,41 +1,43 @@
 @echo off
-
-rem general properties
-set SERVER=localhost:8080
-rem Please edit codebase if you have any other jar file in webserver 
-set CODEBASE="http://%SERVER%/sfExamples.jar" 
-
-set SFLIBPATH=%SFHOME%\lib
-
 rem ------------------------------------------------------
 rem SF ENV PROPERTIES  - Please edit with your preferences
 rem ------------------------------------------------------
 
+rem -- User libs directory --
 rem set SFUSERHOME=%SFHOME%\mylibs
 
-if not defined SFDEFAULTINI set SFDEFAULTINI=-Dorg.smartfrog.iniFile=org/smartfrog/default.ini
-if not defined SFDEFAULTSF set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=org/smartfrog/default.sf
-rem if not defined SFSECURITY set SFSECURITY=-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFHOME%\private\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFHOME%\private\SFSecurity.properties
-rem if not defined SFCODEBASE set SFCODEBASE=-Dorg.smartfrog.codebase=%CODEBASE%
+rem -- default.ini and default.sf without security --
+set SFDEFAULTINI=-Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini
+set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=%SFHOME%\bin\default.sf
 
-rem ------------------------------------------------------
+rem -- Dynamic classloading: CODEBASE --
+rem set SERVER=localhost:8080
+rem set CODEBASE="http://%SERVER%/sfExamples.jar" 
+rem set SFCODEBASE=-Dorg.smartfrog.codebase=%CODEBASE%
+
+rem -- Security properties --
+rem set SFSECURITY=-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFHOME%\private\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFHOME%\private\SFSecurity.properties
+rem set SFDEFAULTINI=-Dorg.smartfrog.iniFile=org/smartfrog/default.ini
+rem set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=org/smartfrog/default.sf
+
+rem -------------------End user properties-------------------------
+
 
 if defined SFSECURITY   set SFCMDPARAMETERS=%SFCMDPARAMETERS% %SFSECURITY%
 if defined SFDEFAULTINI set SFCMDPARAMETERS=%SFCMDPARAMETERS% %SFDEFAULTINI%
 if defined SFCODEBASE   set SFCMDPARAMETERS=%SFCMDPARAMETERS% %SFCODEBASE%
 
+set SFLIBPATH=%SFHOME%\lib
 if defined SFSECURITY set SFLIBPATH=%SFHOME%\signedLib
 
 rem reset CLASSPATH
 set CLASSPATH=
 
-
-
 call "%SFHOME%\bin\setClassPath"
 
 if not defined SFUSERHOME goto continue2
   set SFLIBPATH=%SFUSERHOME%
-  CALL "%SFHOME%\bin\setClassPath.bat"
+  CALL "%SFHOME%\bin\setClassPath"
 :continue2
 
 if defined srcDir set CLASSPATH=%srcDir%;%classpath%
