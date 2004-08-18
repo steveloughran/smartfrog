00e0abe998e86245cfe2fe01cc81892cca63517b
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-08-18 18:02:38 +0000

    Added multiple SFUSERHOME variables.

--- trunk/core/smartfrog/bin/setSFProperties.bat
+++ trunk/core/smartfrog/bin/setSFProperties.bat
@@ -11,12 +11,15 @@ set SFDEFAULTINI=-Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini
 set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=%SFHOME%\bin\default.sf
 
 rem -- Dynamic classloading: CODEBASE --
-rem set SERVER=localhost:8080
-rem set CODEBASE="http://%SERVER%/sfExamples.jar" 
-rem set SFCODEBASE=-Dorg.smartfrog.codebase=%CODEBASE%
+rem http://eb97201.india.hp.com:8080/sfExamples.jar
+set SERVER=eb97201.india.hp.com:8080
+set SERVER=15.196.4.45
+set CODEBASE="http://%SERVER%/sfExamples.jar" 
+set SFCODEBASE=-Dorg.smartfrog.codebase=%CODEBASE%
 
 rem -- Security properties --
-rem set SFSECURITY=-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFHOME%\private\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFHOME%\private\SFSecurity.properties
+rem set SFSECURITY=-Djava.security.policy==%SFHOME%\private\sf.policy -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName=%SFHOME%\private\mykeys.st -Dorg.smartfrog.sfcore.security.propFile=%SFHOME%\private\SFSecurity.properties 
+rem set SFSECURITY=-Dorg.smartfrog.sfcore.security.debug=true 
 rem set SFDEFAULTINI=-Dorg.smartfrog.iniFile=org/smartfrog/default.ini
 rem set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=org/smartfrog/default.sf
 
@@ -39,6 +42,27 @@ if not defined SFUSERHOME goto continue2
   set SFLIBPATH=%SFUSERHOME%
   CALL "%SFHOME%\bin\setClassPath"
 :continue2
+
+if not defined SFUSERHOME1 goto continue3
+  set SFLIBPATH=%SFUSERHOME1%
+  CALL "%SFHOME%\bin\setClassPath"
+:continue3
+
+if not defined SFUSERHOME2 goto continue4
+  set SFLIBPATH=%SFUSERHOME2%
+  CALL "%SFHOME%\bin\setClassPath"
+:continue4
+
+
+if not defined SFUSERHOME3 goto continue5
+  set SFLIBPATH=%SFUSERHOME3%
+  CALL "%SFHOME%\bin\setClassPath"
+:continue5
+
+if not defined SFUSERHOME4 goto continue6
+  set SFLIBPATH=%SFUSERHOME4%
+  CALL "%SFHOME%\bin\setClassPath"
+:continue6
 
 if defined srcDir set CLASSPATH=%srcDir%;%classpath%
 
