049f9be7a505a7cf25d79efdbe85cc61a16e0ac6
julgui <julgui@users.noreply.github.com>
2004-08-19 15:54:32 +0000

    New permanent scripts - That is the intention anyway :-)

---
+++ core/smartfrog/bin/security/sfManagementConsole.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+if defined SFHOME goto continue1
+  if exist "%cd%\..\sfDaemon.bat" cd ..\..
+  set SFHOME="%cd%"
+  cd bin
+:continue1
+
+rem Uncomment to enable Dynamic ClassLoading
+rem set SFDYNAMICCLASSLOADING_ON=ENABLED
+
+rem Security Enabled
+set SFSECURITY_ON=ENABLED
+
+call "%SFHOME%\bin\sfManagementConsole"
+
+endlocal
+
