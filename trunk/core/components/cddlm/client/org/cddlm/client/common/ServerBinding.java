266e2c7b648c1d76c433fb0d9c8bc8c503911f83
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-09-01 16:32:30 +0000

    ListApplications has a command line and matchin test. Command line support for declaring a new url via the -url: parameter, this has its own tests. All is well.

--- trunk/core/components/cddlm/client/org/cddlm/client/common/ServerBinding.java
+++ trunk/core/components/cddlm/client/org/cddlm/client/common/ServerBinding.java
@@ -34,7 +34,6 @@ import java.rmi.RemoteException;
 public class ServerBinding implements Serializable {
 
 
-
     /**
      * url
      */
@@ -42,6 +41,7 @@ public class ServerBinding implements Serializable {
 
     private String username;
     private String password;
+    public static final String URL_COMMAND = "-url:";
 
 
     public ServerBinding() {
@@ -50,7 +50,6 @@ public class ServerBinding implements Serializable {
     public ServerBinding(URL url) {
         this.url = url;
     }
-
 
 
     public URL getUrl() {
@@ -105,11 +104,92 @@ public class ServerBinding implements Serializable {
     }
 
     public String toString() {
-        if(url==null) {
+        if (url == null) {
             return "(unbound)";
         } else {
             return url.toExternalForm();
         }
+    }
+
+    /**
+     * convert to an external form.
+     *
+     * @return
+     */
+    public String toCommandLineElement() {
+        return URL_COMMAND + url.toExternalForm();
+    }
+
+    /**
+     * get the binding of this element, null for no match,
+     *
+     * @param commandLineElement
+     * @return
+     * @throws MalformedURLException if there was anything wrong with the URL
+     */
+    public static ServerBinding fromCommandLineElement(
+            String commandLineElement)
+            throws MalformedURLException {
+        if (commandLineElement.indexOf(URL_COMMAND) == 0) {
+            String urlBody = commandLineElement.substring(URL_COMMAND.length());
+            if ("".equals(urlBody)) {
+                throw new MalformedURLException(
+                        "no URL in " + commandLineElement);
+            }
+            URL newurl = new URL(urlBody);
+            ServerBinding binding = new ServerBinding(newurl);
+            return binding;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * get the binding of this element, null for no match,
+     *
+     * @param commandLine full command line args
+     * @return
+     * @throws MalformedURLException if there was anything wrong with the URL
+     */
+    public static ServerBinding fromCommandLine(String[] commandLine)
+            throws MalformedURLException {
+        ServerBinding binding = null;
+        for (int i = 0; i < commandLine.length; i++) {
+            binding = fromCommandLineElement(commandLine[i]);
+            if (binding != null) {
+                break;
+            }
+        }
+        return binding;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ServerBinding)) return false;
+
+        final ServerBinding serverBinding = (ServerBinding) o;
+
+        if (password != null ?
+                !password.equals(serverBinding.password) :
+                serverBinding.password != null) {
+            return false;
+        }
+        if (!url.equals(serverBinding.url)) return false;
+        if (username != null ?
+                !username.equals(serverBinding.username) :
+                serverBinding.username != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = url.hashCode();
+        result = 29 * result + (username != null ? username.hashCode() : 0);
+        result = 29 * result + (password != null ? password.hashCode() : 0);
+        return result;
     }
 
 }
