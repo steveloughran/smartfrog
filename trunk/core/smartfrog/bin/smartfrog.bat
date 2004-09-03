c7bfaa14754cf98fabded3cd2166581ae8c20f17
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-09-03 06:19:08 +0000

    Fix for Jira bug SF-25 ; the dist path for smartfrog can have spaces as well

--- trunk/core/smartfrog/bin/smartfrog.bat
+++ trunk/core/smartfrog/bin/smartfrog.bat
@@ -8,7 +8,7 @@ if defined SFHOME goto continue1
 
 if exist "%SFHOME%\jre\bin\java.exe" set path=%SFHOME%\jre\bin
 rem call %SFHOME%\bin\setClassPath
-call %SFHOME%\bin\setSFProperties
+call "%SFHOME%\bin\setSFProperties"
 rem echo %SFCMDPARAMETERS%
 rem echo java -Dorg.smartfrog.iniFile=%SFHOME%\bin\default.ini org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
 java %SFCMDPARAMETERS% org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
