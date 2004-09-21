f520b053d95824e95a15363919817455f99cf977
sfpcg <sfpcg@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-09-21 13:04:59 +0000

    ref function for use in defining references that are built from strings

---
+++ core/smartfrog/src/org/smartfrog/sfcore/languages/sf/functions/Ref.java
@@ -0,0 +1,52 @@
+
+package org.smartfrog.sfcore.languages.sf.functions;
+
+import org.smartfrog.sfcore.languages.sf.PhaseAction;
+import org.smartfrog.sfcore.languages.sf.SmartFrogCompileResolutionException;
+import org.smartfrog.sfcore.languages.sf.functions.BaseFunction;
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.sfcore.common.SFNull;
+/**
+ * Defines the unary operator not that negates its boolean parameter.
+ * The parameter attribute is "data" and an exception will be thrown if it
+ * doesn't exist or is not boolean
+ */
+public class Ref extends BaseFunction implements PhaseAction {
+    boolean lazy;
+    String reference = "";
+
+    /**
+     * Takes the inputs and creates a reference and resolves it if the lazy attribute is false
+     * @return Either the reference, or the resolved reference if the lazy attribute is false
+     * @throws SmartFrogCompileResolutionException if any error occurs in evalatnig the reference or resolving it
+     */
+    protected Object doFunction() throws SmartFrogCompileResolutionException {
+	Object val;
+	Reference r;
+	try {
+	    try {
+		reference = (String) context.get("reference");
+	    } catch (ClassCastException e) {
+		reference = null;
+	    }
+	    
+	    try {
+		lazy = ((Boolean) context.get("lazy")).booleanValue();
+	    } catch (Exception e) {
+		lazy = false;
+	    }
+	    
+	    r = new Reference(reference, true);
+	    if (lazy) {
+		r.setEager(false);
+		val = r;
+	    } else {
+		r.setEager(true);
+		val = component.sfResolve(r);
+	    }
+	} catch (Exception e) {
+	    throw new SmartFrogCompileResolutionException("Error resolving Ref function" , e);
+	}
+	return val;
+    }
+}
