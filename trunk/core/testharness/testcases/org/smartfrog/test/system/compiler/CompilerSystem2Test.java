e4dac32ebc16ca5f876870942a634742d0528f0c
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2004-11-05 01:50:34 +0000

    This test has to be uncommented once CC runs.

--- trunk/core/testharness/testcases/org/smartfrog/test/system/compiler/CompilerSystem2Test.java
+++ trunk/core/testharness/testcases/org/smartfrog/test/system/compiler/CompilerSystem2Test.java
@@ -47,6 +47,6 @@ public class CompilerSystem2Test extends LocalJVMTestBase {
 
             OptionSet options=new OptionSet(args);
 
-            new SFSystem().runSmartFrog(options.cfgDescriptors);
+           // new SFSystem().runSmartFrog(options.cfgDescriptors);
     }
 }
