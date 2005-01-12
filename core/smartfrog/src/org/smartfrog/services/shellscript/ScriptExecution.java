b94f0a125ac23b0e636397a8fa2ab45dd72e833a
sfpcg <sfpcg@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-01-12 11:30:52 +0000

    interfaces to new shellscipt component

---
+++ core/smartfrog/src/org/smartfrog/services/shellscript/ScriptExecution.java
@@ -0,0 +1,100 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.smartfrog.services.shellscript;
+
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import java.util.Vector;
+
+/**
+ * Iterface that provides the API to the script comopnent, allowing
+ * pther co-located comopnents to submit script commands.
+ * 
+ * The interface provides for the submission of lnies of scriot and the
+ * ability to lock a script component for unique use for a period. This ensures
+ * that sequences of script commands will not be niterleaved with other
+ * script requets to the component.
+ *
+ * The operational model is asynchronous, in that the execute operation only queues
+ * the execute request and does not wait until it is complete. An object imlpementing
+ * the ScriptResult interface is returned, and this can be queried to find if the script has
+ * compelted and obtain the resultant output, both error and normal.
+ *
+ * Commands to be executed are passed in as a vector the following format.
+ * Each element is either a string, in which case it is treated as a command, or
+ * a vector in which case the command is the space-separated "toString" of its elements.
+ */
+public interface ScriptExecution {
+    /**
+     * obtain a lock on the shell, will block until it is available
+     *
+     * @param timeout max number of miliseconds to obtain the lock:
+     *    0 is don't wait,
+     *    -1 is wait forever
+     *
+     * @returns lock object that must be used in follow-up execute and
+     * lock release operations
+     *
+     * @throws SmartFrogException if the lock is not obtained in the requisite
+     * time
+     */
+    public ScriptLock lockShell(long timeout) throws SmartFrogException;
+
+    /**
+     * submit  a vector of commands to the shell
+     *
+     * @parameter commands the vector of commands
+     * @parameter lock the lock object receieved from the lockShell
+     *
+     * @returns the "future" ScriptResult implementation that allows the code to
+     * obtain the results of executnig the script
+     *
+     * @throws SmartFrogException if the lock object is not valid, i.e. if it is
+     * not currently holding the l0ck
+     */
+    public ScriptResults execute(Vector commands, ScriptLock lock) throws SmartFrogException;
+
+    /**
+     * relaese the lock on the shell
+     *
+     * @param lock the lock object receieved from the lockShell
+     *
+     * @throws SmartFrogException if the lock object is not valid, i.e. if it is
+     * not currently holding the l0ck
+     */
+    public void releaseShell(ScriptLock lock) throws SmartFrogException;
+
+    /**
+     * submit  a vector of commands to the shell as a single atomic lock/execute/unlock
+     *
+     * @param commands the vector of commands
+     * @param timeout max number of miliseconds to obtain the lock:
+     *    0 is don't wait,
+     *    -1 is wait forever
+     *
+     * @returns the "future" ScriptResult implementation that allows the code to
+     * obtain the results of executnig the script
+     *
+     * @throws SmartFrogException  if the lock is not obtained in the requisite
+     * time
+     */
+    public ScriptResults execute(Vector commands, long timeout) throws SmartFrogException;
+}
---
+++ core/smartfrog/src/org/smartfrog/services/shellscript/ScriptResults.java
@@ -0,0 +1,59 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.smartfrog.services.shellscript;
+
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import java.util.Vector;
+
+/**
+ * Iterface to the "future" object returned from submiting an execute request
+ * to A ScriptExecution imlpmenting object.
+ *
+ * The resuts are structured in the vector of three elements as follows:
+ *   first the result code of the final command in the vector - 0 if not supported in shell,
+ *   second a vector of lines on stdout - empty if not supported in shell,
+ *   third a vector of lines on stderr - empty if not supported in shell.
+ */
+public interface ScriptResults {
+    /**
+     * check to see if the results have now been collates
+     *
+     * @erturns true if the results are erady, false otherwise
+     */
+    public boolean resultsReady();
+
+    /**
+     * wait for the results to be ready for the timeout, and return them when they are
+     *
+     * @oaram timeout the maximum time to wait for the results: 0 don't wait, -1 wait forever
+     *
+     * @returns a vector of results:
+     * The resuts are structured in the vector of three elements as follows:
+     *   first the result code of the final command in the vector - 0 if not supported in shell,
+     *   second a vector of lines on stdout - empty if not supported in shell,
+     *   third a vector of lines on stderr - empty if not supported in shell.
+     *
+     * @throws SmartFrogException if the results are not ready in time
+     */
+    public Vector waitForResults(long timeout) throws SmartFrogException;
+
+}
