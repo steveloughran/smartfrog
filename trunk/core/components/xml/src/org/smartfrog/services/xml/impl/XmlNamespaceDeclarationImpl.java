38cd6cdd532b5e0bb39d27648a38c2e07aaebc5a
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-03-24 16:22:18 +0000

    more XML stuff

--- trunk/core/components/xml/src/org/smartfrog/services/xml/impl/XmlNamespaceDeclarationImpl.java
+++ trunk/core/components/xml/src/org/smartfrog/services/xml/impl/XmlNamespaceDeclarationImpl.java
@@ -1,30 +1,33 @@
 package org.smartfrog.services.xml.impl;
 
-import org.smartfrog.sfcore.prim.PrimImpl;
-import org.smartfrog.sfcore.common.SmartFrogResolutionException;
+import nu.xom.Element;
 import org.smartfrog.services.xml.interfaces.XmlNamespaceDeclaration;
+import org.smartfrog.sfcore.common.SmartFrogResolutionException;
+import org.smartfrog.sfcore.prim.PrimImpl;
 
 import java.rmi.RemoteException;
 
-import nu.xom.Element;
-
 /**
- * This class does nothing other than add its declaration to an XML element when told to do so.
+ * This class does nothing other than add its declaration to an XML element when
+ * told to do so.
  */
-public class XmlNamespaceDeclarationImpl extends PrimImpl implements XmlNamespaceDeclaration {
+public class XmlNamespaceDeclarationImpl extends PrimImpl
+        implements XmlNamespaceDeclaration {
 
     public XmlNamespaceDeclarationImpl() throws RemoteException {
     }
 
     /**
      * look up our namespace declaration and add it to the XML element
+     *
      * @param element
      * @throws SmartFrogResolutionException
      * @throws RemoteException
      */
-    public void addDeclaration(Element element) throws SmartFrogResolutionException, RemoteException {
+    public void addDeclaration(Element element)
+            throws SmartFrogResolutionException, RemoteException {
         String prefix = sfResolve(ATTR_PREFIX, (String) null, true);
         String namespace = sfResolve(ATTR_NAMESPACE, (String) null, false);
-        element.addNamespaceDeclaration(prefix,namespace);
+        element.addNamespaceDeclaration(prefix, namespace);
     }
 }
