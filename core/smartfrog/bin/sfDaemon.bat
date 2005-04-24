a8bdc48b0ab5217a83d80ef9aa8d55b5ea765345
julgui <julgui@users.noreply.github.com>
2005-04-24 20:16:33 +0000

    #JVMARGS are passed as args to the java exec
    #sort cmd line args into JVMARGS or CLASSARGS.
    #JVMARGS are declared using -J token
    #e.g. -J "-Djava.library.path=/libs -Xmx400M"
    #e.g. -J -Djava.library.path=/libs -J -Xmx400M

--- core/smartfrog/bin/sfDaemon.bat
+++ core/smartfrog/bin/sfDaemon.bat
@@ -15,6 +15,27 @@ call "%SFHOME%\bin\setSFProperties"
 rem sfDefault files are only needed in sfDaemon and sfRun
 if defined SFDEFAULTSF  set SFCMDPARAMETERS=%SFCMDPARAMETERS% %SFDEFAULTSF%
 
+rem sort cmd line args into JVMARGS or CLASSARGS.
+rem JVMARGS are declared using -J token 
+rem e.g. -J "-Djava.library.path=/libs -Xmx400M"
+rem e.g. -J "-Djava.library.path=/libs" -J -Xmx400M
+rem (JVMARGS are appended to SFCMDPARAMETERS)
+SET CLASSARGS=
+:start
+IF /I "%1"=="-J" GOTO readJARG 
+SET CLASSARGS=%CLASSARGS% %1
+GOTO test
+
+:readJARG
+SHIFT
+SET SFCMDPARAMETERS=%SFCMDPARAMETERS% %1
+
+:test
+SHIFT
+IF NOT "%1"=="" GOTO start
+
+:end
+@echo on
 java %SFCMDPARAMETERS% -Dorg.smartfrog.sfcore.processcompound.sfProcessName=rootProcess org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 endlocal
