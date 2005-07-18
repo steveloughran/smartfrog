b4e7712732ccf0050632ba69278df0978354056d
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2005-07-18 13:49:49 +0000

    message handler handles messages

--- projects/alpine/prototype/M32/src/java/org/smartfrog/projects/alpine/interfaces/MessageHandler.java
+++ projects/alpine/prototype/M32/src/java/org/smartfrog/projects/alpine/interfaces/MessageHandler.java
@@ -1,7 +1,19 @@
 package org.smartfrog.projects.alpine.interfaces;
 
+import org.smartfrog.projects.alpine.core.MessageContext;
+import org.smartfrog.projects.alpine.core.EndpointContext;
+import org.smartfrog.projects.alpine.faults.AlpineRuntimeException;
+
 /**
  * A message handler
  */
 public interface MessageHandler {
+    
+    /**
+     * Message handler
+     * @param messageContext
+     * @param endpointContext
+     * @throws AlpineRuntimeException
+     */ 
+    public void processMessage(MessageContext messageContext,EndpointContext endpointContext) ;
 }
