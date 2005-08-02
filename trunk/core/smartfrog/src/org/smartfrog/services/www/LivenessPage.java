3d9b726ec77680cdc2e239e0d8a6c233f9e5bed4
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-08-02 21:11:04 +0000

    more changes here

--- trunk/core/smartfrog/src/org/smartfrog/services/www/LivenessPage.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/www/LivenessPage.java
@@ -22,22 +22,24 @@ package org.smartfrog.services.www;
 import java.rmi.Remote;
 
 /**
+ * Interface for liveness checking
  * created 28-Apr-2004 17:41:06
  */
 
 
 public interface LivenessPage extends Remote {
 
-    String URL = "url";
-    String HOST = "host";
-    String PORT = "port";
-    String PROTOCOL = "protocol";
-    String PAGE = "page";
-    String FOLLOW_REDIRECTS = "followRedirects";
-    String FETCH_ERROR_TEXT = "fetchErrorText";
-    String MINIMUM_RESPONSE_CODE = "minimumResponseCode";
-    String MAXIMUM_RESPONSE_CODE = "maximumResponseCode";
-    String CHECK_FREQUENCY = "checkFrequency";
-    String ENABLED = "enabled";
-    String QUERIES = "queries";
+    static final String ATTR_URL = "url";
+    static final String ATTR_HOST = "host";
+    static final String ATTR_PORT = "port";
+    static final String ATTR_PROTOCOL = "protocol";
+    static final String ATTR_PAGE = "page";
+    static final String ATTR_FOLLOW_REDIRECTS = "followRedirects";
+    static final String ATTR_ERROR_TEXT = "fetchErrorText";
+    static final String ATTR_MINIMUM_RESPONSE_CODE = "minimumResponseCode";
+    static final String ATTR_MAXIMUM_RESPONSE_CODE = "maximumResponseCode";
+    static final String ATTR_CHECK_FREQUENCY = "checkFrequency";
+    static final String ATTR_ENABLED = "enabled";
+    static final String ATTR_QUERIES = "queries";
+    static final String ATTR_MIME_TYPES = "mimeType";
 }
