18e3c596c1b494da7374330c6d893490dbd6af8d
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-09-05 21:24:25 +0000

    updated WAR support.

--- trunk/core/smartfrog/src/org/smartfrog/services/www/context/WebApplicationImpl.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/www/context/WebApplicationImpl.java
@@ -2,38 +2,23 @@ package org.smartfrog.services.www.context;
 
 import org.smartfrog.sfcore.common.SmartFrogException;
 import org.smartfrog.sfcore.common.SmartFrogLivenessException;
+import org.smartfrog.sfcore.prim.Liveness;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+import org.smartfrog.services.www.ApplicationServerContext;
+import org.smartfrog.services.www.JavaWebApplication;
 
 import java.rmi.RemoteException;
 
 /**
+ * A WAR File
  */
-public class WebApplicationImpl extends ApplicationServerContextImpl {
+public class WebApplicationImpl extends ApplicationServerContextImpl implements JavaWebApplication {
 
     public WebApplicationImpl() throws RemoteException {
     }
 
-    /**
-     * undeploy us if bound, do nothing if not. the context handle is reset,
-     * so we no longer consider ourselves bound
-     *
-     * @throws SmartFrogException
-     * @throws RemoteException
-     */
-    public void undeploy() throws SmartFrogException, RemoteException {
-        super.undeploy();
+    protected ApplicationServerContextEntry deployThisComponent() throws RemoteException, SmartFrogException {
+        return getServer().deployWebApplication(this);
     }
 
-    public void start() throws SmartFrogException, RemoteException {
-        super.start();
-    }
-
-    /**
-     * liveness check
-     *
-     * @throws SmartFrogLivenessException
-     * @throws RemoteException
-     */
-    public void ping() throws SmartFrogLivenessException, RemoteException {
-        super.ping();
-    }
 }
