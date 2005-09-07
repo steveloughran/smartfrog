39d2b3dbb413fac913a4a77ecbf44bc8af6fc387
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2005-09-07 10:30:28 +0000

    separate build of the services.www component. check out the v. minimal build.xml!

---
+++ core/components/www/src/org/smartfrog/services/www/context/ServletComponentImpl.java
@@ -0,0 +1,88 @@
+package org.smartfrog.services.www.context;
+
+import org.smartfrog.services.www.ServletComponent;
+import org.smartfrog.services.www.ServletContextComponentDelegate;
+import org.smartfrog.services.www.ServletContextIntf;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.common.SmartFrogLivenessException;
+import org.smartfrog.sfcore.prim.Liveness;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+
+import java.rmi.RemoteException;
+
+/**
+ * This implements a servlet
+ */
+public class ServletComponentImpl extends ServletContextComponentImpl implements ServletComponent {
+    private ServletContextComponentDelegate delegate;
+
+    public ServletComponentImpl() throws RemoteException {
+    }
+
+
+
+    /**
+     * Called after instantiation for deployment purposes. Heart monitor is
+     * started and if there is a parent the deployed component is added to the
+     * heartbeat. Subclasses can override to provide additional deployment
+     * behavior.
+     *
+     * @throws SmartFrogException error while deploying
+     * @throws RemoteException    In case of network/rmi error
+     */
+    public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
+        super.sfDeploy();
+        //here we are bound to our context
+        ServletContextIntf servletContext = getServletContext();
+        delegate = servletContext.addServlet(this);
+
+    }
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws SmartFrogException failure while starting
+     * @throws RemoteException    In case of network/rmi error
+     */
+    public synchronized void sfStart() throws SmartFrogException, RemoteException {
+        super.sfStart();
+        delegate.start();
+    }
+
+    /**
+     * Liveness call in to check if this component is still alive.
+     *
+     * @param source source of call
+     * @throws SmartFrogLivenessException component is terminated
+     * @throws RemoteException            for consistency with the {@link Liveness} interface
+     */
+    public void sfPing(Object source) throws SmartFrogLivenessException, RemoteException {
+        super.sfPing(source);
+        if(delegate==null) {
+            throw new SmartFrogLivenessException("Not live");
+        } else {
+            delegate.ping();
+        }
+    }
+
+    /**
+     * Provides hook for subclasses to implement useful termination behavior.
+     * Deregisters component from local process compound (if ever registered)
+     *
+     * @param status termination status
+     */
+    public synchronized void sfTerminateWith(TerminationRecord status) {
+        super.sfTerminateWith(status);
+        if (delegate != null) {
+            try {
+                delegate.undeploy();
+            } catch (RemoteException e) {
+                //swallowed
+
+            } catch (SmartFrogException e) {
+                //swallowed
+            }
+        }
+    }
+}
