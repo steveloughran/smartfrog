44515db8c86deda9718cd53bac42a21bfc7e273b
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2005-10-13 16:15:31 +0000

    Refactored and improved.

--- trunk/core/smartfrog/src/org/smartfrog/sfcore/workflow/combinators/Deployer.java
+++ trunk/core/smartfrog/src/org/smartfrog/sfcore/workflow/combinators/Deployer.java
@@ -6,11 +6,8 @@ import org.smartfrog.sfcore.common.SmartFrogException;
 import java.rmi.RemoteException;
 import java.util.Vector;
 import org.smartfrog.sfcore.reference.Reference;
-import org.smartfrog.sfcore.common.CreateNewChildThread;
 import org.smartfrog.sfcore.common.SmartFrogRuntimeException;
-import java.util.Enumeration;
 import org.smartfrog.sfcore.componentdescription.ComponentDescription;
-import org.smartfrog.sfcore.prim.Prim;
 import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.parser.Phases;
 import java.io.InputStream;
@@ -23,18 +20,6 @@ import java.util.Map;
  *
  */
 public class Deployer  extends Run implements Compound {
-
-    /** Reference to myName for the new component.
-     String name for attribute. Value {@value} or asName.*/
-    final static String ATTR_NAME = "newComponentName";
-
-    /** Reference to parent for the new deployment.
-     String name for attribute. Value {@value} or parent.*/
-    final static String ATTR_PARENT = "newComponentParent";
-
-    /** Reference to compund component used to drive the new deployment.
-     String name for attribute. Value {@value} or parent.*/
-    final static String ATTR_DEPLOYER = "newComponentDeployer";
 
     /** Reference to #codebase.
      String name for attribute. Value {@value}.*/
@@ -52,10 +37,6 @@ public class Deployer  extends Run implements Compound {
     String newComponentDescription = null;
     Vector newComponentExtraAtributes = null;
 
-    Compound newComponentDeployer = null;
-
-    ComponentDescription newComponentCD = null;
-
     /**
      * Constructs Deployer.
      *
@@ -65,48 +46,17 @@ public class Deployer  extends Run implements Compound {
         super();
     }
 
-    /**
-     * Reads the basic configuration of the component and deploys.
-     *
-     * @throws RemoteException In case of network/rmi error
-     * @throws SmartFrogDeploymentException In case of any error while
-     *         deploying the component
-     */
-    public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
-        super.sfDeploy();
-        newComponentCD = getComponentDescription();
-    }
-
     protected void readSFAttributes() throws  RemoteException, SmartFrogResolutionException {
         // It will by default use the same used in Run if available
         super.readSFAttributes();
-        parent =  sfResolve(ATTR_PARENT, parent, false);
-        asName =  sfResolve(ATTR_NAME, asName, false);
-
-        newComponentDeployer =  sfResolve(ATTR_DEPLOYER, newComponentDeployer, false);
 
         newComponentDescription = sfResolve(ATTR_DESCRIPTION,newComponentDescription,true);
         newComponentCodebase = sfResolve(ATTR_HCODEBASE,newComponentCodebase,false);
         newComponentExtraAtributes = sfResolve(ATTR_EXTRA_ATTRIBUTES,newComponentExtraAtributes,false);
     }
 
-    protected Prim createNewChild() throws SmartFrogDeploymentException, RemoteException {
-        Prim comp;
-        if (newComponentDeployer!=null) {
-            comp = newComponentDeployer.sfCreateNewChild(asName,parent,newComponentCD,null);
-        } else {
-            //Same semantics as RUN
-            if (parent!=null) {
-                comp = parent.sfCreateNewChild(asName, newComponentCD, null);
-            } else {
-                comp = sfCreateNewApp(asName, newComponentCD, null);
-            }
-        }
-        return comp;
-    }
 
-    protected ComponentDescription getComponentDescription() throws
-        SmartFrogDeploymentException, SmartFrogException {
+    protected ComponentDescription getComponentDescription() throws SmartFrogException {
         Phases phases = null;
         try {
             InputStream descriptionStream = org.smartfrog.SFSystem.getInputStreamForResource(newComponentDescription);
