582ca7978550253b194d3d03881fc000a119eb6d
julgui <julgui@users.noreply.github.com>
2005-12-13 17:39:23 +0000

    New CopyFile component.

---
+++ core/smartfrog/src/org/smartfrog/services/filesystem/CopyFileImpl.java
@@ -0,0 +1,89 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+package org.smartfrog.services.filesystem;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.compound.Compound;
+import org.smartfrog.sfcore.compound.CompoundImpl;
+import org.smartfrog.sfcore.utils.ComponentHelper;
+import org.smartfrog.sfcore.common.*;
+
+
+public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
+
+    FileUsingComponent fromFile = null;
+    FileUsingComponent toFile = null;
+
+    public CopyFileImpl() throws RemoteException {
+    }
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws org.smartfrog.sfcore.common.SmartFrogException
+     *                                  failure while starting
+     * @throws java.rmi.RemoteException In case of network/rmi error
+     */
+    public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
+        super.sfDeploy();
+        copyFile();
+    }
+
+    /**
+     * Copies file using attributes "source" and "destination" from description
+     * @throws SmartFrogException
+     * @throws RemoteException
+     * @throws SmartFrogResolutionException
+     */
+    private void copyFile() throws SmartFrogException, RemoteException,
+        SmartFrogResolutionException {
+        Object obj =null;
+        obj = sfResolve (ATTR_FROM , true);
+        if ((obj!=null) && (obj instanceof FileUsingComponent)) {
+            fromFile = ((FileUsingComponent)obj);
+        }
+        obj = sfResolve (ATTR_TO , false);
+        if ((obj!=null) && (obj instanceof FileUsingComponent)) {
+            toFile = ((FileUsingComponent)obj);
+        }
+        try {
+            FileSystem.fCopy( ((FileUsingComponentImpl)fromFile).getFile(), ((FileUsingComponentImpl)toFile).getFile());
+        } catch (IOException ex) {
+            throw SmartFrogException.forward("Failed when copying "+
+                                              ((FileIntf)fromFile).getAbsolutePath() +
+                                              " to " +((FileIntf)fromFile).getAbsolutePath()
+                                              ,ex);
+        }
+    }
+
+    /**
+     * we only create the directory at startup time, even though we bond at deploy time
+     * @throws SmartFrogException
+     * @throws RemoteException
+     */
+    public synchronized void sfStart() throws SmartFrogException, RemoteException {
+        super.sfStart();
+        new ComponentHelper(this).sfSelfDetachAndOrTerminate("normal","Copy ",this.sfCompleteNameSafe(),null);
+    }
+}
