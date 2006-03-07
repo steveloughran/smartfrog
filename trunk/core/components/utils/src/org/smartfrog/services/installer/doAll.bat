6b087f63dd2efb6c9f860bc23c5c711821af6664
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-03-07 10:53:54 +0000

    moved to a new folder core\sfdeprecated

--- trunk/core/components/utils/src/org/smartfrog/services/installer/doAll.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-call %SFHOME%\bin\setSFProperties
-java org.smartfrog.services.installer.TemplateGen -h hosts.all -o examples\test.sf -t sfInstaller.vm
-java org.smartfrog.services.installer.TemplateGen -s -h hosts.all -o examples\sectest.sf -t sfInstaller.vm
-java org.smartfrog.services.installer.TemplateGen -d -h hosts.all -o examples\DLtest.sf -t sfInstaller.vm
-java org.smartfrog.services.installer.TemplateGen -s -d -h hosts.all -o examples\secDLtest.sf -t sfInstaller.vm
-
