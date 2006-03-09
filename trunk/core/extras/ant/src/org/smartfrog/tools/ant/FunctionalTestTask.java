1dbbcd71101e11769b8d301e249337583de94c3f
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-03-09 12:50:57 +0000

    updated ant test jar. the functional task has been reworked to trigger teardown, even while the application is running. After a pause (Configurable by shutdownTime), if the app is still running, it is forcibly terminated.

--- trunk/core/extras/ant/src/org/smartfrog/tools/ant/FunctionalTestTask.java
+++ trunk/core/extras/ant/src/org/smartfrog/tools/ant/FunctionalTestTask.java
@@ -19,7 +19,6 @@
  */
 package org.smartfrog.tools.ant;
 
-import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask;
 import org.apache.tools.ant.taskdefs.Sequential;
 import org.apache.tools.ant.taskdefs.Parallel;
 import org.apache.tools.ant.Task;
@@ -27,30 +26,24 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 
 /**
- * This is an extension of junit that lets us integrate startup,
- * a waitfor condition and a shutdown sequence into the test case.
- * It's based upon Ant's own JUnit task, and credits Cactus as
- * showing what could be done. Apache Cactus is coded around deploying
- * to an application server; this task can deploy to anything for
- * which we have setup and teardown sequences.
- *
- *
- * This is the workflow for testing
- * <ol>
- * <li>The startup sequence is run to completion</li>
- * <li>In parallel, ( a) the application is started</li>
- * <li>and (b) the sequence of waitfor+tests is run.</li>
- * <li>After the tests run, the teardown operation is executed</li>
- * <li>Any build exception thrown by testing is thrown</li>
- * </ol>
- * <p>No matter what goes wrong, once startup has succeeded, teardown
- * gets invoked.
- * </p>
+ * This is an extension of junit that lets us integrate startup, a waitfor
+ * condition and a shutdown sequence into the test case. It's based upon Ant's
+ * own JUnit task, and credits Cactus as showing what could be done. Apache
+ * Cactus is coded around deploying to an application server; this task can
+ * deploy to anything for which we have setup and teardown sequences.
+ * <p/>
+ * <p/>
+ * This is the workflow for testing <ol> <li>The startup sequence is run to
+ * completion</li> <li>In parallel, ( a) the application is started</li> <li>and
+ * (b) the sequence of waitfor+tests is run.</li> <li>After the tests run, the
+ * teardown operation is executed</li> <li>Any build exception thrown by testing
+ * is thrown</li> </ol> <p>No matter what goes wrong, once startup has
+ * succeeded, teardown gets invoked. </p>
  */
 public class FunctionalTestTask extends Task {
 
     //delegate to hold our settings
-    private JUnitTask junitTask;
+    private Sequential test;
     //the parallel workflow
     private Parallel parallel;
     //startup sequence
@@ -62,9 +55,11 @@ public class FunctionalTestTask extends Task {
     //waitfor operation
     private FaultingWaitForTask probe;
     private int timeout;
+    private int shutdownTime;
     private TaskHelper helper = new TaskHelper(this);
-    public static final String MESSAGE_NO_JUNIT = "No junit element has been defined -no tests will run";
+    public static final String MESSAGE_NO_JUNIT = "No tests defined";
     public static final String EXCEPTION_CAUGHT_ON_CLEANUP = "Exception caught on cleanup:";
+    public static final String MESSAGE_FORCED_SHUTDOWN_OF_APPLICATION = "Forced shutdown of application";
 
 
     public FunctionalTestTask() throws Exception {
@@ -76,9 +71,9 @@ public class FunctionalTestTask extends Task {
     }
 
     /**
-     * Define a sequence of operations to run at startup.
-     * After running these, the teardown sequence will be called to tear down
-     * the system.
+     * Define a sequence of operations to run at startup. After running these,
+     * the teardown sequence will be called to tear down the system.
+     *
      * @param setup
      */
     public void addSetup(Sequential setup) {
@@ -91,9 +86,13 @@ public class FunctionalTestTask extends Task {
 
     /**
      * A sequence of operations that are used to tear down the system.
+     *
      * @param teardown
      */
     public void addTeardown(Sequential teardown) {
+        if (this.teardown != null) {
+            log("Overriding previous definition of <teardown>");
+        }
         this.teardown = teardown;
     }
 
@@ -103,44 +102,59 @@ public class FunctionalTestTask extends Task {
     }
 
     /**
-     * The sequence of tasks used to define the application. This
-     * is for hosting the server in a parallel thread to the test run.
+     * The sequence of tasks used to define the application. This is for hosting
+     * the server in a parallel thread to the test run.
+     *
      * @param application
      */
     public void addApplication(Sequential application) {
+        if(this.application!=null) {
+            log("Overriding previous definition of <test>");
+        }
         this.application = application;
     }
 
     /**
      * This is the timeout for testing; any timeouts in the probe are not
      * covered under this.
+     *
      * @param timeout in seconds.
      */
     public void setTestTimeout(int timeout) {
         this.timeout = timeout;
     }
 
+    /**
+     * Set the timeout allowed for a clean shutdown of the application.
+     * After this timeout, a more brutal operation is used.
+     * @param shutdownTime timeout in seconds
+     */
+    public void setShutdownTime(int shutdownTime) {
+        this.shutdownTime = shutdownTime;
+    }
 
     /**
      * Add a probe, conditions that have to be met before testing begins.
+     *
      * @param probe
      */
     public void addProbe(FaultingWaitForTask probe) {
-        if(this.probe !=null) {
-            throw new BuildException("Only one probe is supported");
+        if (this.probe != null) {
+            log("Overriding previous definition of <probe>");
         }
-        this.probe =probe;
+        this.probe = probe;
     }
 
     /**
      * Declare a list of junit tasks
+     *
      * @param junit
      */
-    public void addJunit(JUnitTask junit) {
-        if (this.junitTask!= null) {
-            throw new BuildException("Only one junit is supported");
+    public void addTest(Sequential junit) {
+        if (this.test != null) {
+            log("Overriding previous definition of <test>");
         }
-        this.junitTask = junit;
+        this.test = junit;
     }
 
     /**
@@ -156,65 +170,179 @@ public class FunctionalTestTask extends Task {
 
         //set up the test and application in parallel; if
         //the application is null, then only the tests run
+        int totalTimeout = timeout;
+        if (probe != null) {
+            totalTimeout += probe.getMaxWait();
+        }
         parallel = new Parallel();
         helper.bindTask(parallel);
-        int totalTimeout=timeout;
-        if(probe!=null) {
-            totalTimeout+=probe.getMaxWait();
-        }
-        if(totalTimeout>0) {
-            parallel.setTimeout(totalTimeout*1000);
-        }
         parallel.setFailOnAny(true);
 
+        Parallel applicationParallel = new Parallel();
+        helper.bindTask(applicationParallel);
+        BackgroundTask backgroundApp = null;
+        backgroundApp = new BackgroundTask(applicationParallel);
+
         //add the application and test task in parallel
-        if(application!=null) {
-            parallel.addTask(application);
+        if (application != null) {
+            applicationParallel.addTask(application);
+        }
+        int timeoutMillis = totalTimeout * 1000;
+        if (totalTimeout > 0) {
+            parallel.setTimeout(timeoutMillis);
         }
 
         //but the test task runs after the probe
-        Sequential testRun=new Sequential();
+        Sequential testRun = new Sequential();
         helper.bindTask(testRun);
-        if(probe !=null) {
+        if (probe != null) {
             testRun.addTask(probe);
         }
-        if(junitTask!=null) {
-            junitTask.setTimeout(new Integer(timeout));
-            testRun.addTask(junitTask);
+        if (test != null) {
+            testRun.addTask(test);
         } else {
             log(MESSAGE_NO_JUNIT);
         }
         parallel.addTask(testRun);
 
         //this is where we encode all the workflow sequence described earlier.
-        if(setup !=null) {
+        if (setup != null) {
             setup.execute();
         }
-        BuildException caught=null;
+        BuildException testFault = null;
+        BuildException teardownFault = null;
         //here we have started, so from hereon teardown is required to be called
         try {
+            //start the app
+            backgroundApp.start();
+            //start the probe+test sequence
             parallel.execute();
         } catch (BuildException e) {
-            caught=e;
+            //something went wrong, probably in execution
+            testFault = e;
         }
         finally {
-            if(teardown!=null) {
+            //teardown
+            if (teardown != null) {
                 try {
                     teardown.execute();
                 } catch (BuildException e) {
-                    if(caught!=null) {
-                        //dont let the cleanup exception get in the way of any other failure
-                        log(EXCEPTION_CAUGHT_ON_CLEANUP +e.toString(), Project.MSG_ERR);
-                    } else {
-                        caught = e;
-                    }
+                    teardownFault=e;
                 }
             }
         }
-        //rethrow anything we caught during execution or cleanup
-        if(caught!=null) {
-            throw caught;
+        //now, teardown has executed, so we wait for the application to finish
+        try {
+            //first a moment of time for a clean shutdown, equal to the total time
+            if (backgroundApp.isRunning()) {
+                backgroundApp.waitForTermination(shutdownTime);
+            }
+            if (backgroundApp.isRunning()) {
+                //then, if its still running, interrupt it.
+                log(MESSAGE_FORCED_SHUTDOWN_OF_APPLICATION);
+                backgroundApp.interrupt();
+                backgroundApp.waitForTermination(shutdownTime);
+            }
+        } catch (InterruptedException e) {
+            //this is good.
         }
 
+        //now some fault cleanup.
+        //test faults get priority
+        //then application faults
+        //and finally teardown ones
+
+
+        //look for an application fault
+        BuildException applicationFault = backgroundApp.getException();
+        if (applicationFault != null) {
+            if (testFault == null ) {
+                //copy any
+                testFault = applicationFault;
+            } else {
+                log(EXCEPTION_CAUGHT_ON_CLEANUP + applicationFault.toString(),
+                        Project.MSG_ERR);
+            }
+        }
+
+        //now look for teardown faults
+        if(teardownFault!=null) {
+            if (testFault == null) {
+                testFault = teardownFault;
+            } else {
+                //dont let the cleanup exception get in the way of any other failure
+                log(EXCEPTION_CAUGHT_ON_CLEANUP + teardownFault.toString(),
+                        Project.MSG_ERR);
+            }
+        }
+
+        //rethrow anything we caught during execution or cleanup
+        if (testFault != null) {
+            throw testFault;
+        }
+
+    }
+
+    /**
+     * Run a task in the background. when finished, it will signal.
+     */
+    private class BackgroundTask implements Runnable {
+        private Task task;
+
+        private BuildException exception;
+        private Thread thread;
+
+        public BackgroundTask(Task task) {
+            thread = new Thread(this);
+            this.task = task;
+        }
+
+        public BuildException getException() {
+            return exception;
+        }
+
+        public synchronized void start() {
+            thread.start();
+        }
+
+        public synchronized void waitForTermination(int seconds)
+                throws InterruptedException {
+            wait(seconds*1000);
+        }
+
+        /**
+         * Interrupt execution of the system.
+         */
+        public synchronized void interrupt() {
+            if (isRunning()) {
+                thread.interrupt();
+            }
+        }
+
+        /**
+         * Test for a thread running
+         *
+         * @return
+         */
+        private boolean isRunning() {
+            return thread.isAlive();
+        }
+
+        /**
+         * @see Thread#run()
+         */
+        public void run() {
+            try {
+                task.execute();
+            } catch (BuildException e) {
+                exception = e;
+            } finally {
+                //wake up our owner, if it is waiting
+                synchronized(this) {
+                    notify();
+                }
+            }
+
+        }
     }
 }
