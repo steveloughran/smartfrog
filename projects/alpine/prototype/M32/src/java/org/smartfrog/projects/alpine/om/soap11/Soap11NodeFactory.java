c6e80f9bbe7a685db4ae16442663e6d75eda2647
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2006-03-27 10:05:56 +0000

    now that CVS is back, time to commit

--- projects/alpine/prototype/M32/src/java/org/smartfrog/projects/alpine/om/soap11/Soap11NodeFactory.java
+++ projects/alpine/prototype/M32/src/java/org/smartfrog/projects/alpine/om/soap11/Soap11NodeFactory.java
@@ -48,6 +48,7 @@ public class Soap11NodeFactory implements NamespaceNodeFactory {
             return new Header(name, namespace);
         }
         if (Soap11Constants.ELEMENT_FAULT.equals(name)) {
+            //TODO: more processing from the content?
             return new Fault(name, namespace);
         }
         
