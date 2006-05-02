12d5b0bdd3f67a05e759dcf0d6c2ad2529dd4de1
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-05-02 22:58:35 +0000

    no message

--- trunk/core/smartfrog/bin/setSFDefaultProperties.bat
+++ trunk/core/smartfrog/bin/setSFDefaultProperties.bat
@@ -7,12 +7,12 @@ rem ------------------------------------------------------
 rem -- User libs directory --
 rem set SFUSERHOME=%SFHOME%\mylibs
 rem -- server hostname for multihomed machines
-set SFSERVERHOSTNAME=127.0.0.1
+rem set SFSERVERHOSTNAME=localhost
 
 rem -- default.ini and default.sf without security --
 if not defined SFDEFAULTINI set SFDEFAULTINI=-Dorg.smartfrog.iniFile="%SFHOME%\bin\default.ini"
 if not defined SFDEFAULTSF set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault="%SFHOME%\bin\default.sf"
 
-if defined SFSERVERHOSTNAME set SFRMIHOSTNAME=-Djava.rmi.server.hostname="&SFSERVERHOSTNAME"
+if defined SFSERVERHOSTNAME set SFRMIHOSTNAME=-Djava.rmi.server.hostname="%SFSERVERHOSTNAME%"
 
 rem -------------------End user properties-------------------------
