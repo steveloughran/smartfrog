bdd0882b22c0fdad27c016e8cd1741313c52e455
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-05-02 16:50:01 +0000

    Added new env variable SFSERVERHOSTNAME to define the -Djava.rmi.sever.hostname=SFSEVERHOSTNAME property that is useful in multihomed machines. This is only used in sfDaemon shell scripts.

--- trunk/core/smartfrog/bin/setSFDefaultProperties.bat
+++ trunk/core/smartfrog/bin/setSFDefaultProperties.bat
@@ -6,9 +6,13 @@ rem ------------------------------------------------------
 
 rem -- User libs directory --
 rem set SFUSERHOME=%SFHOME%\mylibs
+rem -- server hostname for multihomed machines
+rem set SFSEVERHOSTNAME=127.0.0.1
 
 rem -- default.ini and default.sf without security --
 if not defined SFDEFAULTINI set SFDEFAULTINI=-Dorg.smartfrog.iniFile="%SFHOME%\bin\default.ini"
 if not defined SFDEFAULTSF set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault="%SFHOME%\bin\default.sf"
+
+if defined SFSEVERHOSTNAME set SFRMIHOSTNAME=-Djava.rmi.server.hostname="&SFSEVERHOSTNAME"
 
 rem -------------------End user properties-------------------------
