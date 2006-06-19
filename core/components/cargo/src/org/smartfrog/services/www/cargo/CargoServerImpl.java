c568ffdb606019aaca4e45038f2627ba564c7c91
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2006-06-19 16:26:37 +0000

    minor tweaks to something that is still broken

--- core/components/cargo/src/org/smartfrog/services/www/cargo/CargoServerImpl.java
+++ core/components/cargo/src/org/smartfrog/services/www/cargo/CargoServerImpl.java
@@ -533,14 +533,7 @@ public class CargoServerImpl extends PrimImpl implements CargoServer, Runnable {
     }
 
     /**
-     * When an object implementing interface <code>Runnable</code> is used to
-     * create a thread, starting the thread causes the object's <code>run</code>
-     * method to be called in that separately executing thread.
-     * <p/>
-     * The general contract of the method <code>run</code> is that it may take
-     * any action whatsoever.
-     *
-     * @see Thread#run()
+     * {@inheritDoc}
      */
     public void run() {
         try {
