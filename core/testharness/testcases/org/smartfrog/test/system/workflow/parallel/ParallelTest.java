a4a3b78fa7b383abb323c7c219a627a65b64d77a
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2006-10-31 16:43:57 +0000

    Proof that Parallel is not working.

---
+++ core/testharness/testcases/org/smartfrog/test/system/workflow/parallel/ParallelTest.java
@@ -0,0 +1,50 @@
+/** (C) Copyright 2006 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+package org.smartfrog.test.system.workflow.parallel;
+
+import org.smartfrog.test.DeployingTestBase;
+import org.smartfrog.services.assertions.TestBlock;
+import org.smartfrog.sfcore.prim.Prim;
+
+/**
+ * @author Ashish Awasthi
+ * Date: 02-Jun-2004
+ */
+public class ParallelTest extends DeployingTestBase {
+    protected static final String FILES = "org/smartfrog/test/system/workflow/parallel/";
+
+    public ParallelTest(String s) {
+        super(s);
+    }
+    public void testEmptyParallel() throws Throwable {
+        application=deployExpectingSuccess(FILES +"testEmptyParallel.sf","testEmptyParallel");
+        TestBlock block=(TestBlock)application;
+        expectSuccessfulTermination(block);
+    }
+
+    public void testSimpleParallel() throws Throwable {
+        application = deployExpectingSuccess(FILES + "testSimpleParallel.sf", "testSimpleParallel");
+        TestBlock block = (TestBlock) application;
+        expectAbnormalTermination(block);
+        Prim action=block.getAction();
+        Prim toggle=(Prim) ((Prim) block).sfResolve("toggle");
+        assertAttributeEquals(toggle,"value",true);
+    }
+}
