b3b454f63fba6fbd58b5da402b1b6d0d4d87db9a
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-11-22 14:22:21 +0000

    For java 1.5 and to enable JMX remote agent add: -Dcom.sun.management.jmxremote

--- trunk/core/smartfrog/bin/sfDaemon.bat
+++ trunk/core/smartfrog/bin/sfDaemon.bat
@@ -35,6 +35,7 @@ IF NOT "%1"=="" GOTO start
 
 :end
 @echo off
+rem for JMX remote agent add: -Dcom.sun.management.jmxremote 
 %SFJVM% %SFCMDPARAMETERS% -Dorg.smartfrog.sfcore.processcompound.sfProcessName=rootProcess org.smartfrog.SFSystem %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 endlocal
