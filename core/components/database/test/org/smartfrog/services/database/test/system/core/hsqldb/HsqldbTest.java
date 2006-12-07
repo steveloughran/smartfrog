91db3b611e6056a47172f65628ee9edc6e004114
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2006-12-07 17:16:00 +0000

    renaming

---
+++ core/components/database/test/org/smartfrog/services/database/test/system/core/hsqldb/HsqldbTest.java
@@ -0,0 +1,32 @@
+package org.smartfrog.services.database.test.system.core.hsqldb;
+
+import org.smartfrog.test.DeployingTestBase;
+import org.smartfrog.services.assertions.TestBlock;
+
+/**
+ */
+public class HsqldbTest extends DeployingTestBase {
+
+    public HsqldbTest(String name) {
+        super(name);
+    }
+
+
+
+    public void testTableManipulationTest() throws Throwable {
+        application = deployExpectingSuccess(
+                "/org/smartfrog/services/database/test/system/core/hsqldb/TableManipulationTest.sf",
+                "TableManipulationTest");
+        TestBlock block = (TestBlock) application;
+        expectSuccessfulTermination(block);
+    }
+
+
+    public void testConnectionOpenTestt() throws Throwable {
+        application = deployExpectingSuccess(
+                "/org/smartfrog/services/database/test/system/core/hsqldb/ConnectionOpenTest.sf",
+                "ConnectionOpenTest");
+        TestBlock block = (TestBlock) application;
+        expectSuccessfulTermination(block);
+    }
+}
