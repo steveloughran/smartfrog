b9c23600411c32e470e69cb5e38355cf3882141c
sfpcg <sfpcg@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2006-12-19 16:58:26 +0000

    completed functionality

--- core/smartfrog/src/org/smartfrog/sfcore/languages/sf/functions/Ref.java
+++ core/smartfrog/src/org/smartfrog/sfcore/languages/sf/functions/Ref.java
@@ -3,6 +3,7 @@ package org.smartfrog.sfcore.languages.sf.functions;
 
 import org.smartfrog.sfcore.common.SmartFrogFunctionResolutionException;
 import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.sfcore.reference.ReferenceResolver;
 
 /**
  * Defines the unary operator not that negates its boolean parameter.
@@ -19,7 +20,7 @@ public class Ref extends BaseFunction {
      * @throws SmartFrogFunctionResolutionException if any error occurs in evalatnig the reference or resolving it
      */
     protected Object doFunction() throws SmartFrogFunctionResolutionException {
-    Object val;
+    Object val = null;
     Reference r;
     try {
         try {
@@ -40,7 +41,11 @@ public class Ref extends BaseFunction {
         val = r;
         } else {
         r.setEager(true);
-        val = null; //component.sfResolve(r);
+
+        if (rr != null)
+           val = rr.sfResolve(r);
+        else if (rrr != null)
+           val = rrr.sfResolve(r);
         }
     } catch (Exception e) {
         throw new SmartFrogFunctionResolutionException("Error resolving Ref function" , e);
