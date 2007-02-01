e7620cd8ed921c2b07d7944faa951a72517b9108
julgui <julgui@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-02-01 14:43:44 +0000

    More user info added to the files.

--- trunk/core/smartfrog/bin/setSFDefaultProperties.bat
+++ trunk/core/smartfrog/bin/setSFDefaultProperties.bat
@@ -1,11 +1,22 @@
 @echo off
 
+rem INFO
+rem JVMARGS are declared using -J token in the command line
+rem e.g. -J "-Djava.library.path=/libs -Xmx400M"
+rem e.g. -J "-Djava.library.path=/libs" -J -Xmx400M
+
 rem ------------------------------------------------------
 rem SF ENV PROPERTIES  - Please edit with your preferences
 rem ------------------------------------------------------
 
-rem -- User libs directory --
+rem -- User libs directories --
 rem set SFUSERHOME=%SFHOME%\mylibs
+rem set SFUSERHOME1=%SFHOME%\mylibs1
+rem set SFUSERHOME2=%SFHOME%\mylibs2
+rem set SFUSERHOME3=%SFHOME%\mylibs3
+rem set SFUSERHOME4=%SFHOME%\mylibs4
+rem set SFUSERCLASSPATH=%SFHOME%\myotheruserlibs
+
 rem -- server hostname for multihomed machines
 rem set SFSERVERHOSTNAME=localhost
 
