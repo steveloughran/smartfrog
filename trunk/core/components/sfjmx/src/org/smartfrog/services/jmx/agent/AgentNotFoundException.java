bf9ff897a40dbb5ef06d3c545ac05283f9fd5b21
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-02-05 07:21:19 +0000

    JMX implementation - mx4j libraries

---
+++ trunk/core/components/sfjmx/src/org/smartfrog/services/jmx/agent/AgentNotFoundException.java
@@ -0,0 +1,90 @@
+/** (C) Copyright 1998-2005 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.smartfrog.services.jmx.agent;
+
+import java.io.Serializable;
+
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.sfcore.common.SmartFrogException;
+
+/**
+ *  Title: sfJMX Description: JMX-based Management Framework for SmartFrog
+ *  Applications Copyright: Copyright (c) 2001 Company: Hewlett Packard
+ *
+ *@title          sfJMX
+ *@description    JMX-based Management Framework for SmartFrog Applications
+ *@company        Hewlett Packard
+ *
+ *@version        1.0
+ */
+
+public class AgentNotFoundException extends Exception implements Serializable {
+
+    public Object remoteAgentAddress=null;
+    /** The source that was trying to resolve the remoteAgent */
+    public Reference source = null;
+
+    public String extraInfo = null;
+
+
+    /**
+     *  Constructor for the AgentNotFoundException object
+     */
+    public AgentNotFoundException() { }
+
+
+    /**
+     *  Constructor for the AgentNotFoundException object
+     *
+     *@param  s  Description of the Parameter
+     */
+
+
+    public AgentNotFoundException(Object remoteAgentAddress, String reason) {
+        this(remoteAgentAddress, null, reason);
+    }
+
+
+    public AgentNotFoundException (Object remoteAgentAddress, Reference source,
+                               String reason) {
+      this(remoteAgentAddress, null, reason, null);
+  }
+
+  public AgentNotFoundException (Object remoteAgentAddress, Reference source,
+                             String reason, String extraInfo) {
+    super(reason);
+    this.remoteAgentAddress = remoteAgentAddress;
+    this.source = source;
+    this.extraInfo = extraInfo;
+}
+
+
+    /** Returns a string representation of the Agent Not Found exception
+     *
+     * @return reason source and agentAdd of exception */
+    public String toString() {
+      return
+        (getMessage() == null ? "\n nAgentNotFoundException: Unknown" : "\nAgentNotFoundException: " +getMessage())
+        + (source == null || source.size() == 0 ? "" : "\n  source: " + source.toString())
+        + (remoteAgentAddress == null ? "" : "\n  remoteAgentAddress: " + remoteAgentAddress.toString()+ ", class: "+ remoteAgentAddress.getClass())
+        + (extraInfo == null ? "" : "\n  extraInfo: " + extraInfo);
+      }
+}
