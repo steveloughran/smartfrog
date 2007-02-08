7541b701a090105e76e29481c072837bec33ca65
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-02-08 12:28:10 +0000

    moving into the OSS build  -ivy.xml file  -copyright headers present and dated 2007  -optimize imports  -layout source  -use logging more for errors  -less exception print and drop, more rethrow

--- trunk/core/components/quartz/src/org/smartfrog/services/quartz/scheduler/SchedulerImpl.java
+++ trunk/core/components/quartz/src/org/smartfrog/services/quartz/scheduler/SchedulerImpl.java
@@ -1,95 +1,86 @@
-/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+/** (C) Copyright 2007 Hewlett-Packard Development Company, LP
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-For more information: www.smartfrog.org
+ For more information: www.smartfrog.org
 
-*/
+ */
 
 package org.smartfrog.services.quartz.scheduler;
-
-import java.rmi.RemoteException;
-
-import org.smartfrog.sfcore.common.SmartFrogException;
-
-import org.smartfrog.sfcore.prim.Prim;
-import org.smartfrog.sfcore.prim.TerminationRecord;
-import org.smartfrog.sfcore.prim.PrimImpl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerFactory;
 import org.quartz.impl.StdSchedulerFactory;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+
+import java.rmi.RemoteException;
 
 /**
- *  Basic example component.
- *  The Counter component (in components.sf) is a basic primitive component so
- *  its component description class CounterImpl extends PrimImpl (the base
- *  class for all the deployed components) which provides the default lifecycle
- *  template methods for a primitive component.
- *  Although PrimImpl itself implements Prim (the base interface for all the
- *  deployed components) CounterImpl also implements Prim because it is
- *  necessary for RMI that component also does so; the rmic compiler will
- *  otherwise not behave correctly.
- *  The CounterImpl class needs to be prepared for RMI for remote deployment
- *  This is done by creating and compiling the stubs and skeletons using the
- *  rmic compiler.
- *  This class is included in rmitargets that is read by the rmic compiler.
+ * Starts the quartz scheduler on startup
  */
 public class SchedulerImpl extends PrimImpl implements Prim {
-  
-       	public Scheduler sched = null;	
-		
-	Log log = LogFactory.getLog(SchedulerImpl.class);
+
+    private Scheduler scheduler = null;
+
+    private static final Log log = LogFactory.getLog(SchedulerImpl.class);
 
     public SchedulerImpl() throws RemoteException {
     }
 
-    
-    public synchronized void sfDeploy() throws SmartFrogException,
-    RemoteException {
-            super.sfDeploy();
-    }
 
-   
     public synchronized void sfStart() throws SmartFrogException,
-    RemoteException {
+            RemoteException {
         super.sfStart();
-	try {
+        try {
 
-		log.info("------- Initializing ----------------------");
+            log.info("Initializing");
 
-	SchedulerFactory schedFact = new StdSchedulerFactory();
+            SchedulerFactory schedFact = new StdSchedulerFactory();
 
-	sched = schedFact.getScheduler();
-	log.info("------- Initialization Complete -----------");
-       
-	
-	sched.start();
-	log.info("------- Started Scheduler -----------------");
-	System.out.println("Quartz Started");
-	} catch (Exception ex) {}
+            scheduler = schedFact.getScheduler();
+            log.info("Initialization Complete");
+            scheduler.start();
+            log.info("Started Scheduler");
+        } catch (Exception ex) {
+            throw SmartFrogException.forward(ex);
+        }
     }
 
-    public synchronized void sfTerminateWith(TerminationRecord t) {
-	    try {
-            log.info("------- Shutting Down ---------------------");
-		sched.shutdown(true);
-		log.info("------- Shutdown Complete -----------------");
-	    } catch (Exception ex) {}
-	    super.sfTerminateWith(t);
+    public  void sfTerminateWith(TerminationRecord t) {
+        synchronized(this) {
+            try {
+                if(scheduler!=null) {
+                    log.info("Shutting Down");
+                    scheduler.shutdown(true);
+                    log.info("Shutdown Complete");
+                }
+            } catch (Exception ex) {
+                log.error("when shutting down the schedulur",ex);
+            } finally {
+                scheduler=null;
+            }
+        }
+        super.sfTerminateWith(t);
+    }
+
+    public Scheduler getScheduler() {
+        return scheduler;
     }
 }
