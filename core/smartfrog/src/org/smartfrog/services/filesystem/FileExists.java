f03d1f356904f2da2108a29a765c634c2ff0da1a
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2007-02-20 17:38:35 +0000

    New FileExists component checks for a file existing as its condition. You can set a minimum file size too.

---
+++ core/smartfrog/src/org/smartfrog/services/filesystem/FileExists.java
@@ -0,0 +1,15 @@
+package org.smartfrog.services.filesystem;
+
+import org.smartfrog.sfcore.workflow.conditional.Condition;
+
+/**
+
+ */
+public interface FileExists extends Condition {
+
+    /**
+     * {@value}
+     */
+    String ATTR_MIN_SIZE="minSize";
+
+}
---
+++ core/smartfrog/src/org/smartfrog/services/filesystem/FileExistsImpl.java
@@ -0,0 +1,49 @@
+package org.smartfrog.services.filesystem;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.PrimImpl;
+
+import java.rmi.RemoteException;
+import java.io.File;
+
+/**
+
+ */
+public class FileExistsImpl extends FileUsingComponentImpl implements FileExists {
+
+    private long minSize=-1;
+
+    public FileExistsImpl() throws RemoteException {
+    }
+
+
+
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws org.smartfrog.sfcore.common.SmartFrogException
+     *                                  failure while starting
+     * @throws java.rmi.RemoteException In case of network/rmi error
+     */
+    public synchronized void sfStart()
+        throws SmartFrogException, RemoteException {
+        super.sfStart();
+        //set up all the filename bindings
+        bind(true,null);
+        minSize=sfResolve(ATTR_MIN_SIZE,minSize,true);
+    }
+
+    /**
+     * Evaluate the condition.
+     *
+     * @return true if it is successful, false if not
+     * @throws RemoteException for network problems
+     * @throws SmartFrogException for any other problem
+     */
+    public boolean evaluate() throws RemoteException, SmartFrogException {
+        return getFile().exists() &&
+            (minSize < 0 || getFile().length() >= minSize);
+    }
+}
