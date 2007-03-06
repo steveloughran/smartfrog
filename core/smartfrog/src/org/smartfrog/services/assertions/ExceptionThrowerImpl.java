f48eef67febe7e3ba3520ded76eb1f261f28b1b2
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2007-03-06 17:39:36 +0000

    Exception thrower can throw exceptions in different places now

--- core/smartfrog/src/org/smartfrog/services/assertions/ExceptionThrowerImpl.java
+++ core/smartfrog/src/org/smartfrog/services/assertions/ExceptionThrowerImpl.java
@@ -20,7 +20,10 @@
 package org.smartfrog.services.assertions;
 
 import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.prim.Liveness;
+import org.smartfrog.sfcore.prim.TerminationRecord;
 import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.common.SmartFrogLivenessException;
 import org.smartfrog.services.assertions.ExceptionThrower;
 
 import java.rmi.RemoteException;
@@ -33,13 +36,74 @@ import java.lang.reflect.InvocationTargetException;
 
 public class ExceptionThrowerImpl extends PrimImpl implements ExceptionThrower {
 
+    private boolean throwOnStartup;
+    private boolean throwOnDeploy;
+    private boolean throwOnPing;
+    private String classname;
+    private String message;
+
     public ExceptionThrowerImpl() throws RemoteException {
     }
 
+
+    /**
+     * Called after instantiation for deployment purposes. Heart monitor is started and if there is a parent the
+     * deployed component is added to the heartbeat. Subclasses can override to provide additional deployment behavior.
+     *
+     * @throws SmartFrogException error while deploying
+     * @throws RemoteException    In case of network/rmi error
+     */
+    public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
+        super.sfDeploy();
+        classname = sfResolve(ATTR_CLASSNAME, "", true);
+        message = sfResolve(ATTR_MESSAGE, "", true);
+        throwOnStartup = sfResolve(ATTR_THROW_ON_STARTUP, false, true);
+        throwOnDeploy = sfResolve(ATTR_THROW_ON_DEPLOY, false, true);
+        throwOnPing = sfResolve(ATTR_THROW_ON_PING, false, true);
+        if(throwOnDeploy) {
+            raiseException(classname, message);
+        }
+    }
+
+    /**
+     * Startup may raise an exception if that is required from the component settings
+     * @throws SmartFrogException
+     * @throws RemoteException
+     */
     public synchronized void sfStart() throws SmartFrogException, RemoteException {
         super.sfStart();
-        String classname=sfResolve(ATTR_CLASSNAME,"",true);
-        String message= sfResolve(ATTR_MESSAGE, "", false);
+        if (throwOnStartup) {
+            raiseException(classname, message);
+        }
+    }
+
+
+    /**
+     * Liveness call in to check if this component is still alive.
+     * @param source source of call
+     * @throws SmartFrogLivenessException component is terminated
+     * @throws RemoteException            for consistency with the {@link Liveness} interface
+     */
+    public void sfPing(Object source) throws SmartFrogLivenessException, RemoteException {
+        super.sfPing(source);
+        if (throwOnStartup) {
+            try {
+                raiseException(classname, message);
+            } catch (SmartFrogException e) {
+                throw (SmartFrogLivenessException) SmartFrogLivenessException.forward(e);
+            }
+        }
+    }
+
+    /**
+     * Raise an exception
+     * @param classname name of the class to use
+     * @param message message to raise
+     * @throws RemoteException network problems or this exception was asked for
+     * @throws SmartFrogException which can be the exception, or wrap something inside
+     * @throws RuntimeException if that was asked for
+     */
+    protected void raiseException(String classname, String message) throws RemoteException, SmartFrogException {
         Throwable instance;
         try {
             Class eClass = getClass().getClassLoader().loadClass(classname);
@@ -73,8 +137,6 @@ public class ExceptionThrowerImpl extends PrimImpl implements ExceptionThrower {
             throw (RemoteException) instance;
         }
         throw SmartFrogException.forward(instance);
-
-
     }
 
 
