2fc3b6c2203211d40d17d7361059664233983aa7
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2007-03-07 12:06:12 +0000

    SFOS-83 let Parallel behaviour on abnormal child termination be controllable
    Lots of concurrency tests; failing children, children failing on startup, etc.

--- core/smartfrog/src/org/smartfrog/sfcore/workflow/combinators/Parallel.java
+++ core/smartfrog/src/org/smartfrog/sfcore/workflow/combinators/Parallel.java
@@ -20,23 +20,24 @@ For more information: www.smartfrog.org
 
 package org.smartfrog.sfcore.workflow.combinators;
 
-import java.rmi.RemoteException;
-import java.util.Enumeration;
-import java.util.Vector;
-import java.util.NoSuchElementException;
-
+import org.smartfrog.sfcore.common.Context;
+import org.smartfrog.sfcore.common.CreateNewChildThread;
+import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.common.SmartFrogException;
 import org.smartfrog.sfcore.common.SmartFrogRuntimeException;
-import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
-import org.smartfrog.sfcore.common.CreateNewChildThread;
-import org.smartfrog.sfcore.common.SmartFrogCoreKeys;
 import org.smartfrog.sfcore.componentdescription.ComponentDescription;
 import org.smartfrog.sfcore.compound.Compound;
+import org.smartfrog.sfcore.logging.LogSF;
 import org.smartfrog.sfcore.prim.Prim;
 import org.smartfrog.sfcore.prim.TerminationRecord;
 import org.smartfrog.sfcore.reference.Reference;
-import org.smartfrog.sfcore.workflow.eventbus.EventCompoundImpl;
 import org.smartfrog.sfcore.utils.ComponentHelper;
+import org.smartfrog.sfcore.workflow.eventbus.EventCompoundImpl;
+
+import java.rmi.RemoteException;
+import java.util.Enumeration;
+import java.util.NoSuchElementException;
+import java.util.Vector;
 
 /**
  * Parallel is a modified compound which differs in that the sub-components
@@ -56,18 +57,26 @@ import org.smartfrog.sfcore.utils.ComponentHelper;
  */
 public class Parallel extends EventCompoundImpl implements Compound {
 
-    private static final Reference asynchCreateChildRef = new Reference ("asynchCreateChild");
+    public static final String ATTR_ASYNCH_CREATE_CHILD = "asynchCreateChild";
+    private static final Reference asynchCreateChildRef = new Reference (ATTR_ASYNCH_CREATE_CHILD);
     /** {@value} */
     public static final String ATTR_TERMINATE_IF_EMPTY = "terminateOnEmptyDeploy";
-    private static Reference terminateIfEmptyRef = new Reference(ATTR_TERMINATE_IF_EMPTY);
+    /** {@value} */
+    public static final String ATTR_TERMINATE_IF_CHILD_TERMINATES_ABNORMAL = "terminateOnAbnormalChildTermination";
+    /** {@value} */
+    public static final String ATTR_TERMINATE_IF_CHILD_DEPLOYS_ABNORMAL = "terminateOnAbnormalChildDeploy";
+    private static final Reference terminateIfEmptyRef = new Reference(ATTR_TERMINATE_IF_EMPTY);
     private boolean asynchCreateChild=false;
     private boolean terminateIfEmpty=false;
-    private Vector asynchChildren = null;
+    private boolean terminateOnAbnormalChildTermination=true;
+    private boolean terminateOnAbnormalChildDeploy = true;
+    private Vector asynchChildren;
+    private Vector results;
 
     /**
      * Constructs Parallel.
      *
-     * @throws java.rmi.RemoteException In case of network or RMI failure.
+     * @throws RemoteException In case of network or RMI failure.
      */
     public Parallel() throws RemoteException {
         super();
@@ -82,8 +91,16 @@ public class Parallel extends EventCompoundImpl implements Compound {
      */
     public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
         super.sfDeploy();
+        asynchChildren = new Vector(0);
         asynchCreateChild = sfResolve(asynchCreateChildRef,asynchCreateChild,false);
         terminateIfEmpty = sfResolve(terminateIfEmptyRef, terminateIfEmpty, false);
+        terminateOnAbnormalChildTermination = sfResolve(ATTR_TERMINATE_IF_CHILD_TERMINATES_ABNORMAL,
+                terminateOnAbnormalChildTermination, true);
+        terminateOnAbnormalChildDeploy = sfResolve(ATTR_TERMINATE_IF_CHILD_DEPLOYS_ABNORMAL,
+                terminateOnAbnormalChildDeploy, true);
+        if(!terminateOnAbnormalChildDeploy && !asynchCreateChild) {
+            sfLog().warn("The attribute "+ATTR_TERMINATE_IF_CHILD_DEPLOYS_ABNORMAL+" is only valid with "+ATTR_ASYNCH_CREATE_CHILD);
+        }
     }
 
     /**
@@ -96,34 +113,53 @@ public class Parallel extends EventCompoundImpl implements Compound {
         super.sfStart();
         TerminationRecord terminationRecord = null;
         if (!actions.isEmpty()) {
-// let any errors be thrown and caught by SmartFrog for abnormal termination  - including empty actions
+            // let any errors be thrown and caught by SmartFrog for abnormal termination  - including empty actions
             try {
-                if (!asynchCreateChild){
-                    if (sfLog().isDebugEnabled()){sfLog().debug(" Parallel Synch");};
+                if (!asynchCreateChild) {
+                    sfLog().debug(" Parallel Synch");
                     synchCreateChildren();
                 } else {
-                    if (sfLog().isDebugEnabled()){sfLog().debug(" Parallel Asynch");};
+                    sfLog().debug(" Parallel Asynch");
                     asynchCreateChildren();
                 }
             } catch (Exception ex) {
-                if (sfLog().isErrorEnabled()){
-                  sfLog().error(sfCompleteNameSafe()+" - Failed to start sub-components ",ex);
+                if (sfLog().isErrorEnabled()) {
+                    sfLog().error(getName() + " - Failed to start sub-components ", ex);
                 }
                 terminationRecord = TerminationRecord
-                        .abnormal("Failed to start sub-components " + ex, name);
-                //sfTerminate(terminationRecord);
+                        .abnormal("Failed to start sub-components " + ex, getName(), ex);
             }
         } else {
             //no actions. Maybe terminate 
-            if(terminateIfEmpty) {
+            if (terminateIfEmpty) {
                 terminationRecord = new TerminationRecord(TerminationRecord.NORMAL,
-                        "Parallel component is empty",name);
+                        "Parallel component is empty", getName());
             }
         }
-        if (terminationRecord!=null) {
-            new ComponentHelper(this).targetForWorkflowTermination(terminationRecord);
+        if (terminationRecord != null) {
+            //something went wrong; act on it
+            maybeTerminate(terminationRecord, true);
         }
 
+    }
+
+
+    /**
+     * A factoring out of all handling for startup failures
+     * Log the event or queue for termination, depending upon the fail parameter
+     * @param terminationRecord non null termination record
+     * @param fail set to true to terminate; false to log
+     * @return true if we started a terminator thread
+     */
+    private void maybeTerminate(TerminationRecord terminationRecord, boolean fail) {
+        logChildDeployFailure(terminationRecord);
+        if (fail) {
+            new ComponentHelper(this).targetForWorkflowTermination(terminationRecord);
+        }
+    }
+
+    private void logChildDeployFailure(TerminationRecord terminationRecord) {
+        sfLog().err("Failed to start child", null, terminationRecord);
     }
 
 
@@ -133,21 +169,22 @@ public class Parallel extends EventCompoundImpl implements Compound {
      * @throws SmartFrogException
      */
     protected void asynchCreateChildren() throws RemoteException, SmartFrogException {
-            asynchChildren = new Vector();
-            actionKeys = actions.keys();
-            try {
-                while (actionKeys.hasMoreElements()) {
-                    Object key = actionKeys.nextElement();
-                    ComponentDescription act = (ComponentDescription) actions.get(key);
-                    //asynchChildren.add();
-                    Thread thread = new CreateNewChildThread(key,this,act, null,this);
-                    asynchChildren.add(thread);
-                    thread.start();
-                    if (sfLog().isDebugEnabled()) sfLog().debug("Creating "+key);
-                }
-            } catch (NoSuchElementException ignored){
-               throw new SmartFrogRuntimeException ("Found no children to deploy",this);
+        int size=actions.size();
+        asynchChildren = new Vector(size);
+        results = new Vector(size);
+        actionKeys = actions.keys();
+        try {
+            while (actionKeys.hasMoreElements()) {
+                Object key = actionKeys.nextElement();
+                ComponentDescription act = (ComponentDescription) actions.get(key);
+                ParallelWorker thread = new ParallelWorker(this, key, act, null);
+                asynchChildren.add(thread);
+                if (sfLog().isDebugEnabled()) sfLog().debug("Creating " + key);
+                thread.start();
             }
+        } catch (NoSuchElementException ignored) {
+            throw new SmartFrogRuntimeException(ERROR_NO_CHILDREN_TO_DEPLOY, this);
+        }
     }
 
 
@@ -155,53 +192,196 @@ public class Parallel extends EventCompoundImpl implements Compound {
      * If normal termination, Parallel behaviour is to terminate
      * that component but leave the others running if it is the last -
      * terminate normally. if an erroneous termination -
-     * terminate immediately passing on the error
+     * act on the value of {@link #terminateOnAbnormalChildTermination}
      *
-     *
-     * @param status exit record of the component
-     * @param comp   child component that is terminating
+     * There's a lot of complex logic in here, as it has to deal with both sync and async deployments,
+     * 
+     * @param record exit record of the component
+     * @param child   child component that is terminating
      * @return true if the termination event is to be forwarded up the chain.
      */
-    protected boolean onChildTerminated(TerminationRecord status, Prim comp) {
-        boolean forward = true;
-        if (status.isNormal()) {
-            try {
-                sfRemoveChild(comp);
-                forward = !sfChildren().hasMoreElements();
-            } catch (Exception e) {
-                if (sfLog().isErrorEnabled()) {
-                    sfLog().error(sfCompleteNameSafe() + " - error handling child termination ", e);
-                }
-                sfTerminate(TerminationRecord.abnormal("error handling child termination " + e, name));
-                forward = false;
+    protected boolean onChildTerminated(TerminationRecord record, Prim child) {
+        boolean forward = false;
+        boolean normalRecord = record.isNormal();
+        try {
+            sfRemoveChild(child);
+        } catch (SmartFrogRuntimeException e) {
+            //this exception gets ignored by CompoundImpl's implementation, but there is
+            //a risk something changes in the future
+            sfLog().warn("Ignoring error on child termination ", e);
+        } catch (RemoteException e) {
+            //here's a network error; very unusual and merits propagating
+            sfLog().error("Error handling child termination ", e);
+            //failure to remove the child is always a problem
+            if(normalRecord) {
+                sfTerminate(TerminationRecord.abnormal("error removing the child", getName(), e));
+                //bail out right now -this simplifies the logic slightly (currently).
+                return false;
+            } else {
+                //its an error that should be forwarded. Again, bail out early
+                return true;
             }
+        }
+        boolean lastChild = !hasActiveChildren();
+        //we forward if this is the last child, or it is an abnormal and we want to forward it
+
+        if(normalRecord ) {
+            forward = lastChild;
+        } else {
+            //failure
+            if(terminateOnAbnormalChildTermination) {
+                forward=true;
+            } else {
+                //we are here if this is a fault we may want to ignore
+                //if this is the last child, we actually raise a new term record
+                //after ignoring this one
+                ignoringChildTermination(record, child);
+                forward=false;
+                if(lastChild) {
+                    //trigger a normal termination, even though this component terminated abnormally
+                    sfTerminate(TerminationRecord.normal(getName()));
+                }
+            }
+        }
+        if (!normalRecord && !forward) {
+            //we want to to ignore this fault
+            ignoringChildTermination(record, child);
         }
         return forward;
     }
 
     /**
-     * Cancels all remaining createChild threads
+     * Cancels all remaining createChild threads. This will trigger callbacks to the
      *
      * @param status Termination  Record
      */
     public synchronized void sfTerminateWith(TerminationRecord status) {
 
-        /* unregister from all remote registrations */
-        if  (asynchChildren!=null){
-            for (Enumeration e = asynchChildren.elements(); e.hasMoreElements(); ) {
-                CreateNewChildThread t = (CreateNewChildThread)e.nextElement();
-                boolean  sfSyncTerminate = false;
+        // unregister from all remote registrations
+        if (asynchChildren != null) {
+            for (Enumeration e = asynchChildren.elements(); e.hasMoreElements();) {
+                ParallelWorker worker = (ParallelWorker) e.nextElement();
                 try {
-                    sfSyncTerminate = sfResolve(SmartFrogCoreKeys.SF_SYNC_TERMINATE, sfSyncTerminate, false);
-                } catch (Exception sfrex){
-                    //Ignore
-                }
-                try {
-                    t.cancel(sfSyncTerminate,true);
+                    worker.cancel(sfSyncTerminate, true);
                 } catch (Exception ignored) {
+                    sfLog().ignore("when canceling", ignored);
                 }
             }
         }
         super.sfTerminateWith(status);
+    }
+
+
+    /**
+     * This gets called when we ignore a child termination; the base class just logs it at the
+     * err level. Override if you want more behaviour, such as saving the record somewhere
+     * @param record the termination record
+     * @param comp the component that has just terminated.
+     */
+    protected void ignoringChildTermination(TerminationRecord record, Prim comp) {
+        sfLog().err("Ignoring child termination", null, record);
+    }
+
+
+    /**
+     * Called in the worker thread, this method logs that the worker has finished by
+     * removing it from our child list, which is then optionally used to trigger a failure.
+     * @param worker
+     */
+    protected synchronized void workerFinished(ParallelWorker worker) {
+        //and remove from our child list
+        asynchChildren.removeElement(worker);
+        if(sfLog().isDebugEnabled()) {
+            sfLog().debug("Worker finished " + worker);
+        }
+        if (!isWorkflowTerminating()) {
+            //this is only run if we arent actually shutting down, because at that point
+            //it is needless
+            try {
+                results.add(worker.get(0));
+                //if we get here, success.
+            } catch (InterruptedException ignored) {
+                sfLog().ignore("Worker failed" + worker, ignored);
+            } catch (SmartFrogException ex) {
+                //failure: add the exception to the results
+                results.add(ex);
+                //this fault means that we failed to start up
+                //create a term record
+                TerminationRecord terminationRecord = TerminationRecord
+                        .abnormal("Failure while starting sub-components " + ex, getName(), ex);
+                //and maybe terminate
+                // This may be called more than once, but appears to be harmless in this case.
+
+                maybeTerminate(terminationRecord, terminateOnAbnormalChildDeploy);
+                if(!terminateOnAbnormalChildDeploy) {
+                    //at this point we had an error, but we are not finishing. This may leave us with
+                    //no children, which is itself a cause for termination. What we cannot do is rely on
+                    //!sfChildren().hasMoreElements(); as a valid test, as there may be active threads
+                    //that have not got there yet.
+                    //we have to look at both
+                    if(!hasActiveChildren()) {
+                        //ok, ready to go
+                        new ComponentHelper(this).targetForWorkflowTermination(
+                                TerminationRecord.normal(getName()));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Test for active children.
+     *  What we cannot do is rely on {@link #sfChildren()} not being empty, as there may be active threads
+     * that have not got there yet...we have to look at the active thread count and return false if there
+     * are threads there
+     * @return
+     */
+    private synchronized boolean hasActiveChildren() {
+        return sfChildren().hasMoreElements() || !asynchChildren.isEmpty();
+    }
+
+    /**
+     * Worker thread tied to this component, that notifies by calling {@link Parallel#workerFinished(ParallelWorker)}
+     * after the thread is done.
+     */
+    private static class ParallelWorker extends CreateNewChildThread {
+
+        /** owner class */
+        private Parallel owner;
+
+
+        /**
+         * Creates a <tt>CreateNewChildThread</tt> that will upon running, execute the given <tt>CreateNewChild</tt>. If parent
+         * null then it will create a independent application but a deployer needs to be provided
+         *
+         * @param owner   parent component
+         * @param name     child name
+         * @param cmp      component description
+         * @param parms    Context
+         * @throws SmartFrogException if callable is null
+         */
+        private ParallelWorker(Parallel owner, Object name, ComponentDescription cmp, Context parms) throws SmartFrogException {
+            super(name, owner, cmp, parms, owner);
+            this.owner = owner;
+        }
+
+        /**
+         * Protected method invoked when this task transitions to state <tt>isDone</tt> .
+         * This implementation queues the event with the parent
+         */
+        protected void done() {
+            owner.workerFinished(this);
+        }
+
+
+        /**
+         * get the owner log instead of the core
+         *
+         * @return Logger implementing LogSF and Log
+         */
+        protected LogSF sfLog() {
+            return owner.sfLog();
+        }
+
     }
 }
