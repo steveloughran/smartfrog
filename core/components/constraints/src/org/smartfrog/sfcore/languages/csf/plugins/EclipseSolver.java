6a34534d06019644f199cabd1eea5208d12f2a8a
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-04-20 09:07:08 +0000

    Changes to incorporate allocation Eclipse logic

--- core/components/constraints/src/org/smartfrog/sfcore/languages/csf/plugins/EclipseSolver.java
+++ core/components/constraints/src/org/smartfrog/sfcore/languages/csf/plugins/EclipseSolver.java
@@ -1,10 +1,13 @@
 package org.smartfrog.sfcore.languages.csf.plugins;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 import org.smartfrog.sfcore.languages.csf.constraints.PrologSolver;
 
+import com.parctechnologies.eclipse.Atom;
 import com.parctechnologies.eclipse.CompoundTerm;
 import com.parctechnologies.eclipse.EclipseEngine;
 import com.parctechnologies.eclipse.EclipseEngineOptions;
@@ -37,16 +40,37 @@ public class EclipseSolver extends PrologSolver {
     	m_eclipse.compile(new File(prologFile));   	
     }
 
-    public void solveQuery(StringBuffer constraint) throws Exception {
+    public void runGoal(String constraint) throws Exception {
+    	m_eclipse.rpc(constraint.toString());
+    }
+
+    public void solveQuery(String constraint) throws Exception {
     	CompoundTerm ct = m_eclipse.rpc(constraint.toString());
     	m_results = (List)((CompoundTerm)ct.arg(1)).arg(2);
-    	
-    	// Destroy the Eclipse process
+    }
+    
+    public void destroy() throws Exception {
+    	//Destroy the Eclipse process
     	((EmbeddedEclipse) m_eclipse).destroy();
     }
     
     public Object getBinding(int var){
     	return m_results.get(var);
+    }
+
+    public Vector getResults(){
+    	Vector vec = new Vector();
+    	Iterator iter = m_results.iterator();
+    	while (iter.hasNext()){
+    	   CompoundTerm ct = (CompoundTerm) iter.next();
+    	   Vector vec1 = new Vector();
+    	   String vm = ((Atom)ct.arg(1)).functor();
+    	   String host = ((Atom)((CompoundTerm)ct.arg(2)).arg(1)).functor();
+    	   vec1.add(vm);
+    	   vec1.add(host);
+    	   vec.add(vec1);
+    	}
+    	return vec;
     }
 }
 
