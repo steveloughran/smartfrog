369e55b952fc429d65390dbb09d907777939a82f
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-07-26 10:10:57 +0000

    SFOS-315 add an IsPropertySet condition to look for a system property

---
+++ trunk/core/smartfrog/src/org/smartfrog/sfcore/workflow/conditional/conditions/IsPropertySet.java
@@ -0,0 +1,45 @@
+package org.smartfrog.sfcore.workflow.conditional.conditions;
+
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.workflow.conditional.Condition;
+import org.smartfrog.sfcore.common.SmartFrogException;
+
+import java.rmi.RemoteException;
+
+/** Condition that looks for a property being set */
+public class IsPropertySet extends PrimImpl implements Condition {
+
+
+    /** takes the string value of a property to look for: {@value} */
+    public static final String ATTR_PROPERTY = "property";
+
+
+    private String property;
+
+    public IsPropertySet() throws RemoteException {
+    }
+
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws SmartFrogException failure while starting
+     * @throws RemoteException In case of network/rmi error
+     */
+    public synchronized void sfStart()
+        throws SmartFrogException, RemoteException {
+        super.sfStart();
+        property = sfResolve(ATTR_PROPERTY, property, true);
+    }
+
+
+    /**
+     * Evaluate the condition.
+     *
+     * @return true if it is successful, false if not
+     */
+    public boolean evaluate() {
+        return System.getProperty(property)!=null;
+    }
+}
