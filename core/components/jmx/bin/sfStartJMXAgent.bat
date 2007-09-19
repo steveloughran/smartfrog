c35818ed47a59554c8d79d435424f9da9c41cc18
kumaravelg <kumaravelg@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-09-19 10:16:07 +0000

    fix

--- core/components/jmx/bin/sfStartJMXAgent.bat
+++ core/components/jmx/bin/sfStartJMXAgent.bat
@@ -4,9 +4,16 @@ setlocal
 if defined SFHOME goto continue1
   if exist "%cd%\sfStartJMXAgent.bat" cd ..
   set SFHOME=%cd%
+ cd bin
 :continue1
 
-call %SFHOME%\bin\setClassPath
+call "%SFHOME%\bin\setSFProperties"
 
-java com.hp.SmartFrog.SFSystem -h localhost -n sfJMXAgent %SFHOME%/bin/sfJMXAgent.sf -e
+rem call %SFHOME%\bin\setClassPath
+
+
+java org.smartfrog.SFSystem  -a \"sfJMXAgent\":DEPLOY:\"%SFHOME%/bin/sfJMXAgent.sf\"::localhost: -e
+
+rem java com.hp.SmartFrog.SFSystem -h localhost -n sfJMXAgent %SFHOME%/bin/sfJMXAgent.sf -e
+
 endlocal
