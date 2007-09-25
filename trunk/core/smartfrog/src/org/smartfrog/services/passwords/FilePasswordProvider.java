6a03369b75f0c50e04ac7cb57a8f6a5aa48226bd
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-09-25 23:21:23 +0000

    SFOS-332 replace duplicate password providers in ssh and net components with a shared one in services

---
+++ trunk/core/smartfrog/src/org/smartfrog/services/passwords/FilePasswordProvider.java
@@ -0,0 +1,83 @@
+/** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.smartfrog.services.passwords;
+
+import java.rmi.RemoteException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.nio.charset.Charset;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.services.filesystem.FileSystem;
+
+/**
+ * Implementation of PasswordProvider which reads the password from a text 
+ * file.
+ * @author Ashish Awasthi
+ */ 
+public class FilePasswordProvider extends PrimImpl implements Prim, 
+                                                    PasswordProvider     {
+
+    private String passwordFile = "";
+    private String password = "";
+    // attribute name for password file
+
+    /**
+     * The attribute of the password file: "{@value}"
+     */
+    public static final String PASSWORD_FILE="passwordFile";
+    
+    /**
+     * Constructs FilePasswordProvider object.
+     *
+     * @throws RemoteException in case of network/rmi error
+     */
+    public FilePasswordProvider() throws RemoteException {
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * @throws SmartFrogException If unable to get the password
+     * @throws RemoteException in case of network or RMI error
+     * @return a password
+     */
+    public String getPassword() throws SmartFrogException, RemoteException {
+        File file =
+            FileSystem.lookupAbsoluteFile(this,
+                PASSWORD_FILE,
+                null,
+                null,
+                true,
+                null);
+        try {
+            String contents =
+                FileSystem.readFile(file, Charset.defaultCharset()).toString();
+            return contents.trim();
+        }catch (IOException ioex) {
+                throw new SmartFrogException(ioex);
+        }
+    }
+}
---
+++ trunk/core/smartfrog/src/org/smartfrog/services/passwords/PasswordChecker.java
@@ -0,0 +1,101 @@
+/** (C) Copyright 2007 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+
+package org.smartfrog.services.passwords;
+
+import org.smartfrog.sfcore.prim.PrimImpl;
+import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.prim.TerminationRecord;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.utils.ComponentHelper;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * This component is here mainly to test password providers, but you can also
+ * use it to verify that a password meets min/max length values
+ */
+public class PasswordChecker extends PrimImpl implements Remote {
+
+    /** component attribute {@value} */
+    public static final String ATTR_PROVIDER = "passwordProvider";
+
+    /** component attribute {@value} */
+    public static final String ATTR_MIN_SIZE = "minSize";
+
+    /** component attribute {@value} */
+    public static final String ATTR_MAX_SIZE = "maxSize";
+
+    /** component attribute {@value} */
+    public static final String ATTR_VALUE = "password";
+
+    /** component attribute {@value} */
+    public static final String ATTR_EXPECTED = "expected";
+
+    /** component attribute {@value} */
+    public static final String ATTR_PUBLISH = "publish";
+
+
+    public PasswordChecker() throws RemoteException {
+    }
+
+
+    /**
+     * Can be called to start components. Subclasses should override to provide
+     * functionality Do not block in this call, but spawn off any main loops!
+     *
+     * @throws SmartFrogException failure while starting
+     * @throws RemoteException    In case of network/rmi error
+     */
+    public synchronized void sfStart()
+        throws SmartFrogException, RemoteException {
+        super.sfStart();
+
+        PasswordProvider provider =
+            (PasswordProvider) sfResolve(ATTR_PROVIDER, (Prim) null, true);
+        int minSize = sfResolve(ATTR_MIN_SIZE, -1, false);
+        int maxSize = sfResolve(ATTR_MAX_SIZE, -1, false);
+        String expected = sfResolve(ATTR_EXPECTED, (String) null, false);
+
+
+        String password = provider.getPassword();
+        if (password == null) {
+            throw new SmartFrogException("Null password returned");
+        }
+
+        if (sfResolve(ATTR_PUBLISH, false, false)) {
+            sfReplaceAttribute(ATTR_VALUE, password);
+        }
+        if (password.length() < minSize) {
+            throw new SmartFrogException("Password is shorter than minimum size");
+        }
+        if (maxSize > 0 && password.length() > maxSize) {
+            throw new SmartFrogException("Password is longer than maximum size");
+        }
+        if (expected != null && !password.equals(expected)) {
+            throw new SmartFrogException(
+                "Password does not match expected value");
+        }
+
+        new ComponentHelper(this).sfSelfDetachAndOrTerminate(TerminationRecord.normal(sfCompleteName()));
+
+    }
+}
