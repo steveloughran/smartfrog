84e87481ef8756a65efd1f0d204f029da272151a
julgui <julgui@users.noreply.github.com>
2007-10-12 10:53:09 +0000

    Added new env property to shell scripts: if SFSECURERESOURCES_OFF=ENABLED is defined in the enviroment then -Dorg.smartfrog.sfcore.security.secureResourcesOff=true is added to the CLI. http://jira.smartfrog.org/jira/browse/SFOS-486

--- core/smartfrog/bin/setSFSecurityProperties.bat
+++ core/smartfrog/bin/setSFSecurityProperties.bat
@@ -14,6 +14,8 @@ rem ------------------------------------------------------
 rem -- Security properties --
 set SFSECURITY=-Djava.security.policy=="%SFPRIVATE%\sf.policy" -Djava.security.manager -Dorg.smartfrog.sfcore.security.keyStoreName="%SFPRIVATE%\%SFHOSTNAME%\mykeys.st" -Dorg.smartfrog.sfcore.security.propFile="%SFPRIVATE%\%SFHOSTNAME%\SFSecurity.properties"
 
+if defined SFSECURERESOURCES_OFF set SFSECURITY=%SFSECURITY% -Dorg.smartfrog.sfcore.security.secureResourcesOff=true
+
 rem  -- default.ini and default.sf with security. These files need to be inside a jar file--
 set SFDEFAULTINI=-Dorg.smartfrog.iniFile=org/smartfrog/default.ini
 set SFDEFAULTSF=-Dorg.smartfrog.sfcore.processcompound.sfDefault.sfDefault=org/smartfrog/default.sf
