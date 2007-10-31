a9bd334993f54bbb3f2b6a941098bfcc2d79c39b
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-10-31 17:31:03 +0000

    SFOS-529  	 add component to run ant XML build files

--- trunk/core/components/ant/src/org/smartfrog/services/ant/AntBuild.java
+++ trunk/core/components/ant/src/org/smartfrog/services/ant/AntBuild.java
@@ -19,12 +19,72 @@
  */
 package org.smartfrog.services.ant;
 
+import java.rmi.Remote;
+
 /**
  *
  * Created 31-Oct-2007 14:23:52
  *
  */
 
+/*
+    antSchema extends Schema {
 
-public interface AntBuild {
+        //list of [name,value] pairs defining ant properties
+        properties extends OptionalVector;
+
+        //list of targets to run. If empty, the default target is executed
+        targets extends OptionalVector;
+
+
+        //this is an optional base directory. If it exists, then
+        //it is used as the base directory for the antfile, and for
+        //any directories in the directories list.
+        basedir extends OptionalFilenameType;
+
+        //the name of an ant file. If relative, it is resolved
+
+        buildfile extends FilenameType;
+
+
+        //a list of directories. Will be resolved relative to basedir, when
+        //relative resolution is required.
+        directories extends OptionalFilenameList;
+
+    }
+ */
+public interface AntBuild extends Remote {
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_PROPERTIES=Ant.ATTR_PROPERTIES;
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_TARGETS = "targets";
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_BASEDIR = Ant.ATTR_BASEDIR;
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_BUILDFILE = "buildfile";
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_DIRECTORIES = "directories";
+
+
+    /**
+     * SmartFrog attribute {@value}
+     */
+    String ATTR_GENERICANTFILE = "genericantfile";
+
+
 }
