3eb20283ed4aea30d77f564665ab3ff830f0d9e1
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2007-11-28 17:36:59 +0000

    SFOS-570 Add Restlet Client support

---
+++ trunk/core/components/ec2/src/org/smartfrog/services/restlet/client/RemoteRestletResource.java
@@ -0,0 +1,87 @@
+/** (C) Copyright 2007 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+package org.smartfrog.services.restlet.client;
+
+import org.smartfrog.services.www.HttpAttributes;
+
+/**
+ * Created 28-Nov-2007 17:23:26
+ */
+
+
+public interface RemoteRestletResource extends HttpAttributes {
+
+
+    /**
+     * SF attribute - {@value}
+     */
+    String ATTR_STARTACTIONS = "startActions";
+
+    /**
+     * SF attribute - {@value}
+     */
+    String ATTR_TERMINATEACTIONS = "terminateActions";
+
+    /**
+     * SF attribute - {@value}
+     */
+    String ATTR_LIVENESSACTIONS = "livenessActions";
+
+    /**
+     * SF attribute - {@value}
+     */
+    String ATTR_RESULTXPATH = "resultXPath";
+
+    /**
+     * SF constant {@value}
+     */
+    String GET= "get";
+
+    /**
+     * SF attribute - {@value}
+     */
+    String POST = "post";
+
+    /**
+     * SF constant {@value}
+     */
+    String PUT = "put";
+    /**
+     * SF constant {@value}
+     */
+    String DELETE = "delete";
+    /**
+     * SF constant {@value}
+     */
+    String OPTIONS = "options";
+    /**
+     * SF constant {@value}
+     */
+    String NO_AUTH = "none";
+    /**
+     * SF constant {@value}
+     */
+    String BASIC_AUTH = "basic";
+    /**
+     * SF constant {@value}
+     */
+    String AMAZON_AUTH = "amazon";
+
+}
