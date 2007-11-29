da9beed1fd5c02d1becf629f723680133feb3393
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2007-11-29 21:51:26 +0000

    SFOS-570 	 Add Restlet Client support

---
+++ core/components/ec2/src/org/smartfrog/services/restlet/client/RepresentationHelper.java
@@ -0,0 +1,85 @@
+/** (C) Copyright 2007 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+
+
+package org.smartfrog.services.restlet.client;
+
+import org.restlet.data.MediaType;
+import org.restlet.resource.Representation;
+
+/**
+ *
+ */
+public class RepresentationHelper {
+
+    private Representation data;
+
+    /**
+     * create an instance
+     * @param data the incoming data
+     */
+    public RepresentationHelper(Representation data) {
+        this.data = data;
+    }
+
+    /**
+     * Is this any text type?
+     * @return true if we think it is a text, XML or XHTML
+     */
+    public boolean isTextType() {
+        MediaType mt=data.getMediaType();
+        return isTextType(mt);
+    }
+
+    /**
+     * query media type
+     * @return true iff this is  XML
+     */
+    public boolean isXMLType() {
+        return isXMLType(data.getMediaType());
+    }
+
+    /**
+     * query media type
+     * @param mt media type
+     * @return true iff this is text or XML
+     */
+    public static boolean isTextType(MediaType mt) {
+        String maintype = mt.getMainType();
+        return "text".equals(maintype)
+                || mt.equals(MediaType.TEXT_ALL, true)
+                || isXMLType(mt);
+    }
+
+    /**
+     * query media type
+     * @param mt media type
+     * @return true iff this is  XML
+     */
+    public static boolean isXMLType(MediaType mt) {
+        return mt.equals(MediaType.APPLICATION_XML, true)
+                || mt.equals(MediaType.APPLICATION_ATOM_XML, true)
+                || mt.equals(MediaType.APPLICATION_XHTML_XML, true)
+                || mt.equals(MediaType.APPLICATION_RDF_XML, true)
+                || mt.equals(MediaType.TEXT_XML, true)
+                ;
+    }
+
+}
---
+++ core/components/ec2/src/org/smartfrog/services/restlet/client/RestletOperationException.java
@@ -0,0 +1,125 @@
+/** (C) Copyright 2007 Hewlett-Packard Development Company, LP
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ For more information: www.smartfrog.org
+
+ */
+
+
+package org.smartfrog.services.restlet.client;
+
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.restlet.resource.Representation;
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.prim.Prim;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class RestletOperationException extends SmartFrogException {
+
+    int status;
+    private String text;
+
+    /**
+     * Constructs a SmartFrogException with specified message.
+     *
+     * @param message exception message
+     */
+    public RestletOperationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a SmartFrogException with specified message and cause.
+     *
+     * @param message exception message
+     * @param cause   exception causing this exception
+     */
+    public RestletOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a SmartFrogException with specified message. Also initializes the
+     * exception context with component details.
+     *
+     * @param message  exception message
+     * @param sfObject The Component that has encountered the exception
+     */
+    public RestletOperationException(String message, Prim sfObject) {
+        super(message, sfObject);
+    }
+
+    /**
+     * Constructs a SmartFrogException with specified message. Also initializes the
+     * exception context with component details.
+     *
+     * @param message  message
+     * @param cause    exception causing this exception
+     * @param sfObject The Component that has encountered the exception
+     */
+    public RestletOperationException(String message,
+                                     Throwable cause, Prim sfObject) {
+        super(message, cause, sfObject);
+    }
+
+    /**
+     * Constructs a SmartFrogException with specified message. Also initializes
+     * the exception context with component details.
+     *
+     * @param message  message
+     * @param response the response in question
+     * @param sfObject The Component that has encountered the exception
+     */
+    public RestletOperationException(String message,
+                                     Response response, Prim sfObject) {
+        super(message, sfObject);
+        build(response);
+    }
+
+
+    /**
+     * Constructs a SmartFrogException with specified cause. Also initializes the
+     * exception context with component details.
+     *
+     * @param cause    cause of the exception
+     * @param sfObject The Component that has encountered the exception
+     */
+    public RestletOperationException(Throwable cause, Prim sfObject) {
+        super(cause, sfObject);
+    }
+
+    /**
+     * Build an exception from a response. Subclasses could do clever things
+     * here.
+     * @param response response text
+     */
+    protected void build(Response response) {
+        status=response.getStatus().getCode();
+        Representation data = response.getEntity();
+        RepresentationHelper rh=new RepresentationHelper(data);
+        if(rh.isTextType())
+        try {
+            text = data.getText();
+        } catch (IOException e) {
+            text=null;
+        }
+    }
+}
