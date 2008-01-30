b74abc67c8d89a09dd94e49c028ec232f84adef9
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-01-30 17:35:28 +0000

    SFOS-632  	 Locate all uses of new TerminationRecord and review them

--- core/testharness/testcases/org/smartfrog/test/process/DaemonTest.java
+++ core/testharness/testcases/org/smartfrog/test/process/DaemonTest.java
@@ -44,7 +44,7 @@ public class DaemonTest extends TestCase {
     }
 
     public void testStopDaemon() throws Exception {
-        TerminationRecord tr = new TerminationRecord("Process Terminated", null, null);
+        TerminationRecord tr = TerminationRecord.normal("Process Terminated", null, null);
         ProcessCompound sfDaemon = SFProcess.getRootLocator().getRootProcessCompound(SFProcess.sfDeployedHost(), 3800);
         TerminatorThread terminator = new TerminatorThread(sfDaemon, tr).detach().quietly();
         terminator.start();
