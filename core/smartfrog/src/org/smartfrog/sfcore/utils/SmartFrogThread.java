584d64039375a41177e3e4e1bcb9a1f69693a451
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-01-31 22:48:52 +0000

    SFOS-640  	 Add methods to SmartFrogThread to request (politely) the termination of the threads

--- core/smartfrog/src/org/smartfrog/sfcore/utils/SmartFrogThread.java
+++ core/smartfrog/src/org/smartfrog/sfcore/utils/SmartFrogThread.java
@@ -25,23 +25,19 @@ import org.smartfrog.sfcore.common.SmartFrogLivenessException;
 import java.rmi.RemoteException;
 
 /**
- * This thread represents the base class for threads run under SmartFrog.
- * It contains extra methods for behaviour we want across all our threads.
- * <p>
- *
- * <ol>
- * <li>A notification is raised after the work is done, on whichever object is the <code>notifyObject</code>.
- * By default, this is the thread itself, though other values may be provided in a constructor.
- * </li>
- * </li>Any throwable thrown in the {@link #execute()} method is caught and can be retrieved with the
- * {@link #getThrown()} method.
- * </li>
- * </ol>
- * To use these features, do not overrride the {@link #run()} method. Override  {@link #execute()} or pass
- * in a Runnable to the constructor.
- * provided as a notifyObject
- * created 13-Feb-2007 10:39:41
- * */
+ * This thread represents the base class for threads run under SmartFrog. It
+ * contains extra methods for behaviour we want across all our threads.
+ * <p/>
+ * <p/>
+ * <ol> <li>A notification is raised after the work is done, on whichever object
+ * is the <code>notifyObject</code>. By default, this is the thread itself,
+ * though other values may be provided in a constructor. </li> </li>Any
+ * throwable thrown in the {@link #execute()} method is caught and can be
+ * retrieved with the {@link #getThrown()} method. </li> </ol> To use these
+ * features, do not overrride the {@link #run()} method. Override  {@link
+ * #execute()} or pass in a Runnable to the constructor. provided as a
+ * notifyObject created 13-Feb-2007 10:39:41
+ */
 
 public class SmartFrogThread extends Thread {
 
@@ -50,32 +46,37 @@ public class SmartFrogThread extends Thread {
     private Runnable runnable;
     private Object notifyObject;
     private volatile boolean finished;
-
+    private volatile boolean terminationRequested = false;
+    private final Object terminationRequestNotifier = new Object();
 
     /**
      * Create a basic thread
+     *
      * @see Thread#Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread() {
-        this(null,(Object)null);
+        this(null, (Object) null);
     }
 
 
     /**
      * Allocates a new <code>SmartFrogThread</code> object.
+     *
      * @param notifyObject to notify afterwards. If null, "this" is used
      *
-     * @see     #Thread(ThreadGroup,Runnable,String)
+     * @see #Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(Object notifyObject) {
-        init(null,notifyObject);
+        init(null, notifyObject);
     }
 
     /**
      * Allocates a new <code>SmartFrogThread</code> object.
-     * @param   target   the object whose <code>run</code> method is called.
+     *
+     * @param target       the object whose <code>run</code> method is called.
      * @param notifyObject to notify afterwards. If null, "this" is used
-     * @see     #Thread(ThreadGroup,Runnable,String)
+     *
+     * @see #Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(Runnable target, Object notifyObject) {
         init(target, notifyObject);
@@ -85,6 +86,7 @@ public class SmartFrogThread extends Thread {
      * Create a basic thread bound to a runnable
      *
      * @param target the object whose <code>run</code> method is called.
+     *
      * @see Thread#Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(Runnable target) {
@@ -94,19 +96,24 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Create a thread
+     *
      * @param group  the thread group.
      * @param target the object whose <code>run</code> method is called.
-     * @throws SecurityException if the current thread cannot create a thread in the specified thread group.
+     *
+     * @throws SecurityException if the current thread cannot create a thread in
+     * the specified thread group.
      * @see Thread#Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(ThreadGroup group, Runnable target) {
-        super(group,(Runnable) null);
+        super(group, (Runnable) null);
         init(target, null);
     }
 
     /**
      * Create a thread
+     *
      * @param name the name of the new thread.
+     *
      * @see Thread#Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(String name) {
@@ -119,6 +126,7 @@ public class SmartFrogThread extends Thread {
      *
      * @param target the object whose <code>run</code> method is called.
      * @param name   the name of the new thread.
+     *
      * @see Thread#Thread(ThreadGroup,Runnable,String)
      */
     public SmartFrogThread(Runnable target, String name) {
@@ -128,17 +136,19 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Internal initialization
+     *
      * @param target what we want to run
-     * @param notify object to notify after the run.
-     * If null, it is set to <code>this</code>
+     * @param notify object to notify after the run. If null, it is set to
+     *               <code>this</code>
      */
     private void init(Runnable target, Object notify) {
         runnable = target;
         notifyObject = notify != null ? notify : this;
     }
 
-     /**
+    /**
      * Get the object that end of run notifications will be raised on.
+     *
      * @return the notify object. It is 'this' by default
      */
     public synchronized Object getNotifyObject() {
@@ -147,6 +157,7 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Return the exception thrown by this thread.
+     *
      * @return any exception or null
      */
     public synchronized Throwable getThrown() {
@@ -155,6 +166,7 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Record the exception thrown by this thread
+     *
      * @param thrown the exception to record as thrown
      */
     public synchronized void setThrown(Throwable thrown) {
@@ -163,25 +175,29 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Rethrow any exception as a SmartFrogException
-     * @throws SmartFrogException if the thread threw an exception that we caught.
+     *
+     * @throws SmartFrogException if the thread threw an exception that we
+     * caught.
      */
     public synchronized void rethrow() throws SmartFrogException {
-        if(thrown!=null) {
+        if (thrown != null) {
             throw convertThrown();
         }
     }
 
     /**
      * Test for having an exception at the end of the run.
+     *
      * @return true if there is an exception.
      */
     public boolean isThrown() {
-        return getThrown()!=null;
+        return getThrown() != null;
     }
 
 
     /**
      * Get the (volatile) finished flag
+     *
      * @return true if the component is finished.
      */
     public boolean isFinished() {
@@ -189,19 +205,21 @@ public class SmartFrogThread extends Thread {
     }
 
     /**
-     * Return true if an InterruptedException was thrown and caught
-     * at the end of execution.
-     * @return true if there was an exception caught and it was an {@link InterruptedException}
+     * Return true if an InterruptedException was thrown and caught at the end
+     * of execution.
+     *
+     * @return true if there was an exception caught and it was an {@link
+     *         InterruptedException}
      */
     public boolean wasInterruptedThrown() {
-        Throwable t=getThrown();
-        return t!=null && t instanceof InterruptedException;
+        Throwable t = getThrown();
+        return t != null && t instanceof InterruptedException;
     }
 
     /**
-     * Override point: convert the exception in thrown to
-     * a SmartFrogException.
+     * Override point: convert the exception in thrown to a SmartFrogException.
      * The base class delegates to {@link SmartFrogException#forward(Throwable)}
+     *
      * @return the exception to throw
      */
     protected SmartFrogException convertThrown() {
@@ -209,14 +227,18 @@ public class SmartFrogThread extends Thread {
     }
 
     /**
-     * Handle pings by rethrowing any caught exception. There is special handling for
-     * {@link InterruptedException}, as it is often raised when stopping a thread. It may not be
-     * an error for that to have happened.
-     * @param rethrowInterruptedExceptions should an InterruptedException be wrapped as a liveness failure.
+     * Handle pings by rethrowing any caught exception. There is special
+     * handling for {@link InterruptedException}, as it is often raised when
+     * stopping a thread. It may not be an error for that to have happened.
+     *
+     * @param rethrowInterruptedExceptions should an InterruptedException be
+     *                                     wrapped as a liveness failure.
+     *
      * @throws SmartFrogLivenessException containing any wrapped exception
      * @throws RemoteException if a remote exception was caught
      */
-    public void ping(boolean rethrowInterruptedExceptions) throws SmartFrogLivenessException, RemoteException {
+    public void ping(boolean rethrowInterruptedExceptions)
+            throws SmartFrogLivenessException, RemoteException {
         Throwable t = getThrown();
         if (t == null) {
             //no fault
@@ -234,8 +256,8 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Runs the {@link #execute()} method, catching any exception it throws and
-     * storing it away for safe keeping After the run, the notify object
-     * is notified
+     * storing it away for safe keeping After the run, the notify object is
+     * notified
      */
     public void run() {
         try {
@@ -243,9 +265,9 @@ public class SmartFrogThread extends Thread {
         } catch (Throwable throwable) {
             setThrown(throwable);
         } finally {
-            synchronized(notifyObject) {
+            synchronized (notifyObject) {
                 //set the finished bit
-                finished=true;
+                finished = true;
                 //notify any waiters
                 notifyObject.notifyAll();
             }
@@ -253,9 +275,11 @@ public class SmartFrogThread extends Thread {
     }
 
     /**
-     * If this thread was constructed using a separate {@link Runnable} run object, then that <code>Runnable</code>
-     * object's <code>run</code> method is called; otherwise, this method does nothing and returns. <p> Subclasses of
-     * <code>Thread</code> should override this method.
+     * If this thread was constructed using a separate {@link Runnable} run
+     * object, then that <code>Runnable</code> object's <code>run</code> method
+     * is called; otherwise, this method does nothing and returns. <p>
+     * Subclasses of <code>Thread</code> should override this method.
+     *
      * @throws Throwable if anything went wrong
      */
     public void execute() throws Throwable {
@@ -267,16 +291,104 @@ public class SmartFrogThread extends Thread {
 
     /**
      * Block on the notify object and so wait until the thread is finished.
+     *
      * @param timeout timeout in milliseconds
-     * @throws InterruptedException if the execution was interrupted
+     *
      * @return true if we are now finished
+     *
+     * @throws InterruptedException if the execution was interrupted
      */
-    public boolean waitForNotification(long timeout) throws InterruptedException {
+    public boolean waitForNotification(long timeout)
+            throws InterruptedException {
         synchronized (notifyObject) {
-            if(!finished) {
+            if (!finished) {
                 notifyObject.wait(timeout);
             }
         }
         return finished;
     }
+
+    /**
+     * Request termination on a thread that polls its {@link
+     * #terminationRequested} field, and/or blocks on the {@link
+     * #terminationRequestNotifier} object
+     */
+    public synchronized void requestTermination() {
+        if (!terminationRequested) {
+            terminationRequested = true;
+            terminationRequestNotifier.notifyAll();
+        }
+    }
+
+    /**
+     * Thread safe poll of the termination requested flag
+     *
+     * @return true if someone has requested this thread's termination
+     */
+    public synchronized boolean isTerminationRequested() {
+        return terminationRequested;
+    }
+
+    /**
+     * Block for a thread to terminate
+     *
+     * @param timeout the time to wait in milliseconds
+     *
+     * @return true if the thread is now terminated.
+     */
+    public boolean waitForThreadTermination(long timeout) {
+        if (!isAlive()) {
+            return true;
+        }
+        //not alive, so let's wait a bit
+        try {
+            join(timeout);
+        } catch (InterruptedException e) {
+
+        }
+        //and try again
+        return !isAlive();
+    }
+
+    /**
+     * Request the thread's termination, then wait for it to do so
+     *
+     * @param timeout the time to wait in milliseconds
+     *
+     * @return true if the thread is now terminated.
+     */
+    public boolean requestAndWaitForThreadTermination(long timeout) {
+        requestTermination();
+        return waitForThreadTermination(timeout);
+    }
+
+    /**
+     * Ping the thread parameter if it is not null
+     *
+     * @param thread thread to ping, may be null
+     *
+     * @throws SmartFrogLivenessException if the thread raises it
+     * @throws RemoteException if the thread raises it
+     */
+    public static void ping(SmartFrogThread thread)
+            throws SmartFrogLivenessException, RemoteException {
+        if (thread != null) {
+            thread.ping(false);
+        }
+    }
+
+    /**
+     * Request the thread to terminate
+     *
+     * @param thread  thread to terminate, can be null
+     * @param timeout time to wait for termination in milliseconds
+     *
+     * @return true if there is no longer a thread at the end of the wait
+     */
+    public static boolean requestAndWaitForThreadTermination(SmartFrogThread thread,
+                                                             long timeout) {
+        return thread == null ||
+                thread.requestAndWaitForThreadTermination(timeout);
+    }
+
 }
