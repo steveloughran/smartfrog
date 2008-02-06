ebd5163782f577376fd4c6d6b3eb3a21e7fcfada
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-02-06 15:15:18 +0000

    SFOS-603 	 Design and implement components that represent sets of files or other resources

--- trunk/core/smartfrog/src/org/smartfrog/services/filesystem/files/FilesImpl.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/filesystem/files/FilesImpl.java
@@ -37,40 +37,54 @@ import java.rmi.RemoteException;
 
 public class FilesImpl extends PrimImpl implements Files {
 
-    private Fileset fileset=new Fileset();
+    private Fileset fileset = new Fileset();
     private FilenamePatternFilter filter;
+    /**
+     * Error text {@value}
+     */
     public static final String ERROR_NOT_STARTED = "The component must be started before it can list the files";
     private static final Reference FILES_ATTRIBUTE = new Reference(ATTR_FILES);
     private static final Reference DIR_ATTRIBUTE = new Reference(ATTR_DIR);
     private static final Reference PATTERN_ATTRIBUTE = new Reference(ATTR_PATTERN);
     private static final Reference CASE_ATTRIBUTE = new Reference(ATTR_CASESENSITIVE);
     private static final Reference HIDDEN_ATTRIBUTE = new Reference(ATTR_INCLUDEHIDDENFILES);
+    public static final String ERROR_FILE_COUNT_MISMATCH = "File count mismatch: expected ";
 
 
     public FilesImpl() throws RemoteException {
     }
 
     /**
-     * Can be called to start components. Subclasses should override to provide
-     * functionality Do not block in this call, but spawn off any main loops!
+     * Can be called to start components. Subclasses should override to provide functionality Do not block in this call,
+     * but spawn off any main loops!
      *
      * @throws SmartFrogException failure while starting
-     * @throws RemoteException In case of network/rmi error
+     * @throws RemoteException    In case of network/rmi error
      */
     public synchronized void sfStart()
             throws SmartFrogException, RemoteException {
         super.sfStart();
-        fileset= resolveFileset(this);
+        fileset = resolveFileset(this);
+        //deal with the file count
+        int filecount = sfResolve(ATTR_FILECOUNT, -1, false);
+        File[] files = listFiles();
+        int length = files.length;
+        if (filecount >= 0 && length != filecount) {
+            throw new SmartFrogDeploymentException(
+                    ERROR_FILE_COUNT_MISMATCH + filecount + " but found " + length + " files"
+                            + "when resolving " + fileset, this);
+        } else if (filecount < 0) {
+            sfReplaceAttribute(ATTR_FILECOUNT, filecount);
+        }
+
     }
 
     /**
-     * Return a list of files that match the current pattern. This may be a
-     * compute-intensive operation, so cache the result. Note that filesystem
-     * race conditions do not guarantee all the files listed still exist...check
-     * before acting
+     * Return a list of files that match the current pattern. This may be a compute-intensive operation, so cache the
+     * result. Note that filesystem race conditions do not guarantee all the files listed still exist...check before
+     * acting
      *
-     * @return a list of files that match the pattern, or an empty list for no
-     *         match
+     * @return a list of files that match the pattern, or an empty list for no match
      * @throws SmartFrogLifecycleException if it could  not do this
      */
 
@@ -82,8 +96,7 @@ public class FilesImpl extends PrimImpl implements Files {
      * Get the base directory of these files (may be null)
      *
      * @return the base directory
-     *
-     * @throws RemoteException when the network plays up
+     * @throws RemoteException    when the network plays up
      * @throws SmartFrogException if something else went wrong
      */
     public File getBaseDir() throws RemoteException, SmartFrogException {
@@ -93,10 +106,11 @@ public class FilesImpl extends PrimImpl implements Files {
 
     /**
      * Build a fileset from a Prim component with the Files attributes.
+     *
      * @param component component to work with
      * @return a newly filled in fileset
      * @throws SmartFrogException if there were problems resolving attributes
-     * @throws RemoteException for networking problems
+     * @throws RemoteException    for networking problems
      */
     public static Fileset resolveFileset(Prim component)
             throws SmartFrogException, RemoteException {
