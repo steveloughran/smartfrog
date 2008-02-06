b22d64efc6005df8f7e889376c02b76ba0c7d2f8
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-02-06 16:29:10 +0000

    SFOS-644 Move Property class to string tuple list logic from LoadPropertyFileImpl to ListUtils

--- trunk/core/smartfrog/src/org/smartfrog/services/os/java/LoadPropertyFileImpl.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/os/java/LoadPropertyFileImpl.java
@@ -25,6 +25,7 @@ import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.common.SmartFrogException;
 import org.smartfrog.sfcore.prim.TerminationRecord;
 import org.smartfrog.sfcore.utils.ComponentHelper;
+import org.smartfrog.sfcore.utils.ListUtils;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -45,35 +46,34 @@ import java.util.Vector;
 
 public class LoadPropertyFileImpl extends FileUsingComponentImpl implements LoadPropertyFile {
     public static final String ERROR_NO_PROPFILE = "No property file or resource specified";
-    public static final String ERROR_TOO_MANY_ATTRIBUTES = "Both "+ATTR_FILENAME+" and "+ATTR_RESOURCE
-           +"are set; only one is allowed";
+    public static final String ERROR_TOO_MANY_ATTRIBUTES = "Both " + ATTR_FILENAME + " and " + ATTR_RESOURCE
+            + "are set; only one is allowed";
 
     public LoadPropertyFileImpl() throws RemoteException {
     }
 
 
     /**
-     * Can be called to start components. Subclasses should override to provide
-     * functionality Do not block in this call, but spawn off any main loops!
+     * Can be called to start components. Subclasses should override to provide functionality Do not block in this call,
+     * but spawn off any main loops!
      *
-     * @throws org.smartfrog.sfcore.common.SmartFrogException
-     *                                  failure while starting
-     * @throws java.rmi.RemoteException In case of network/rmi error
+     * @throws SmartFrogException  failure while starting
+     * @throws RemoteException In case of network/rmi error
      */
     public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
         super.sfDeploy();
-        bindWithDir(false,null);
+        bindWithDir(false, null);
         ComponentHelper helper = new ComponentHelper(this);
-        String resource=sfResolve(ATTR_RESOURCE,(String)null,false);
-        boolean required=sfResolve(ATTR_REQUIRED,true,false);
-        Properties properties=new Properties();
+        String resource = sfResolve(ATTR_RESOURCE, (String) null, false);
+        boolean required = sfResolve(ATTR_REQUIRED, true, false);
+        Properties properties = new Properties();
         //validation
-        if(resource!=null) {
-            if(getFile()!=null) {
+        if (resource != null) {
+            if (getFile() != null) {
                 throw new SmartFrogDeploymentException(ERROR_TOO_MANY_ATTRIBUTES);
             }
         } else {
-            if(getFile()==null) {
+            if (getFile() == null) {
                 throw new SmartFrogDeploymentException(ERROR_NO_PROPFILE);
             }
         }
@@ -88,7 +88,7 @@ public class LoadPropertyFileImpl extends FileUsingComponentImpl implements Load
             }
         } else {
             try {
-                in=new BufferedInputStream(new FileInputStream(getFile()));
+                in = new BufferedInputStream(new FileInputStream(getFile()));
             } catch (FileNotFoundException ioe) {
                 if (required) {
                     throw SmartFrogDeploymentException.forward(ioe);
@@ -96,7 +96,7 @@ public class LoadPropertyFileImpl extends FileUsingComponentImpl implements Load
             }
         }
         //at this point, in is null or set to an input stream
-        if(in!=null) {
+        if (in != null) {
             try {
                 properties.load(in);
             } catch (IOException ioe) {
@@ -109,20 +109,11 @@ public class LoadPropertyFileImpl extends FileUsingComponentImpl implements Load
         }
         //at this point, properties is either an empty list or one containing loaded properties.
         //now its time to write it out
-        Vector propertyList=new Vector(properties.size());
-        final List keyList = new ArrayList(properties.keySet());
-        Collections.sort(keyList);
-        Iterator keys = keyList.iterator();
-        while (keys.hasNext()) {
-            String key = (String) keys.next();
-            String value=properties.getProperty(key);
-            sfReplaceAttribute(key,value);
-            Vector tuple=new Vector(2);
-            tuple.add(key);
-            tuple.add(value);
-            propertyList.add(tuple);
+        Vector<Vector<String>> tuples = ListUtils.propertiesToTuples(properties);
+        for(Vector<String> tuple:tuples) {
+            sfReplaceAttribute(ListUtils.left(tuple), ListUtils.right(tuple));
         }
-        sfReplaceAttribute(ATTR_PROPERTIES,propertyList);
+        sfReplaceAttribute(ATTR_PROPERTIES, tuples);
         helper.sfSelfDetachAndOrTerminate(TerminationRecord.NORMAL,
                 "PropertyFile",
                 null,
