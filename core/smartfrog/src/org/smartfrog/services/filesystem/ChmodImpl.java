965363ded5e5020780ef1784972b7796257cba23
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-02-11 11:54:50 +0000

    SFOS-654 sfSelfDetachAndOrTerminate

--- core/smartfrog/src/org/smartfrog/services/filesystem/ChmodImpl.java
+++ core/smartfrog/src/org/smartfrog/services/filesystem/ChmodImpl.java
@@ -1,10 +1,45 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+Disclaimer of Warranty
+
+The Software is provided "AS IS," without a warranty of any kind. ALL
+EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, OR NON-INFRINGEMENT, ARE HEREBY
+EXCLUDED. SmartFrog is not a Hewlett-Packard Product. The Software has
+not undergone complete testing and may contain errors and defects. It
+may not function properly and is subject to change or withdrawal at
+any time. The user must assume the entire risk of using the
+Software. No support or maintenance is provided with the Software by
+Hewlett-Packard. Do not install the Software if you are not accustomed
+to using experimental software.
+
+Limitation of Liability
+
+TO THE EXTENT NOT PROHIBITED BY LAW, IN NO EVENT WILL HEWLETT-PACKARD
+OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR
+FOR SPECIAL, INDIRECT, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES,
+HOWEVER CAUSED REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+OR RELATED TO THE FURNISHING, PERFORMANCE, OR USE OF THE SOFTWARE, OR
+THE INABILITY TO USE THE SOFTWARE, EVEN IF HEWLETT-PACKARD HAS BEEN
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. FURTHERMORE, SINCE THE
+SOFTWARE IS PROVIDED WITHOUT CHARGE, YOU AGREE THAT THERE HAS BEEN NO
+BARGAIN MADE FOR ANY ASSUMPTIONS OF LIABILITY OR DAMAGES BY
+HEWLETT-PACKARD FOR ANY REASON WHATSOEVER, RELATING TO THE SOFTWARE OR
+ITS MEDIA, AND YOU HEREBY WAIVE ANY CLAIM IN THIS REGARD.
+
+*/
+
 package org.smartfrog.services.filesystem;
 
 import org.smartfrog.sfcore.prim.PrimImpl;
 import org.smartfrog.sfcore.prim.TerminationRecord;
 import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.utils.ComponentHelper;
 
 import java.rmi.RemoteException;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.IOException;
 
 /**
@@ -12,13 +47,15 @@ import java.io.IOException;
  */
 public class ChmodImpl extends PrimImpl implements Chmod {
 
-    private String  strTarget,
-                    strUserPermissions,
-                    strGroupPermissions,
-                    strOtherPermissions,
-                    strOctalNotation;
+    private String target,
+            userPermissions,
+            groupPermissions,
+            otherPermissions,
+            octalNotation;
 
-    private boolean bRecursively;
+    private boolean recursively;
+    public static final String ERROR_NO_PERMISSIONS_SPECIFIED = "No permissions specified.";
+    public static final String ERROR_NO_WINDOWS = "Chmod not supported under Windows.";
 
     public ChmodImpl() throws RemoteException {
 
@@ -40,25 +77,24 @@ public class ChmodImpl extends PrimImpl implements Chmod {
 
         // if this is a windows system terminate
         if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-            // TODO: how to properly terminate components?
-            sfLog().error("Chmod not supported under Windows.");
-            throw new SmartFrogException("Chmod not supported under Windows.");
+            sfLog().error(ERROR_NO_WINDOWS);
+            throw new SmartFrogException(ERROR_NO_WINDOWS);
         }
 
         // resolve the attributes
-        strTarget               = FileSystem.lookupAbsolutePath(this, ATTR_TARGET, null, null, true, null);
-        strUserPermissions      = sfResolve(ATTR_USER_PERMISSIONS, "", false);
-        strGroupPermissions     = sfResolve(ATTR_GROUP_PERMISSIONS, "", false);
-        strOtherPermissions     = sfResolve(ATTR_OTHER_PERMISSIONS, "", false);
-        strOctalNotation        = sfResolve(ATTR_OCTAL_CODE, "", false);
-        bRecursively            = sfResolve(ATTR_RECURSIVELY, false, true);
+        target = FileSystem.lookupAbsolutePath(this, ATTR_TARGET, null, null, true, null);
+        userPermissions = sfResolve(ATTR_USER_PERMISSIONS, "", false);
+        groupPermissions = sfResolve(ATTR_GROUP_PERMISSIONS, "", false);
+        otherPermissions = sfResolve(ATTR_OTHER_PERMISSIONS, "", false);
+        octalNotation = sfResolve(ATTR_OCTAL_CODE, "", false);
+        recursively = sfResolve(ATTR_RECURSIVELY, false, true);
 
-        if (strUserPermissions.equals("") &&
-            strGroupPermissions.equals("") &&
-            strOtherPermissions.equals("") &&
-            strOctalNotation.equals("")) {
-            sfLog().error("No permissions specified.");
-            throw new SmartFrogException("No permissions specified.");
+        if (userPermissions.equals("") &&
+            groupPermissions.equals("") &&
+            otherPermissions.equals("") &&
+            octalNotation.equals("")) {
+            sfLog().error(ERROR_NO_PERMISSIONS_SPECIFIED);
+            throw new SmartFrogException(ERROR_NO_PERMISSIONS_SPECIFIED);
         }
     }
 
@@ -74,47 +110,54 @@ public class ChmodImpl extends PrimImpl implements Chmod {
         super.sfStart();
 
         // compose the options
-        String strOptions = "";
+        List<String> commands=new ArrayList<String>(4);
+        commands.add("chmod");
 
-        if (bRecursively)
-            strOptions = "-R ";
-
-        if (!strOctalNotation.equals("")) {
-            strOptions = strOctalNotation;
-        }
-        else {
-            boolean bComma = false;
-            if (!strUserPermissions.equals("")) {
-                strOptions += "u" + strUserPermissions;
-                bComma = true;
-            }
-            if (!strGroupPermissions.equals("")) {
-                // multiple user classes have to be comma-separated
-                if (bComma)
-                    strOptions += "'";
-
-                strOptions += "g" + strGroupPermissions;
-                bComma = true;
-            }
-            if (!strOtherPermissions.equals("")) {
-                // multiple user classes have to be comma-separated
-                if (bComma)
-                    strOptions += "'";
-                
-                strOptions += "o" + strOtherPermissions;
-            }
+        if (recursively) {
+            commands.add("-R ");
         }
 
-        // compose the command string
-        String strCommand = String.format("chmod %s %s",strOptions, strTarget);
+
+        if (octalNotation.length() != 0) {
+            commands.add(octalNotation);
+        } else {
+            boolean comma = false;
+            String modeString = "";
+            if (userPermissions.length() != 0) {
+                modeString = 'u' + userPermissions;
+                comma = true;
+            }
+            if (groupPermissions.length() != 0) {
+                // multiple user classes have to be comma-separated
+                modeString = (comma ? "," : "") + 'g' + groupPermissions;
+                comma = true;
+            }
+            if (otherPermissions.length() != 0) {
+                // multiple user classes have to be comma-separated
+                modeString = (comma ? "," : "") + 'o' + otherPermissions;
+            }
+            commands.add(modeString);
+        }
 
         // execute the command
+        String[] commandArray = commands.toArray(new String[commands.size()]);
+        String details="";
+        for(String s:commandArray) {
+            details+=s;
+            details+=" ";
+        }
+        sfLog().debug(details);
         try {
-            Runtime.getRuntime().exec(strCommand);
+            Runtime.getRuntime().exec(commandArray);
         }
-        catch (Exception e) {
-            sfLog().error("Error while executing: " + strCommand, e);
-            throw new SmartFrogException("Error while executing: " + strCommand, e);
+        catch (IOException e) {
+            sfLog().error("Error while executing: " + details, e);
+            throw SmartFrogException.forward("Error while executing: " + details, e);
         }
+        //now see if we are to self terminate
+        new ComponentHelper(this).sfSelfDetachAndOrTerminate(TerminationRecord.NORMAL,
+                "chmod executed",
+                sfCompleteNameSafe(),
+                null);
     }
 }
