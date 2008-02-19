df6ce1c8d15565ffed5df8313f8e0ab6d671e213
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-02-19 16:04:04 +0000

    SFOS-686  LivenessPageChecker/Java.net.URL doesnt use username+password SFOS-628 	 JettySecurityRealmImpl does not use the realm

---
+++ trunk/core/components/jetty/test/org/smartfrog/services/www/jetty/test/system/testwar/WarSecurityTest.java
@@ -0,0 +1,24 @@
+package org.smartfrog.services.www.jetty.test.system.testwar;
+
+import org.smartfrog.services.www.jetty.test.system.JettyTestBase;
+
+/**
+ *
+ */
+public class WarSecurityTest extends JettyTestBase {
+
+
+    private static final String FILES ="/org/smartfrog/services/www/jetty/test/system/testwar/";
+
+    public WarSecurityTest(String name) {
+        super(name);
+    }
+
+    public void testSecurity() throws Throwable {
+        expectSuccessfulTestRun(FILES, "testSecurity");
+    }
+
+    public void testSecurityUnauth() throws Throwable {
+        expectSuccessfulTestRun(FILES, "testSecurityUnauth");
+    }
+}
