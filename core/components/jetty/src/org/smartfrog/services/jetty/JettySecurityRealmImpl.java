7952ce8f6728c4643a531bdc0d95f25978846250
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-02-20 14:13:22 +0000

    reformatting prior to changes

--- core/components/jetty/src/org/smartfrog/services/jetty/JettySecurityRealmImpl.java
+++ core/components/jetty/src/org/smartfrog/services/jetty/JettySecurityRealmImpl.java
@@ -1,22 +1,22 @@
 /** (C) Copyright 2007 Hewlett-Packard Development Company, LP
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-For more information: www.smartfrog.org
+ For more information: www.smartfrog.org
 
-*/
+ */
 package org.smartfrog.services.jetty;
 
 import org.smartfrog.sfcore.prim.PrimImpl;
@@ -34,9 +34,7 @@ import java.rmi.RemoteException;
 import java.util.Vector;
 
 /**
- *
  * Created 25-Oct-2007 17:00:02
- *
  */
 
 public class JettySecurityRealmImpl extends PrimImpl implements JettySecurityRealm {
@@ -52,51 +50,52 @@ public class JettySecurityRealmImpl extends PrimImpl implements JettySecurityRea
      * Can be called to start components.
      *
      * @throws SmartFrogException failure while starting
-     * @throws RemoteException In case of network/rmi error
+     * @throws RemoteException    In case of network/rmi error
      */
-     public synchronized void sfStart() throws SmartFrogException, RemoteException {
+    public synchronized void sfStart() throws SmartFrogException, RemoteException {
         super.sfStart();
         jettyHelper.bindToServer();
 
-        Vector users=null,constraints=null;
-        String name=sfResolve(ATTR_NAME,"",true);
+        Vector users = null, constraints = null;
+        String name = sfResolve(ATTR_NAME, "", true);
         realm = new HashUserRealm(name);
-        users=sfResolve(ATTR_USERS,users,true);
-        for(Object user :users) {
-            Vector v=(Vector) user;
-            if(v.size()<3) {
-                throw new SmartFrogResolutionException("User element too short",this);
+        users = sfResolve(ATTR_USERS, users, true);
+        for (Object user : users) {
+            Vector v = (Vector) user;
+            if (v.size() < 3) {
+                throw new SmartFrogResolutionException("User element too short", this);
             }
-            String username=v.elementAt(0).toString();
-            String password= v.elementAt(1).toString();
-            realm.put(username,password);
+            String username = v.elementAt(0).toString();
+            Object passwordEntry = v.elementAt(1);
+            String password = passwordEntry.toString();
+            realm.put(username, password);
             //add the roles
-            for(int role=2;role<v.size();role++) {
-                realm.addUserToRole(username,v.elementAt(role).toString());
+            for (int role = 2; role < v.size(); role++) {
+                realm.addUserToRole(username, v.elementAt(role).toString());
             }
         }
         //now the constraints
-        constraints=sfResolve(ATTR_CONSTRAINTS,constraints,true);
-        ConstraintMapping[] mappings=new ConstraintMapping[constraints.size()];
-        for(int entry=0;entry<constraints.size();entry++) {
-            Vector oneConstraint=(Vector) constraints.elementAt(entry);
-            int roleCount= oneConstraint.size()-2;
-            if(roleCount<=0) {
-                throw new SmartFrogResolutionException("Constraint list length wrong: entry "+entry,this);
+        constraints = sfResolve(ATTR_CONSTRAINTS, constraints, true);
+        ConstraintMapping[] mappings = new ConstraintMapping[constraints.size()];
+        for (int entry = 0; entry < constraints.size(); entry++) {
+            Vector oneConstraint = (Vector) constraints.elementAt(entry);
+            int roleCount = oneConstraint.size() - 2;
+            if (roleCount <= 0) {
+                throw new SmartFrogResolutionException("Constraint list length wrong: entry " + entry, this);
             }
-            String path=oneConstraint.elementAt(0).toString();
-            String constraintName= oneConstraint.elementAt(1).toString();
-            String[] roles=new String[roleCount];
-            for(int roleEntry=0;roleEntry<roleCount;roleEntry++) {
-                roles[roleEntry]= oneConstraint.elementAt(2+roleEntry).toString();
+            String path = oneConstraint.elementAt(0).toString();
+            String constraintName = oneConstraint.elementAt(1).toString();
+            String[] roles = new String[roleCount];
+            for (int roleEntry = 0; roleEntry < roleCount; roleEntry++) {
+                roles[roleEntry] = oneConstraint.elementAt(2 + roleEntry).toString();
             }
-            Constraint cons= new Constraint();
+            Constraint cons = new Constraint();
             cons.setName(constraintName);
             cons.setRoles(roles);
-            ConstraintMapping mapping=new ConstraintMapping();
+            ConstraintMapping mapping = new ConstraintMapping();
             mapping.setPathSpec(path);
             mapping.setConstraint(cons);
-            mappings[entry]=mapping;
+            mappings[entry] = mapping;
         }
         jettyHelper.getServer().addUserRealm(realm);
         security = new SecurityHandler();
@@ -118,7 +117,7 @@ public class JettySecurityRealmImpl extends PrimImpl implements JettySecurityRea
         super.sfTerminateWith(status);
         if (security != null) {
             Server server = jettyHelper.getServer();
-            if(server!=null) {
+            if (server != null) {
                 server.removeHandler(security);
                 server.removeUserRealm(realm);
             }
