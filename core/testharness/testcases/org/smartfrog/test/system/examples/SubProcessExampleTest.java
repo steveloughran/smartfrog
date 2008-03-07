ee395ef292dd61f961e589c26d6c0b48624d9e9d
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-03-07 12:07:06 +0000

    some cleanup while reviewing

--- core/testharness/testcases/org/smartfrog/test/system/examples/SubProcessExampleTest.java
+++ core/testharness/testcases/org/smartfrog/test/system/examples/SubProcessExampleTest.java
@@ -30,114 +30,108 @@ import org.apache.commons.logging.LogFactory;
  * JUnit test class for test cases related to Subprocess Example
  */
 public class SubProcessExampleTest
-    extends DeployingTestBase {
+        extends DeployingTestBase {
 
-  // In this particular case we use the examples without screens
-  //private static final String FILES = "org/smartfrog/examples/subprocesses/";
-  //#include "org/smartfrog/test/system/deploy/subprocessTestHarness.sf"
-  private static final String FILES = "org/smartfrog/test/system/deploy/";
-  private static final Log log = LogFactory.getLog(SubProcessExampleTest.class);
-  public SubProcessExampleTest(String s) {
-    super(s);
-  }
+    private static final String FILES = "org/smartfrog/test/system/deploy/";
+    private static final Log log = LogFactory.getLog(SubProcessExampleTest.class);
+    private static final String COMPOUND_IMPL = "org.smartfrog.sfcore.compound.CompoundImpl";
 
-  public void testCaseSubProcessEx01() throws Throwable {
+    public SubProcessExampleTest(String s) {
+        super(s);
+    }
 
-    application = deployExpectingSuccess(FILES +
-        "subprocessTestHarness.sf", "tcSPE01");
-    assertNotNull(application);
+    public void testCaseSubProcessEx01() throws Throwable {
 
-    String actualSfClass = (String) application.sfResolveHere("sfClass");
-    assertEquals("org.smartfrog.sfcore.compound.CompoundImpl", actualSfClass);
+        application = deployExpectingSuccess(FILES +
+                "subprocessTestHarness.sf", "tcSPE01");
+        assertNotNull(application);
 
-    Prim sys = (Prim) application.sfResolveHere("system");
-    //System.out.println();
-    assertEquals("first", sys.sfDeployedProcessName());
+        String actualSfClass = (String) application.sfResolveHere("sfClass");
+        assertEquals(COMPOUND_IMPL, actualSfClass);
 
-    Prim foo = (Prim) sys.sfResolveHere("foo");
-    assertEquals("test", foo.sfDeployedProcessName());
+        Prim sys = (Prim) application.sfResolveHere("system");
+        assertEquals("first", sys.sfDeployedProcessName());
 
-    Prim bar = (Prim) foo.sfResolveHere("bar");
-    assertEquals("test2", bar.sfDeployedProcessName());
-    Prim traceTest2 = (Prim) bar.sfResolveHere("traceTest2");
-    String actualSfClassTT2 = (String) traceTest2.sfResolveHere("sfClass");
-    assertEquals("org.smartfrog.services.trace.SFTrace", actualSfClassTT2);
+        Prim foo = (Prim) sys.sfResolveHere("foo");
+        assertEquals("test", foo.sfDeployedProcessName());
 
-    Prim demoE = (Prim) bar.sfResolveHere("demoE");
-    int actualdemoE = 0;
-    int expecteddemoE = 9;
-    actualdemoE = demoE.sfResolve("limit", actualdemoE, true);
-    assertEquals(expecteddemoE, actualdemoE);
+        Prim bar = (Prim) foo.sfResolveHere("bar");
+        assertEquals("test2", bar.sfDeployedProcessName());
+        Prim traceTest2 = (Prim) bar.sfResolveHere("traceTest2");
+        String actualSfClassTT2 = (String) traceTest2.sfResolveHere("sfClass");
+        assertEquals("org.smartfrog.services.trace.SFTrace", actualSfClassTT2);
 
-    Prim foobar = (Prim) sys.sfResolveHere("foobar");
+        Prim demoE = (Prim) bar.sfResolveHere("demoE");
+        int actualdemoE = 0;
+        int expecteddemoE = 9;
+        actualdemoE = demoE.sfResolve("limit", actualdemoE, true);
+        assertEquals(expecteddemoE, actualdemoE);
 
-    Prim demoF = (Prim) foobar.sfResolveHere("demoF");
-    int actualdemoF = 0;
-    int expecteddemoF = 7;
-    actualdemoF = demoF.sfResolve("limit", actualdemoE, true);
-    assertEquals(expecteddemoF, actualdemoF);
+        Prim foobar = (Prim) sys.sfResolveHere("foobar");
 
-    //this does not work with the displayless version :-(
-//	Prim displayLOCALHOST = (Prim)foobar.sfResolveHere("displayLOCALHOST");
-//		String actualSfClassDLH = (String)displayLOCALHOST.sfResolveHere("sfClass");
-//		assertEquals("org.smartfrog.services.display.SFDisplay", actualSfClassDLH);
-    Prim displayLOCALHOST = (Prim) foobar.sfResolveHere("displayLOCALHOST");
-    String actualSfClassDLH = (String) displayLOCALHOST.sfResolveHere("sfClass");
-    String desiredClass = "org.smartfrog.sfcore.compound.CompoundImpl";
-    log.info("    Asserting: " + desiredClass
-                       + " with " + actualSfClassDLH);
-    assertEquals(desiredClass, actualSfClassDLH);
+        Prim demoF = (Prim) foobar.sfResolveHere("demoF");
+        int actualdemoF = 0;
+        int expecteddemoF = 7;
+        actualdemoF = demoF.sfResolve("limit", actualdemoE, true);
+        assertEquals(expecteddemoF, actualdemoF);
 
-    Prim baz = (Prim) sys.sfResolveHere("baz");
-    Prim demoC = (Prim) baz.sfResolveHere("demoC");
-    int actualdemoC = 0;
-    int expecteddemoC = 9;
-    actualdemoC = demoC.sfResolve("limit", actualdemoC, true);
-    assertEquals(expecteddemoC, actualdemoC);
+        Prim displayLOCALHOST = (Prim) foobar.sfResolveHere("displayLOCALHOST");
+        String actualSfClassDLH = (String) displayLOCALHOST.sfResolveHere("sfClass");
+        String desiredClass = COMPOUND_IMPL;
+        log.info("    Asserting: " + desiredClass
+                + " with " + actualSfClassDLH);
+        assertEquals(desiredClass, actualSfClassDLH);
 
-    Prim sys_bar = (Prim) sys.sfResolveHere("bar");
-    Prim demoD = (Prim) sys_bar.sfResolveHere("demoD");
-    int actualdemoD = 0;
-    int expecteddemoD = 3;
-    actualdemoD = demoD.sfResolve("limit", actualdemoD, true);
-    assertEquals(expecteddemoD, actualdemoD);
+        Prim baz = (Prim) sys.sfResolveHere("baz");
+        Prim demoC = (Prim) baz.sfResolveHere("demoC");
+        int actualdemoC = 0;
+        int expecteddemoC = 9;
+        actualdemoC = demoC.sfResolve("limit", actualdemoC, true);
+        assertEquals(expecteddemoC, actualdemoC);
 
-  }
+        Prim sys_bar = (Prim) sys.sfResolveHere("bar");
+        Prim demoD = (Prim) sys_bar.sfResolveHere("demoD");
+        int actualdemoD = 0;
+        int expecteddemoD = 3;
+        actualdemoD = demoD.sfResolve("limit", actualdemoD, true);
+        assertEquals(expecteddemoD, actualdemoD);
 
-  public void testCaseExampleProcessComponentName02() throws Throwable {
-    application = deployExpectingSuccess("org/smartfrog/examples/subprocesses/" + "exampleProcessComponentName.sf",
-            "tcExampleProcessComponentName02");
-    assertNotNull(application);
+    }
 
-    String actualSfClass = (String) application.sfResolveHere("sfClass");
-    assertEquals("org.smartfrog.sfcore.compound.CompoundImpl", actualSfClass);
+    public void testCaseExampleProcessComponentName02() throws Throwable {
+        application = deployExpectingSuccess("org/smartfrog/examples/subprocesses/" + "exampleProcessComponentName.sf",
+                "tcExampleProcessComponentName02");
+        assertNotNull(application);
 
-    Prim dos = (Prim) application.sfResolveHere("dos");
-    assertEquals("DOS-VM", dos.sfDeployedProcessName());
+        String actualSfClass = (String) application.sfResolveHere("sfClass");
+        assertEquals(COMPOUND_IMPL, actualSfClass);
 
-    Prim uno = (Prim) application.sfResolveHere("uno");
-    Prim cuatro = (Prim) uno.sfResolveHere("cuatro");
-    assertEquals("CUATRO-VM", cuatro.sfDeployedProcessName());
+        Prim dos = (Prim) application.sfResolveHere("dos");
+        assertEquals("DOS-VM", dos.sfDeployedProcessName());
 
-    Prim tres = (Prim) application.sfResolveHere("tres");
-    assertEquals("rootProcess", tres.sfDeployedProcessName());
+        Prim uno = (Prim) application.sfResolveHere("uno");
+        Prim cuatro = (Prim) uno.sfResolveHere("cuatro");
+        assertEquals("CUATRO-VM", cuatro.sfDeployedProcessName());
 
-    Reference refCUATRO = Reference.fromString("HOST localhost:CUATRO-VM:CUATRO");
-    Prim cuatro2 = (Prim)application.sfResolve(refCUATRO,true);
-    log.info("      Testing: "+refCUATRO);
-    assertEquals(cuatro.sfCompleteName().toString(), cuatro2.sfCompleteName().toString());
+        Prim tres = (Prim) application.sfResolveHere("tres");
+        assertEquals("rootProcess", tres.sfDeployedProcessName());
 
-    Reference refTRES = Reference.fromString("HOST localhost:rootProcess:TRES");
-    Prim tres2 = (Prim)application.sfResolve(refTRES,true);
-    log.info("      Testing: "+refTRES);
-    assertEquals(tres.sfCompleteName().toString(), tres2.sfCompleteName().toString());
+        Reference refCUATRO = Reference.fromString("HOST localhost:CUATRO-VM:CUATRO");
+        Prim cuatro2 = application.sfResolve(refCUATRO, (Prim) null, true);
+        log.info("      Testing: " + refCUATRO);
+        assertEquals(cuatro.sfCompleteName().toString(), cuatro2.sfCompleteName().toString());
 
-    Reference refDOS = Reference.fromString("HOST localhost:DOS-VM:DOS");
-    Prim dos2 = (Prim)application.sfResolve(refDOS,true);
-    log.info("      Testing: "+refDOS);
-    assertEquals(dos.sfCompleteName().toString(), dos2.sfCompleteName().toString());
+        Reference refTRES = Reference.fromString("HOST localhost:rootProcess:TRES");
+        Prim tres2 = application.sfResolve(refTRES, (Prim)null, true);
+        log.info("      Testing: " + refTRES);
+        assertEquals(tres.sfCompleteName().toString(), tres2.sfCompleteName().toString());
+
+        Reference refDOS = Reference.fromString("HOST localhost:DOS-VM:DOS");
+        Prim dos2 = application.sfResolve(refDOS, (Prim) null, true);
+        log.info("      Testing: " + refDOS);
+        assertEquals(dos.sfCompleteName().toString(), dos2.sfCompleteName().toString());
 
 
-  }
+    }
 
 }
