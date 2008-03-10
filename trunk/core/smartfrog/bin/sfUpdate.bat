858fca8607253e77660ee1fe8f52103581517b2e
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-03-10 07:17:28 +0000

    SFOS-710: sfUpdate also changed for -p port option.

--- trunk/core/smartfrog/bin/sfUpdate.bat
+++ trunk/core/smartfrog/bin/sfUpdate.bat
@@ -7,6 +7,9 @@ if defined SFHOME goto continue1
 :continue1
 if (%1) == () goto usage
 if (%1) == (-?) goto help
+if (%1) == (-p) GOTO usage
+if (%2) == (-p) GOTO usage
+if (%3) == (-p) GOTO usage
 if (%2) == () goto usage
 if (%3) == () goto usage
 
@@ -14,22 +17,26 @@ call "%SFHOME%\bin\setSFProperties"
 
 if defined USERNAMEPREFIX_ON goto modify
 set APPNAME=%2
-goto execute
+goto run
 
 :modify
 set APPNAME="%USERNAME%_%2"
-
-:execute
+:run
+if (%4)==(-p) GOTO execute
 rem %SFJVM% %SFCMDPARAMETERS% org.smartfrog.SFSystem -a \"%2\":UPDATE:\"%3\"::%1: -e
 %SFJVM% %SFCMDPARAMETERS% org.smartfrog.SFSystem -a \"%APPNAME%\":UPDATE:\"%3\"::%1: -e
+GOTO end
+:execute
+if (%5)==() goto usage
+%SFJVM% %SFCMDPARAMETERS% org.smartfrog.SFSystem -a \"%APPNAME%\":UPDATE:\"%3\"::%1: -p %5 -e
 
 GOTO end
 :usage
 echo Insufficient arguments to use sfUpdate 
-echo Usage: sfUpdate HostName ApplicationName URL
+echo Usage: sfUpdate HostName ApplicationName URL [-p port]
 exit /B 69
 :help
-echo Usage: sfUpdate HostName ApplicationName URL
+echo Usage: sfUpdate HostName ApplicationName URL [-p port]
 exit /B 0
 :end
 endlocal
