b352fda88e81803a606f816a57723e3b1c731223
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-03-25 16:43:16 +0000

    SFOS-744 	 Add component to deploy/terminate EC2 VMs We use proxy settings in so many places they should be defined

---
+++ core/components/www/src/org/smartfrog/services/www/HttpProxyProperties.java
@@ -0,0 +1,33 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.www;
+
+/**
+ *
+ * Created 25-Mar-2008 16:35:25
+ *
+ */
+
+public class HttpProxyProperties {
+    public static final String HTTP_PROXY_PORT = "http.proxyPort";
+    public static final String HTTP_PROXY_USER = "http.proxyUser";
+    public static final String HTTP_PROXY_PASSWORD = "http.proxyPassword";
+    public static final String HTTP_PROXY_HOST = "http.proxyHost";
+}
