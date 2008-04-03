b978ccd7a684a50c6bff8328dcda444735f899d4
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-04-03 09:56:47 +0000

    SFOS-753 Improve FileExists to help diagnose more complex problems

--- core/smartfrog/src/org/smartfrog/services/filesystem/FileExistsImpl.java
+++ core/smartfrog/src/org/smartfrog/services/filesystem/FileExistsImpl.java
@@ -1,7 +1,37 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+Disclaimer of Warranty
+
+The Software is provided "AS IS," without a warranty of any kind. ALL
+EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, OR NON-INFRINGEMENT, ARE HEREBY
+EXCLUDED. SmartFrog is not a Hewlett-Packard Product. The Software has
+not undergone complete testing and may contain errors and defects. It
+may not function properly and is subject to change or withdrawal at
+any time. The user must assume the entire risk of using the
+Software. No support or maintenance is provided with the Software by
+Hewlett-Packard. Do not install the Software if you are not accustomed
+to using experimental software.
+
+Limitation of Liability
+
+TO THE EXTENT NOT PROHIBITED BY LAW, IN NO EVENT WILL HEWLETT-PACKARD
+OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR
+FOR SPECIAL, INDIRECT, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES,
+HOWEVER CAUSED REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+OR RELATED TO THE FURNISHING, PERFORMANCE, OR USE OF THE SOFTWARE, OR
+THE INABILITY TO USE THE SOFTWARE, EVEN IF HEWLETT-PACKARD HAS BEEN
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. FURTHERMORE, SINCE THE
+SOFTWARE IS PROVIDED WITHOUT CHARGE, YOU AGREE THAT THERE HAS BEEN NO
+BARGAIN MADE FOR ANY ASSUMPTIONS OF LIABILITY OR DAMAGES BY
+HEWLETT-PACKARD FOR ANY REASON WHATSOEVER, RELATING TO THE SOFTWARE OR
+ITS MEDIA, AND YOU HEREBY WAIVE ANY CLAIM IN THIS REGARD.
+
+*/
 package org.smartfrog.services.filesystem;
 
 import org.smartfrog.sfcore.common.SmartFrogException;
-import org.smartfrog.sfcore.prim.PrimImpl;
 
 import java.rmi.RemoteException;
 import java.io.File;
@@ -44,9 +74,9 @@ public class FileExistsImpl extends FileUsingComponentImpl implements FileExists
      */
     public boolean evaluate() throws RemoteException, SmartFrogException {
         File f = getFile();
-        boolean debug=sfLog().isDebugEnabled();
-        if(!f.exists()) {
-            if(debug) sfLog().debug("Does not exist: "+file);
+        boolean debug = sfLog().isDebugEnabled();
+        if (!f.exists()) {
+            if (debug) sfLog().debug("Does not exist: " + file);
             return false;
         }
         boolean isFile = f.isFile();
@@ -57,8 +87,10 @@ public class FileExistsImpl extends FileUsingComponentImpl implements FileExists
         if (isDir && !canBeDir) {
             if (debug) sfLog().debug("Is a directory: " + file);
         }
-        if(minSize >= 0 && f.length() < minSize) {
-            if (debug) sfLog().debug("Too short " + file +" - size of "+f.length()+" is below the minSize of "+minSize);
+        if (minSize >= 0 && f.length() < minSize) {
+            if (debug) {
+                sfLog().debug("Too short " + file + " - size of " + f.length() + " is below the minSize of " + minSize);
+            }
             return false;
         }
         return true;
