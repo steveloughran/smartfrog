40d7811d3b22b704f1f87feb1f8d611b06347c15
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-04-07 20:04:03 +0000

    dependencies refactor, example: manents.sf

--- core/smartfrog/src/org/smartfrog/services/dependencies/examples/ManagedEntity.java
+++ core/smartfrog/src/org/smartfrog/services/dependencies/examples/ManagedEntity.java
@@ -8,25 +8,30 @@ import org.smartfrog.sfcore.prim.Prim;
 
 public class ManagedEntity extends ThreadedState implements Prim {
 	
-	public ManagedEntity() throws RemoteException {}  
+	public ManagedEntity() throws RemoteException { 
+		super(); 
+	}  
 	
 	public boolean requireThread(HashMap data){ return true; }  
 	
 	public HashMap threadBody(HashMap data){
+		
 		HashMap save = new HashMap();
 		
+		boolean created = ((Boolean) data.get("created")).booleanValue();
 		boolean createdprev = ((Boolean) data.get("createdprev")).booleanValue();
 		boolean removedsucc = ((Boolean) data.get("removedsucc")).booleanValue();
 		String name = (String) data.get("name");
 		
-		if (createdprev) {
+		if (createdprev && !created) {
 			System.out.println(name+" setting created to true");
 			save.put("created", true);
 		}
+		
 		if (removedsucc) {
 			System.out.println(name+" setting removed to true");
 			save.put("removed", true);
-		}
+		} 
 		
 		return save;
 	}
