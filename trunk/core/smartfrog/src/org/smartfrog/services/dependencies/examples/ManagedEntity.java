662d418cc7541d00fbe424ec07b1bead90ebc37e
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-04-09 17:05:24 +0000

    trivial delta

--- trunk/core/smartfrog/src/org/smartfrog/services/dependencies/examples/ManagedEntity.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/dependencies/examples/ManagedEntity.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.util.HashMap;
 
 import org.smartfrog.services.dependencies.statemodel.state.ThreadedState;
+import org.smartfrog.sfcore.common.Context;
 import org.smartfrog.sfcore.prim.Prim;
 
 public class ManagedEntity extends ThreadedState implements Prim {
@@ -21,15 +22,39 @@ public class ManagedEntity extends ThreadedState implements Prim {
 		boolean removedsucc = ((Boolean) data.get("removedsucc")).booleanValue();
 		String name = (String) data.get("name");
 		
+		//For test...
+		String cur_output="";
+		Context parent=null;
+		try { 
+			parent = sfParent().sfContext();
+			cur_output = (String) parent.get("output"); 
+		} catch(Exception e){} 
+		
+		if (parent!=null) System.out.println(""+parent);
+		//
+		
 		if (createdprev && !created) {
 			System.out.println(name+" setting created to true");
 			save.put("created", true);
+			
+			//For test
+			cur_output+=(name+"c");
+			//
 		}
 		
 		if (removedsucc) {
 			System.out.println(name+" setting removed to true");
 			save.put("removed", true);
+			
+			//For test
+			cur_output+=(name+"r");
+			//
 		} 
+		
+		//For test
+		parent.put("output",cur_output); 
+		if (parent!=null) System.out.println(""+parent);
+		//
 		
 		return save;
 	}
