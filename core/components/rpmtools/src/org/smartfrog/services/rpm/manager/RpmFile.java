77732a8d94f353b8c5311a30e02b41ae3c309bb0
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-04-16 16:49:05 +0000

    SFOS-454 	 Make RPM code in avalanche self contained component, and production ready -some new components

---
+++ core/components/rpmtools/src/org/smartfrog/services/rpm/manager/RpmFile.java
@@ -0,0 +1,192 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.rpm.manager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+import java.util.Vector;
+
+/**
+ * Serializable RPM class, which models much of the RPM.
+ */
+
+public final class RpmFile implements Serializable, RpmErrors {
+
+    /**
+     * RPM name
+     */
+    public String name;
+
+    /**
+     * RPM Version string
+     */
+    public String version;
+
+    /**
+     * Any description for error messages
+     */
+    public String description;
+
+    /**
+     * Filename of the RPM
+     */
+    public String rpmFile;
+
+    /**
+     * Delete the file during termination?
+     */
+    public boolean deleteOnTermination;
+
+    /**
+     * A list of managed files
+     */
+    public Vector<String> managedFiles = new Vector<String>(0);
+
+
+    /**
+     * Simple constructor. The ManagedFiles list is set to an empty list
+     */
+    public RpmFile() {
+    }
+
+    /**
+     * Full constructor
+     *
+     * @param name         RPM name
+     * @param version      RPM version
+     * @param description  description
+     * @param rpmFile      the RPM file itself
+     * @param managedFiles a list of managed files, can be null
+     */
+    public RpmFile(String name, String version, String description, String rpmFile,
+                   Vector<String> managedFiles) {
+        this.name = name;
+        this.version = version;
+        this.description = description;
+        this.rpmFile = rpmFile;
+        if (managedFiles != null) {
+            this.managedFiles = managedFiles;
+        } else {
+            this.managedFiles = new Vector<String>(0);
+        }
+    }
+
+    /**
+     * Equality is based purely on name, not even the version or filename gets a look in.
+     *
+     * @param o the other object
+     * @return the object
+     */
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RpmFile that = (RpmFile) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Hash Code
+     *
+     * @return the hash code
+     */
+    public int hashCode() {
+        int result;
+        result = (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return a string representation of the object.
+     */
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append("RPM file").append(name).append('-').append(version);
+        b.append('\n');
+        b.append(rpmFile);
+        if (deleteOnTermination) {
+            b.append("\n-deleting on termination");
+        }
+
+        if (description != null) {
+            b.append('\n').append(description);
+        }
+
+        return super.toString();
+    }
+
+    /**
+     * Add another managed file to the list
+     *
+     * @param filename file to add
+     */
+    public void addManagedFile(String filename) {
+        managedFiles.add(filename);
+    }
+
+    /**
+     * Validate the RPM file itself, by checking it is there and not a directory.
+     *
+     * @throws FileNotFoundException if the file is not there.
+     */
+    public void validateRpmFile() throws FileNotFoundException {
+        File file = toFile();
+        if (!file.exists()) {
+            throw new FileNotFoundException(ERROR_NO_SUCH_FILE + file + '\n' + this);
+        }
+
+        if (!file.isFile()) {
+            throw new FileNotFoundException(ERROR_NOT_AN_RPM_FILE + file + '\n' + this);
+        }
+    }
+
+    /**
+     * Convert to a file in the local filesystem syntax
+     *
+     * @return
+     */
+    public File toFile() {
+        return new File(rpmFile);
+    }
+
+    /**
+     * Iterate through all the managed files and verify that they exist
+     *
+     * @throws FileNotFoundException if one is missing
+     */
+    public void verifyAllManagedFilesExist() throws FileNotFoundException {
+        for (String filename : managedFiles) {
+            File file = new File(filename);
+            if (!file.exists()) {
+                throw new FileNotFoundException(ERROR_NO_SUCH_FILE + file + '\n' + this);
+            }
+        }
+    }
+}
