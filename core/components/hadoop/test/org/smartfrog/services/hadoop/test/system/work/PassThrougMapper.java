669dc6898bb97ff09dc701ae7690aa1cb6b3b098
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2008-04-23 10:00:42 +0000

    SFOS-787 	 SFOS-780 subclass Configuration with SF support

---
+++ core/components/hadoop/test/org/smartfrog/services/hadoop/test/system/work/PassThrougMapper.java
@@ -0,0 +1,42 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.hadoop.test.system.work;
+
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.Writable;
+
+import java.io.IOException;
+
+/**
+ *
+ * Created 22-Apr-2008 16:39:56
+ *
+ */
+
+public class PassThrougMapper extends MapReduceBase {
+
+
+
+}
