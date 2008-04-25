1a8ac859bb5ee770c60d9804e2361fe95b706ef3
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-04-25 16:25:17 +0000

    SFOS-454   	 Make RPM code in avalanche self contained component, and production ready

--- trunk/core/components/sfinstaller/src/org/smartfrog/services/sfinstaller/FileGen.java
+++ trunk/core/components/sfinstaller/src/org/smartfrog/services/sfinstaller/FileGen.java
@@ -30,6 +30,9 @@ ITS MEDIA, AND YOU HEREBY WAIVE ANY CLAIM IN THIS REGARD.
 
 */
 package org.smartfrog.services.sfinstaller;
+
+import org.smartfrog.services.filesystem.FileSystem;
+
 import java.io.PrintStream;
 import java.io.FileOutputStream;
 import java.io.BufferedWriter;
@@ -38,102 +41,111 @@ import java.util.Map;
 import java.util.Collection;
 import java.util.Iterator;
 
-/** A class for creating the data file from map 
- * The data file is read for creating the sfinstaller description file
+/**
+ * A class for creating the data file from map The data file is read for creating the sfinstaller description file
  *
  * @author Ritu Sabharwal
  */
-public class FileGen { 
+public class FileGen {
 
-	/** The seperator for fields of data file*/
-	static char seperator = (char)255;
+    /**
+     * The seperator for fields of data file
+     */
+    static char seperator = (char) 255;
 
-	/** The Daemon object to be read from map*/
-	static Daemon host ;
-	
-	/** An iterator for entries in a map.*/
-	static Iterator t;
-	
-       /** Iterator over the map entries and writes to data file
-   	* 
-   	* @param mapFile map with daemon objects
-   	*/
-	static public void createFile(Map mapFile, String inFileName) throws Exception{
-	  try {
-		  if (!mapFile.isEmpty()) {
-		  	Collection values = mapFile.values();
-			t = values.iterator();
-		  } 
+    /**
+     * The Daemon object to be read from map
+     */
+    static Daemon host;
 
-	  PrintStream out = new PrintStream(new FileOutputStream(inFileName, true));
-	  BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
-	  
-	  while (t.hasNext()) {
-		host = (Daemon)t.next();
-	  writer.write(host.name);
-          writer.write(seperator);
-	  writer.write(host.os);
-          writer.write(seperator);
-	  writer.write(host.host);
-          writer.write(seperator);
-	  writer.write(host.transfertype);
-          writer.write(seperator);
-	  writer.write(host.logintype);
-          writer.write(seperator);
-	  writer.write(host.user);
-          writer.write(seperator);
-	  writer.write(host.password);
-          writer.write(seperator);
-	  writer.write(host.localfile1);
-	  if (host.os.equals("windows")){
-          	writer.write(seperator);
-	  	writer.write(host.localfile2);
-          	writer.write(seperator);
-	  	writer.write(host.localfile3);
-	  }
-	  if (host.keyfile != null)  {
-          	writer.write(seperator);
-	  	writer.write(host.keyfile);
-	  }
-	  if (host.secproperties != null)  {
-          	writer.write(seperator);
-	  	writer.write(host.secproperties);
-	  }
-	  if (host.smartfrogjar != null)  {
-          	writer.write(seperator);
-	  	writer.write(host.smartfrogjar);
-	  }
-	  if (host.servicesjar != null)  {
-          	writer.write(seperator);
-	  	writer.write(host.servicesjar);
-	  }
-	  if (host.examplesjar != null)  {
-          	writer.write(seperator);
-	  	writer.write(host.examplesjar);
-	  }
-          writer.write(seperator);
-	  writer.write(host.releasename);
-	//  if (host.os.equals("windows")) {
-	    if (host.javahome != null) {
-          	writer.write(seperator);
-	  	writer.write(host.javahome);
-	    }
-	//  }
-          writer.write(seperator);
-	  writer.write(host.installdir);
-          writer.write(seperator);
-	  writer.write(host.emailto);
-          writer.write(seperator);
-	  writer.write(host.emailfrom);
-          writer.write(seperator);
-	  writer.write(host.emailserver);
-          writer.write(seperator);
-	  writer.write("\n");
-	  }
-	  writer.flush();
-          writer.close();   		  
-	  } catch (Exception ex) {
-		throw ex;
+    /**
+     * An iterator for entries in a map.
+     */
+    static Iterator t;
+
+    /**
+     * Iterator over the map entries and writes to data file
+     *
+     * @param mapFile map with daemon objects
+     */
+    static public void createFile(Map mapFile, String inFileName) throws Exception {
+        BufferedWriter writer=null;
+        try {
+            if (!mapFile.isEmpty()) {
+                Collection values = mapFile.values();
+                t = values.iterator();
+            }
+
+            PrintStream out = new PrintStream(new FileOutputStream(inFileName, true));
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            while (t.hasNext()) {
+                host = (Daemon) t.next();
+                writer.write(host.name);
+                writer.write(seperator);
+                writer.write(host.os);
+                writer.write(seperator);
+                writer.write(host.host);
+                writer.write(seperator);
+                writer.write(host.transfertype);
+                writer.write(seperator);
+                writer.write(host.logintype);
+                writer.write(seperator);
+                writer.write(host.user);
+                writer.write(seperator);
+                writer.write(host.password);
+                writer.write(seperator);
+                writer.write(host.localfile1);
+                if (host.os.equals("windows")) {
+                    writer.write(seperator);
+                    writer.write(host.localfile2);
+                    writer.write(seperator);
+                    writer.write(host.localfile3);
+                }
+                if (host.keyfile != null) {
+                    writer.write(seperator);
+                    writer.write(host.keyfile);
+                }
+                if (host.secproperties != null) {
+                    writer.write(seperator);
+                    writer.write(host.secproperties);
+                }
+                if (host.smartfrogjar != null) {
+                    writer.write(seperator);
+                    writer.write(host.smartfrogjar);
+                }
+                if (host.servicesjar != null) {
+                    writer.write(seperator);
+                    writer.write(host.servicesjar);
+                }
+                if (host.examplesjar != null) {
+                    writer.write(seperator);
+                    writer.write(host.examplesjar);
+                }
+                writer.write(seperator);
+                writer.write(host.releasename);
+                //  if (host.os.equals("windows")) {
+                if (host.javahome != null) {
+                    writer.write(seperator);
+                    writer.write(host.javahome);
+                }
+                //  }
+                writer.write(seperator);
+                writer.write(host.installdir);
+                writer.write(seperator);
+                writer.write(host.emailto);
+                writer.write(seperator);
+                writer.write(host.emailfrom);
+                writer.write(seperator);
+                writer.write(host.emailserver);
+                writer.write(seperator);
+                writer.write("\n");
+            }
+            writer.flush();
+            writer.close();
+            writer=null;
+        } finally {
+            FileSystem.close(writer);
 	  }
 	}
 }
