36089c6039f43ca96a29ce0624f48ae77b41314c
sfrits <sfrits@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-05-06 09:35:26 +0000

    SFOS-767: REST interface integrated with Avalanche.

--- core/extras/avalanche/AvalancheCore/bin/start.bat
+++ core/extras/avalanche/AvalancheCore/bin/start.bat
@@ -26,6 +26,8 @@ start openfire.exe
 set SECURITY_POLICY_FILE=%SFHOME%\private\sf.no.security.policy
 set CLASSPATH=%JAVA_HOME%\lib\tools.jar
 set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\bootstrap.jar
+move %CATALINA_HOME%\common\lib\smartfrog*.jar %CATALINA_HOME%\common\lib\smartfrog.jar
+set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\common\lib\smartfrog.jar 
 set SFSECURITY=
 if "%SECURITY_ON%"=="false" goto catalina
 if not defined SFPRIVATE set SFPRIVATE=%SFHOME%\private
@@ -33,8 +35,6 @@ if not defined SFHOSTNAME set SFHOSTNAME=host1
 set AVALANCHE_ROOT=%cd%
 set SECURITY_POLICY_FILE=%SFHOME%\private\sf.policy
 set SFSECURITY=-Dorg.smartfrog.sfcore.security.keyStoreName="%SFPRIVATE%\%SFHOSTNAME%\mykeys.st" -Dorg.smartfrog.sfcore.security.propFile="%SFPRIVATE%\%SFHOSTNAME%\SFSecurity.properties" -Dorg.smartfrog.sfcore.security.secureResourcesOff=true -Davalanche.home=%AVALANCHE_HOME% -Davalanche.root=%AVALANCHE_ROOT%
-set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\common\lib\smartfrog.jar 
-rem  -Dorg.smartfrog.sfcore.security.debug=true  -Dsun.rmi.loader.loglevel=VERBOSE
 : catalina
 java -classpath %CLASSPATH% -Djava.security.manager -Djava.security.policy=="%SECURITY_POLICY_FILE%" %SFSECURITY%  -Dcatalina.base="%CATALINA_HOME%" -Dcatalina.home="%CATALINA_HOME%" org.apache.catalina.startup.Bootstrap start
 rem call startup.bat
