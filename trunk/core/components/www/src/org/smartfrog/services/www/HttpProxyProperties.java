8591fb0b56bc388c7db8038b9b77b84f69ee650a
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-05-07 22:20:49 +0000

    add https proxy names

--- trunk/core/components/www/src/org/smartfrog/services/www/HttpProxyProperties.java
+++ trunk/core/components/www/src/org/smartfrog/services/www/HttpProxyProperties.java
@@ -26,8 +26,13 @@ package org.smartfrog.services.www;
  */
 
 public class HttpProxyProperties {
+
+    public static final String HTTP_PROXY_HOST = "http.proxyHost";
     public static final String HTTP_PROXY_PORT = "http.proxyPort";
     public static final String HTTP_PROXY_USER = "http.proxyUser";
     public static final String HTTP_PROXY_PASSWORD = "http.proxyPassword";
-    public static final String HTTP_PROXY_HOST = "http.proxyHost";
+    public static final String HTTPS_PROXY_HOST = "https.proxyHost";
+    public static final String HTTPS_PROXY_PORT = "https.proxyPort";
+    public static final String HTTPS_PROXY_USER = "https.proxyUser";
+    public static final String HTTPS_PROXY_PASSWORD = "https.proxyPassword";
 }
