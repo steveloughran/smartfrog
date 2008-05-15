fdc239a6a05c8b3ee54737aba9a16a02b2086441
Helge Mahrt <5497139+helgemahrt@users.noreply.github.com>
2008-05-15 08:53:34 +0000

    Fixed a bug where paths containing whitespaces led to errors when starting Avalanche.

--- core/extras/avalanche/AvalancheCore/bin/start.bat
+++ core/extras/avalanche/AvalancheCore/bin/start.bat
@@ -2,8 +2,8 @@
 setlocal
 if defined SFHOME goto continue1
 cd ..\..
-set AVALANCHE_HOME=%cd%\server
-set SFHOME=%AVALANCHE_HOME%\smartfrog\dist
+set AVALANCHE_HOME="%cd%\server"
+set SFHOME="%AVALANCHE_HOME%\smartfrog\dist"
 :continue1
 
 if defined XMPP_HOME goto continue2
@@ -19,22 +19,22 @@ set SFSECURITY_ON=ENABLED
 set SFSECURERESOURCES_OFF=ENABLED
 
 :start
-set PATH=%SFHOME%\bin;%XMPP_HOME%\bin;%TOMCAT_HOME%\bin;%PATH%
+set PATH="%SFHOME%\bin";"%XMPP_HOME%\bin";"%TOMCAT_HOME%\bin";%PATH%
 
 start sfDaemon.bat
 start openfire.exe
-set SECURITY_POLICY_FILE=%SFHOME%\private\sf.no.security.policy
-set CLASSPATH=%JAVA_HOME%\lib\tools.jar
-set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\bootstrap.jar
-move %CATALINA_HOME%\common\lib\smartfrog*.jar %CATALINA_HOME%\common\lib\smartfrog.jar
-set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\common\lib\smartfrog.jar 
+set SECURITY_POLICY_FILE="%SFHOME%\private\sf.no.security.policy"
+set CLASSPATH="%JAVA_HOME%\lib\tools.jar"
+set CLASSPATH=%CLASSPATH%;"%CATALINA_HOME%\bin\bootstrap.jar"
+move "%CATALINA_HOME%\common\lib\smartfrog*.jar" "%CATALINA_HOME%\common\lib\smartfrog.jar"
+set CLASSPATH=%CLASSPATH%;"%CATALINA_HOME%\common\lib\smartfrog.jar"
 set SFSECURITY=
 if "%SECURITY_ON%"=="false" goto catalina
-if not defined SFPRIVATE set SFPRIVATE=%SFHOME%\private
+if not defined SFPRIVATE set SFPRIVATE="%SFHOME%\private"
 if not defined SFHOSTNAME set SFHOSTNAME=host1
 set AVALANCHE_ROOT=%cd%
-set SECURITY_POLICY_FILE=%SFHOME%\private\sf.policy
-set SFSECURITY=-Dorg.smartfrog.sfcore.security.keyStoreName="%SFPRIVATE%\%SFHOSTNAME%\mykeys.st" -Dorg.smartfrog.sfcore.security.propFile="%SFPRIVATE%\%SFHOSTNAME%\SFSecurity.properties" -Dorg.smartfrog.sfcore.security.secureResourcesOff=true -Davalanche.home=%AVALANCHE_HOME% -Davalanche.root=%AVALANCHE_ROOT%
+set SECURITY_POLICY_FILE="%SFHOME%\private\sf.policy"
+set SFSECURITY=-Dorg.smartfrog.sfcore.security.keyStoreName="%SFPRIVATE%\%SFHOSTNAME%\mykeys.st" -Dorg.smartfrog.sfcore.security.propFile="%SFPRIVATE%\%SFHOSTNAME%\SFSecurity.properties" -Dorg.smartfrog.sfcore.security.secureResourcesOff=true -Davalanche.home="%AVALANCHE_HOME%" -Davalanche.root="%AVALANCHE_ROOT%"
 : catalina
 java -classpath %CLASSPATH% -Djava.security.manager -Djava.security.policy=="%SECURITY_POLICY_FILE%" %SFSECURITY%  -Dcatalina.base="%CATALINA_HOME%" -Dcatalina.home="%CATALINA_HOME%" org.apache.catalina.startup.Bootstrap start
 rem call startup.bat
