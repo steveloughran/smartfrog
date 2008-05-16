baa5033368efac04c9d4ad12bcc0b5c9190e59d3
helgemahrt <helgemahrt@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-05-16 12:28:20 +0000

    Commented out a hard-coded link.

--- trunk/core/extras/avalanche/AvalancheGUI/web/host_list_ajax.js
+++ trunk/core/extras/avalanche/AvalancheGUI/web/host_list_ajax.js
@@ -90,11 +90,11 @@ function updateHostList() {
                                     // Cell: Hostname
                                     var hostNameCell = row.insertCell(1);
                                     hostNameCell.className = "sorted";
-                                    var consoleLink = document.createElement("a");
-				    consoleLink.href = "http://16.181.41.84:8081/AvalancheGUI/rest/"+hosts[i].getAttribute("name")+"/3800/?responseType=HTML";
-				    consoleLink.appendChild(document.createTextNode(hosts[i].getAttribute("name")));
-				    //hostNameCell.appendChild(document.createTextNode(hosts[i].getAttribute("name")));
-				    hostNameCell.appendChild(consoleLink);
+                                    //var consoleLink = document.createElement("a");
+				    //consoleLink.href = "http://16.181.41.84:8081/AvalancheGUI/rest/"+hosts[i].getAttribute("name")+"/3800/?responseType=HTML";
+				    //consoleLink.appendChild(document.createTextNode(hosts[i].getAttribute("name")));
+				    hostNameCell.appendChild(document.createTextNode(hosts[i].getAttribute("name")));
+				    //hostNameCell.appendChild(consoleLink);
 
 
                                     // Cell: Manage
