eab7f3d2dff6874581cad89984508b91ecef178d
kumaravelg <kumaravelg@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-06-10 00:08:45 +0000

    an Attribute can be tagged as statedata/statenotify/statelisten and specify it's value type is lazy or not.

--- trunk/core/extras/AuthoringTool/src/org/smartfrog/authoringtool/emf/SimpleDependencyConnection.java
+++ trunk/core/extras/AuthoringTool/src/org/smartfrog/authoringtool/emf/SimpleDependencyConnection.java
@@ -18,6 +18,8 @@ import org.eclipse.emf.ecore.EObject;
  * <ul>
  *   <li>{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getSource <em>Source</em>}</li>
  *   <li>{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getTarget <em>Target</em>}</li>
+ *   <li>{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getRelevant <em>Relevant</em>}</li>
+ *   <li>{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getEnabled <em>Enabled</em>}</li>
  * </ul>
  * </p>
  *
@@ -78,5 +80,59 @@ public interface SimpleDependencyConnection extends EObject
    * @generated
    */
   void setTarget(Component value);
+
+  /**
+   * Returns the value of the '<em><b>Relevant</b></em>' attribute.
+   * The default value is <code>"0"</code>.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Relevant</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Relevant</em>' attribute.
+   * @see #setRelevant(String)
+   * @see org.smartfrog.authoringtool.emf.SmartfrogPackage#getSimpleDependencyConnection_Relevant()
+   * @model default="0"
+   * @generated
+   */
+  String getRelevant();
+
+  /**
+   * Sets the value of the '{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getRelevant <em>Relevant</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Relevant</em>' attribute.
+   * @see #getRelevant()
+   * @generated
+   */
+  void setRelevant(String value);
+
+  /**
+   * Returns the value of the '<em><b>Enabled</b></em>' attribute.
+   * The default value is <code>"0"</code>.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Enabled</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Enabled</em>' attribute.
+   * @see #setEnabled(String)
+   * @see org.smartfrog.authoringtool.emf.SmartfrogPackage#getSimpleDependencyConnection_Enabled()
+   * @model default="0"
+   * @generated
+   */
+  String getEnabled();
+
+  /**
+   * Sets the value of the '{@link org.smartfrog.authoringtool.emf.SimpleDependencyConnection#getEnabled <em>Enabled</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Enabled</em>' attribute.
+   * @see #getEnabled()
+   * @generated
+   */
+  void setEnabled(String value);
 
 } // SimpleDependencyConnection
