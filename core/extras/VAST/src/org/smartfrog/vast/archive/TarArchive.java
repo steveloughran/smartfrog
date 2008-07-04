5317df494aa12219dc7ba8315f7ab76bbd3f81bd
Helge Mahrt <5497139+helgemahrt@users.noreply.github.com>
2008-07-04 17:28:43 +0000

    SFOS-918 Moved the archive package a little bit and added axtracting functionality.

--- core/extras/VAST/src/org/smartfrog/vast/archive/TarArchive.java
+++ core/extras/VAST/src/org/smartfrog/vast/archive/TarArchive.java
@@ -18,10 +18,11 @@ For more information: www.smartfrog.org
 
 */
 
-package org.smartfrog.vast.architecture.archive;
+package org.smartfrog.vast.archive;
 
 import org.apache.tools.tar.TarEntry;
 import org.apache.tools.tar.TarOutputStream;
+import org.apache.tools.tar.TarInputStream;
 
 import java.io.IOException;
 import java.io.FileInputStream;
@@ -31,8 +32,12 @@ import java.io.File;
 public class TarArchive extends BaseArchive {
 	TarOutputStream out;
 
-	public void create(String inPath) throws IOException {
-		out = new TarOutputStream(new FileOutputStream(inPath));
+	public TarArchive(String inPath) {
+		ArchivePath = inPath;
+	}
+
+	public void create() throws IOException {
+		out = new TarOutputStream(new FileOutputStream(ArchivePath));
 	}
 
 	public void putNextEntry(String inPath, String inRelPath) throws IOException {
@@ -62,5 +67,49 @@ public class TarArchive extends BaseArchive {
 
 	public void close() throws IOException {
 		out.close();
+	}
+
+	public void extract(String inDestination) throws IOException {
+		File destFolder = new File(inDestination);
+		if (!destFolder.isDirectory())
+			throw new IOException("Destination is not a folder.");
+
+		// open the archive
+		TarInputStream in = new TarInputStream(new FileInputStream(ArchivePath));
+		TarEntry entry;
+		File dest;
+		byte [] buffer = new byte[BUFFER_SIZE];
+		int read;
+
+		// parse the entries
+		while ((entry = in.getNextEntry()) != null) {
+			dest = new File(destFolder.getPath() + "/" + entry.getName());
+			if (entry.isDirectory())
+				// create the directory
+				dest.mkdir();
+			else {
+				// create the file
+				FileOutputStream out = new FileOutputStream(dest);
+
+				// extract the data
+				while ((read = in.read(buffer, 0, BUFFER_SIZE)) > 0) {
+					out.write(buffer, 0, read);
+				}
+
+				// close the file
+				out.close();
+
+				// set file access rights
+				int mode = entry.getMode();
+				int owner = mode / 100;
+				int group = (mode - (owner * 100)) / 10;
+				dest.setExecutable((owner & 1) != 0, (group & 1) != 0);
+				dest.setWritable((owner & 2) != 0, (group & 2) != 0);
+				dest.setReadable((owner & 4) != 0, (group & 4) != 0);
+			}
+		}
+
+		// close the archive
+		in.close();
 	}
 }
