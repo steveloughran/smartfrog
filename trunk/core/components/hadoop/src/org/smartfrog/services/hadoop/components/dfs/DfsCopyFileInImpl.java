fe2abdb67af175e32c62bc810b5b8077ed030a4f
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-07-07 21:09:50 +0000

    SFOS-780 updates to deal with moving hadoop codebase

--- trunk/core/components/hadoop/src/org/smartfrog/services/hadoop/components/dfs/DfsCopyFileInImpl.java
+++ trunk/core/components/hadoop/src/org/smartfrog/services/hadoop/components/dfs/DfsCopyFileInImpl.java
@@ -19,13 +19,12 @@ For more information: www.smartfrog.org
 */
 package org.smartfrog.services.hadoop.components.dfs;
 
-import org.apache.hadoop.dfs.DistributedFileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.smartfrog.services.filesystem.FileSystem;
 import org.smartfrog.services.hadoop.conf.ManagedConfiguration;
 import org.smartfrog.services.hadoop.common.DfsUtils;
 import org.smartfrog.sfcore.common.SmartFrogException;
-import org.smartfrog.sfcore.common.SmartFrogRuntimeException;
 
 import java.io.File;
 import java.rmi.RemoteException;
