0cd532118591b4618db60846e0d1a56c91871d4a
aftereight <aftereight@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-08-05 20:39:44 +0000

    automated deployment of servers on remote machine, using SmartFrog

--- trunk/core/components/ssh/src/org/smartfrog/services/ssh/ScpBulkUpload.java
+++ trunk/core/components/ssh/src/org/smartfrog/services/ssh/ScpBulkUpload.java
@@ -34,4 +34,5 @@ public interface ScpBulkUpload extends SSHComponent, Files {
      * remote directory for this operation {@value}
      */
     String ATTR_REMOTE_DIR = "remoteDir";
+    String ATTR_RECURSIVE = "recursive";
 }
