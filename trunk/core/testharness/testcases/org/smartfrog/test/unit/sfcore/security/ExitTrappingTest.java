66e37652b8ee97f4690997438ea68bca5cd4a27c
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-10-28 17:13:51 +0000

    SFOS-1016   	 Catch System.exit() calls in third party libraries through a new SecurityManager

---
+++ trunk/core/testharness/testcases/org/smartfrog/test/unit/sfcore/security/ExitTrappingTest.java
@@ -0,0 +1,87 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.test.unit.sfcore.security;
+
+import junit.framework.TestCase;
+import org.smartfrog.sfcore.security.ExitTrappingSecurityManager;
+
+/**
+ * Created 28-Oct-2008 15:17:11
+ */
+
+public class ExitTrappingTest extends TestCase {
+    private ExitTrappingSecurityManager manager;
+
+    /**
+     * Constructs a test case with the given name.
+     */
+    public ExitTrappingTest(String name) {
+        super(name);
+    }
+
+    /**
+     * Sets up the fixture, for example, open a network connection. This method is called before a test is executed.
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        manager = new ExitTrappingSecurityManager();
+    }
+
+    public void testManagerConstruct() throws Throwable {
+        ExitTrappingSecurityManager.setSystemExitPermitted(true);
+        assertTrue(ExitTrappingSecurityManager.isSystemExitPermitted());
+        ExitTrappingSecurityManager.setSystemExitPermitted(false);
+        assertFalse(ExitTrappingSecurityManager.isSystemExitPermitted());
+    }
+
+    public void testManagerCheckEnabled() throws Throwable {
+        ExitTrappingSecurityManager.setSystemExitPermitted(true);
+        manager.checkExit(1);
+        //we should get here OK
+    }
+
+    public void testManagerCheckThrowsExceptionWhenBlocking() throws Throwable {
+        ExitTrappingSecurityManager.setSystemExitPermitted(false);
+        try {
+            manager.checkExit(1);
+            fail("expected failure");
+        } catch (ExitTrappingSecurityManager.SystemExitException expected) {
+            //all is well
+            assertEquals(1, expected.getStatus());
+        }
+    }
+
+    public void testSettingSecurityManager() throws Throwable {
+        if (System.getSecurityManager() != null) {
+            fail("There is a security manager already");
+        }
+        try {
+            System.setSecurityManager(manager);
+            System.exit(1);
+        } catch (ExitTrappingSecurityManager.SystemExitException expected) {
+            //all is well
+            assertEquals(1, expected.getStatus());
+        } finally {
+            System.setSecurityManager(null);
+        }
+    }
+
+}
