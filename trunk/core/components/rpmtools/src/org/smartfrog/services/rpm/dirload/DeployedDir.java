6a51e5fc668330638f81f161f7649961cc0f13ed
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2008-12-09 15:27:02 +0000

    SFOS-149 Add a component to deploy every .sf file in a well known directory

--- trunk/core/components/rpmtools/src/org/smartfrog/services/rpm/dirload/DeployedDir.java
+++ trunk/core/components/rpmtools/src/org/smartfrog/services/rpm/dirload/DeployedDir.java
@@ -20,27 +20,103 @@ For more information: www.smartfrog.org
 package org.smartfrog.services.rpm.dirload;
 
 import org.smartfrog.sfcore.prim.Prim;
+import org.smartfrog.sfcore.common.SmartFrogRuntimeException;
 
 import java.io.File;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URI;
 
 /**
  * Created 08-Dec-2008 16:49:38
  */
 
-public class DeployedDir {
+public class DeployedDir implements Comparable {
 
-  public File path;
+    private File path;
+    private File applicationFile;
 
-  public Prim application;
+    private Prim application;
 
-  public String name;
+    private String name;
 
-  public Status status;
+    private Status status = Status.NEW;
 
-  public enum Status {
-    NEW,
-    DEPLOYED,
-    TERMINATED,
-    FAILED,
-  }
+    public DeployedDir(File path, String name, String applicationFilename) {
+        this.path = path;
+        this.name = name;
+        applicationFile = new File(path, applicationFilename);
+    }
+
+    public File getPath() {
+        return path;
+    }
+
+    public File getApplicationFile() {
+        return applicationFile;
+    }
+
+
+    public void setApplication(Prim application) {
+        this.application = application;
+    }
+
+    public Prim getApplication() {
+        return application;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * test for the application file being valid
+     *
+     * @return true if the application file exists and is a simple file
+     */
+    public boolean isApplicationFileValid() {
+        return applicationFile.exists() && applicationFile.isFile();
+    }
+
+    /**
+     * Get the URL of the application file
+     *
+     * @return the URL
+     * @throws SmartFrogRuntimeException if it will not parse
+     */
+    public URL getURL() throws SmartFrogRuntimeException {
+        URI uri = getApplicationFile().toURI();
+        try {
+            return uri.toURL();
+        } catch (MalformedURLException e) {
+            throw new SmartFrogRuntimeException("URI=" + uri + " " + e, e);
+        }
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a negative integer, zero, or a positive
+     * integer as this object is less than, equal to, or greater than the specified object.
+     *
+     * the sorting is done using the application name as the comparison
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than
+     *         the specified object.
+     * @throws ClassCastException if the specified object's type prevents it from being compared to this object.
+     */
+    public int compareTo(Object o) {
+        DeployedDir other = (DeployedDir) o;
+        return name.compareTo(other.name);
+    }
+
+    public enum Status {
+        NEW,
+        DEPLOYED,
+        TERMINATED,
+        FAILED,
+    }
 }
