9887fa49da7d487b6a1852da06eb38dde1534df0
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2009-02-09 14:48:36 +0000

    SFOS-1100:

--- core/smartfrog/src/org/smartfrog/sfcore/languages/sf/constraints/ConstraintConstants.java
+++ core/smartfrog/src/org/smartfrog/sfcore/languages/sf/constraints/ConstraintConstants.java
@@ -1,3 +1,22 @@
+/** (C) Copyright 1998-2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+ */
 package org.smartfrog.sfcore.languages.sf.constraints;
 /**
  * Collection of constant literals used in 
@@ -5,6 +24,11 @@ package org.smartfrog.sfcore.languages.sf.constraints;
  *
  */
 public class ConstraintConstants {
+	
+	public static final String AGG_SPEC="sfIsAggregateSpecifier";
+	public static final String IGNORE_TAG="sfIgnoreLink";
+	
+	public static final String UNIFY="unify";
 	
 	public static final String PREFIX="prefix";
 	public static final String PREFIX_TAG="sfPrefix";
@@ -15,11 +39,20 @@ public class ConstraintConstants {
 	public static final String PRED="pred";
 	public static final String PRED_TAG="sfPred";
 	
-	public static final String SRC="source";
-	public static final String SRC_TAG="sfSource";
+	public static final String ARRAY="array";
+	public static final String ARRAY_TAG="sfArray";
 	
 	public static final String UPDATE="update";
+	public static final String UPDATES="aggregates";
 	public static final String UPDATE_TAG="sfUpdate";
+	
+	public static final String KEY="key";
+	public static final String KEY_TAG="sfKey";
+	
+	public final static String DEPLOY = "deployComponent";
+	
+	public static final String CONTEXT="context";
+	public static final String CONTEXT_TAG="sfContext";
 	
 	public static final String EXTENT="extent";
 	public static final String EXTENT_TAG="sfExtent";
@@ -32,6 +65,7 @@ public class ConstraintConstants {
 	
 	public static final String GUARD="guard";
 	public static final String GUARD_TAG="sfGuard";
+	public static final String GUARDS="guards";
 	
 	public static final String VALUE="value";
 	public static final String VALUE_TAG="sfValue";
@@ -56,8 +90,13 @@ public class ConstraintConstants {
 	public static final String IRANGE="integerrange";
 	
 	public static final String AUTOVAR="auto";
+	public static final String AUTOVAR_TAG="sfConstraintAutoVar";
+	
 	public static final String AUTOEFFECTS="postActions";
+	
 	public static final String USERVAR="user";
+	public static final String USERVAR_TAG="sfConstraintUserVar";
+	
 	public static final String DEFVAR="default";
 		
 	public static final String FunctionClassStatus="sfFunctionClassStatus";
