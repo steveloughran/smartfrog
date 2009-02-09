b2eb563541da8221a7f2861ac93215bdba9f8289
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2009-02-09 14:31:45 +0000

    SFOS-1103: Move of legacy dependencies

--- trunk/core/smartfrog/src/org/smartfrog/services/dependencies/legacy/statemodel/dependency/DependencyValidation.java
+++
@@ -1,9 +0,0 @@
-package org.smartfrog.services.dependencies.legacy.statemodel.dependency;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface DependencyValidation extends Remote {
-   public boolean isEnabled() throws RemoteException;
-}
