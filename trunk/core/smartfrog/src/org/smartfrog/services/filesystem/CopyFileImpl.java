2750e7d459c18084ed775d0eaf55eb585c3f45e8
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2009-02-25 16:29:31 +0000

    redo layout

--- trunk/core/smartfrog/src/org/smartfrog/services/filesystem/CopyFileImpl.java
+++ trunk/core/smartfrog/src/org/smartfrog/services/filesystem/CopyFileImpl.java
@@ -19,37 +19,39 @@
  */
 package org.smartfrog.services.filesystem;
 
-import java.io.IOException;
-import java.io.File;
-import java.rmi.RemoteException;
-
+import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.common.SmartFrogResolutionException;
 import org.smartfrog.sfcore.compound.Compound;
 import org.smartfrog.sfcore.compound.CompoundImpl;
-import org.smartfrog.sfcore.utils.ComponentHelper;
-import org.smartfrog.sfcore.common.*;
-import org.smartfrog.sfcore.prim.TerminationRecord;
 import org.smartfrog.sfcore.logging.Log;
+import org.smartfrog.sfcore.utils.ComponentHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.rmi.RemoteException;
 
 /**
- *   Implemetation for CopyFile component.
+ * Implemetation for CopyFile component.
  */
 public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
 
 
-    private File fromFile=null;
-    private File toFile=null;
-    private boolean copyOnDeploy=true;
+    private File fromFile = null;
+    private File toFile = null;
+    private boolean copyOnDeploy = true;
 
     /**
      * Constructor
-     * @throws RemoteException  In case of network/rmi error
+     *
+     * @throws RemoteException In case of network/rmi error
      */
     public CopyFileImpl() throws RemoteException {
     }
 
     /**
      * Get From file
+     *
      * @return File
      */
     public File getFromFile() {
@@ -58,6 +60,7 @@ public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
 
     /**
      * Get to file
+     *
      * @return File
      */
     public File getToFile() {
@@ -66,8 +69,8 @@ public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
 
 
     /**
-     * Can be called to start components. Subclasses should override to provide
-     * functionality Do not block in this call, but spawn off any main loops!
+     * Can be called to start components. Subclasses should override to provide functionality Do not block in this call,
+     * but spawn off any main loops!
      *
      * @throws org.smartfrog.sfcore.common.SmartFrogException
      *                                  failure while starting
@@ -75,23 +78,22 @@ public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
      */
     public synchronized void sfDeploy() throws SmartFrogException, RemoteException {
         super.sfDeploy();
-        copyOnDeploy=sfResolve(ATTR_COPY_ON_DEPLOY,copyOnDeploy,true);
+        copyOnDeploy = sfResolve(ATTR_COPY_ON_DEPLOY, copyOnDeploy, true);
         if (copyOnDeploy) {
             copyFile();
         }
     }
 
 
-
     /**
      * Copies file using attributes "source" and "destination" from description
-     * @throws SmartFrogException failure while copying
-     * @throws RemoteException  In case of network/rmi error
+     *
+     * @throws SmartFrogException           failure while copying
+     * @throws RemoteException              In case of network/rmi error
      * @throws SmartFrogResolutionException failure while resolving
      */
-    private void copyFile() throws SmartFrogException, RemoteException,
-        SmartFrogResolutionException {
-        String from= FileSystem.lookupAbsolutePath(this, ATTR_FROM,null,null,true,null);
+    private void copyFile() throws SmartFrogException, RemoteException {
+        String from = FileSystem.lookupAbsolutePath(this, ATTR_FROM, null, null, true, null);
         String to =
                 FileSystem.lookupAbsolutePath(this,
                         ATTR_TO,
@@ -99,50 +101,51 @@ public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
                         null,
                         true,
                         null);
-        fromFile=new File(from);
-        toFile=new File(to);
+        fromFile = new File(from);
+        toFile = new File(to);
         //if the destination is a directory, we copy the file in there
         //using its name as the key
-        if(toFile.isDirectory()) {
-            toFile=new File(toFile,fromFile.getName());
+        if (toFile.isDirectory()) {
+            toFile = new File(toFile, fromFile.getName());
         }
         //check that the sorce file exists and fail if it is not there
         if (!fromFile.exists()) {
             throw new SmartFrogDeploymentException("Unable to copy "
-                +fromFile.getAbsolutePath() +
-                " to " +
-                toFile.getAbsolutePath()
-            +" as the source file does not exist");
+                    + fromFile.getAbsolutePath() +
+                    " to " +
+                    toFile.getAbsolutePath()
+                    + " as the source file does not exist");
         }
-        if(fromFile.isDirectory()) {
+        if (fromFile.isDirectory()) {
             throw new SmartFrogDeploymentException("Unable to copy the directory"
-                + fromFile.getAbsolutePath() 
-                + " as directory copy is not implemented");
+                    + fromFile.getAbsolutePath()
+                    + " as directory copy is not implemented");
         }
-        Log log=sfLog();
-        if(log.isInfoEnabled()) {
-            log.info("Copying "+fromFile.getAbsolutePath()+" to "+toFile.getAbsolutePath());
+        Log log = sfLog();
+        if (log.isInfoEnabled()) {
+            log.info("Copying " + fromFile.getAbsolutePath() + " to " + toFile.getAbsolutePath());
         }
-        boolean overwrite=sfResolve(ATTR_OVERWRITE,true,true);
-        if(!overwrite && toFile.exists()) {
+        boolean overwrite = sfResolve(ATTR_OVERWRITE, true, true);
+        if (!overwrite && toFile.exists()) {
             log.info("Skipping copy as the destination file exists and 'overwrite'==false");
         } else {
             try {
-                FileSystem.fCopy( fromFile, toFile);
+                FileSystem.fCopy(fromFile, toFile);
             } catch (IOException ex) {
                 throw SmartFrogException.forward("Failed when copying "
-                        +fromFile.getAbsolutePath() +
-                        " to " +
-                        toFile.getAbsolutePath()
-                        ,ex);
+                        + fromFile.getAbsolutePath()
+                        + " to " +
+                        toFile.getAbsolutePath(),
+                        ex);
             }
         }
     }
 
     /**
      * we only create the directory at startup time, even though we bond at deploy time
+     *
      * @throws SmartFrogException failure while starting
-     * @throws RemoteException In case of network/rmi error
+     * @throws RemoteException    In case of network/rmi error
      */
     public synchronized void sfStart() throws SmartFrogException, RemoteException {
         super.sfStart();
@@ -152,11 +155,11 @@ public class CopyFileImpl extends CompoundImpl implements CopyFile, Compound {
                 copyFile();
             }
         } catch (SmartFrogException e) {
-            thrown=e;
+            thrown = e;
         } catch (RemoteException e) {
             thrown = e;
         }
-        new ComponentHelper(this).sfSelfDetachAndOrTerminate(null, "Copy",null,thrown);
+        new ComponentHelper(this).sfSelfDetachAndOrTerminate(null, "Copy", null, thrown);
     }
 
 }
