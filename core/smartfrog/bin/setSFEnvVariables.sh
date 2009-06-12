dc4fc05c9e85524baa381b8ca06ce16beca69583
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2009-06-12 13:03:35 +0000

    SFOS-1229 	 add a setSFEnvVariables shell script that is marked as conf in the RPMs, so can be overwritten with custom env variables

---
+++ core/smartfrog/bin/setSFEnvVariables.sh
@@ -0,0 +1,4 @@
+# This empty shell script is sourced by the SmartFrog entry points, so as to provide
+# an override point for anyone wishing to set up environment variables. 
+# You are free to add whatever you wish here; it is read by the shell script setSFProperties
+# It is marked in the RPM files as a configuration file -when upgrading, all customisations are preserved.
