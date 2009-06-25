962e9394f2faaf5f0e41b1961f80c25bee139ba0
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2009-06-25 10:41:07 +0000

    SFOS-1244   	 Add component to run Hadoop tools

--- trunk/core/smartfrog/src/org/smartfrog/sfcore/utils/Executable.java
+++ trunk/core/smartfrog/src/org/smartfrog/sfcore/utils/Executable.java
@@ -20,10 +20,15 @@ For more information: www.smartfrog.org
 package org.smartfrog.sfcore.utils;
 
 /**
- * Created 11-Jun-2008 11:51:59
+ * An execution interface. This is similar to Runnable, except that you can throw exceptions
  */
 
 
 public interface Executable {
+
+    /**
+     * Execute in a thread
+     * @throws Throwable if something goes wrong
+     */
     void execute() throws Throwable;
 }
