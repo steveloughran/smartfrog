89d2201125868c71bf6f42636a49d47d5902d06b
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2009-06-25 16:20:43 +0000

    SFOS-1230 	 no IPv6 please

--- core/extras/hadoop-cluster/src/org/smartfrog/extras/hadoop/cluster/services/scripts/setSFEnvVariables.sh
+++ core/extras/hadoop-cluster/src/org/smartfrog/extras/hadoop/cluster/services/scripts/setSFEnvVariables.sh
@@ -1,2 +1,4 @@
 # Switch to a less strict security manager, as Hadoop does not like a full one
 export SFSECURITYMANAGER=org.smartfrog.sfcore.security.ExitTrappingSecurityManager
+#IPv4 only, please
+export SF_OPTS="-Djava.net.preferIPv4Stack=true"
