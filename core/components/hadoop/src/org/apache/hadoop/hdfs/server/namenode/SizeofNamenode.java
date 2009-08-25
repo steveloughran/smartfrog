9f18c77fe11acbb9f5f555bea5193e8dd091fa14
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2009-08-25 10:46:30 +0000

    HDFS-559 Work out the memory consumption of NN artifacts on a compressed pointer JVM

---
+++ core/components/hadoop/src/org/apache/hadoop/hdfs/server/namenode/SizeofNamenode.java
@@ -0,0 +1,51 @@
+/* (C) Copyright 2008 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.apache.hadoop.hdfs.server.namenode;
+
+import org.smartfrog.services.hadoop.common.SizeOfAgent;
+import org.apache.hadoop.hdfs.protocol.Block;
+import org.apache.hadoop.fs.permission.PermissionStatus;
+import org.apache.hadoop.fs.permission.FsPermission;
+
+public class SizeofNamenode {
+
+    static PermissionStatus perms = new PermissionStatus("hadoop","hadoop",
+            new FsPermission((short)0));
+
+    public static long sizeOfINodeFile() {
+        return SizeOfAgent.sizeOf(new INodeFile());
+    }
+
+    public static long sizeOfINodeDirectory() {
+        return SizeOfAgent.sizeOf(new INodeDirectory("/",perms));
+    }
+    public static long sizeOfINodeDirectoryWithQuota() {
+        return SizeOfAgent.sizeOf(new INodeDirectoryWithQuota("/",perms,0,0));
+    }
+
+    public static long sizeOfBlockInfo() {
+        return SizeOfAgent.sizeOf(new BlockInfo(new Block(),0));
+    }
+
+
+
+
+}
--- core/components/hadoop/src/org/smartfrog/services/hadoop/common/SizeOfAgent.java
+++ core/components/hadoop/src/org/smartfrog/services/hadoop/common/SizeOfAgent.java
@@ -1,32 +1,111 @@
 package org.smartfrog.services.hadoop.common;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hdfs.server.namenode.SizeofNamenode;
+import org.apache.hadoop.hdfs.server.namenode.DatanodeDescriptor;
+
 import java.lang.instrument.Instrumentation;
 
 /**
- * Uses the Instrumentation API to determine the size of an entity
+ * Uses the Instrumentation API to determine the size of an entity.
+ *
+ *
+ * Needs to be built with a manifest that says Agent-Class or Premain-Class of org.smartfrog.services.hadoop.common.SizeOfAgent
  */
 public class SizeOfAgent {
 
+    /** This is the interface we only get if started as an agent */
     static Instrumentation instrumentation;
+    static final Log LOG = LogFactory.getLog(SizeOfAgent.class);
 
-    public static void premain(String agentArgs, Instrumentation inst) {
-        instrumentation = inst;
+
+    /**
+     * Calculate size of an item and print it
+     *
+     * @param name     object name object name: null to work it out
+     * @param instance instance to measure the size of
+     */
+    private static void printSizeof(String name, Object instance) {
+        long size = SizeOfAgent.sizeOf(instance);
+        printSizeof(
+                name == null ?
+                        instance.getClass().toString()
+                        : name,
+                size);
     }
 
+    /**
+     * Print out the size of an item, iff the size > 0
+     *
+     * @param name name to print out
+     * @param size size to print
+     */
+    private static void printSizeof(String name, long size) {
+        if (size >= 0) {
+            LOG.info("sizeof(" + name + ") = " + size);
+        } else {
+            LOG.info("sizeof(" + name + ") not known: sizeof agent is not installed");
+        }
+    }
+
+    /**
+     * Do we have the sizeof operator?
+     *
+     * @return true iff sizeof is working
+     */
     public static boolean hasSizeof() {
         return instrumentation != null;
     }
 
     /**
      * Determine the size of an instance. Only works if we are running with instrumentation
+     *
      * @param instance object instance to look at
+     *
      * @return the size, or -1 if we don't know
      */
     public static long sizeOf(Object instance) {
-        if(!hasSizeof()) {
+        if (!hasSizeof()) {
             return -1;
         }
         return instrumentation.getObjectSize(instance);
+    }
+
+    /**
+     * This is called on startup to measure the size
+     *
+     * @param agentArgs any agent arguments
+     * @param inst      the instrumetnation interface
+     */
+    public static void premain(String agentArgs, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    /**
+     * This is called if (somehow) the class has been dynamically registered as an agent
+     *
+     * @param agentArgs any agent arguments
+     * @param inst      the instrumetnation interface
+     */
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+
+    /**
+     * And here is a main file to print off the sizes
+     *
+     * @param args the arguments
+     *
+     * @throws Throwable on a failure
+     */
+    public static void main(String[] args) throws Throwable {
+        printSizeof("BlockInfo", SizeofNamenode.sizeOfBlockInfo());
+        printSizeof("INode", SizeofNamenode.sizeOfINodeFile());
+        printSizeof("INodeDirectory", SizeofNamenode.sizeOfINodeDirectory());
+        printSizeof("INodeDirectorywithQuota", SizeofNamenode.sizeOfINodeDirectoryWithQuota());
+        printSizeof("DatanodeDescriptor", new DatanodeDescriptor());
     }
 
 }
