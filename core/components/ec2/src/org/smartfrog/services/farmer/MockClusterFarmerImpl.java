13d4bf18105f521953da4d2ac98524226d7e9d42
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2009-09-11 20:53:34 +0000

    SFOS-1279 	 Create "Farmer" interface for talking (remotely) to a cluster farmer -add Mock and EC2 impls. This is the mock one.

--- core/components/ec2/src/org/smartfrog/services/farmer/MockClusterFarmerImpl.java
+++ core/components/ec2/src/org/smartfrog/services/farmer/MockClusterFarmerImpl.java
@@ -24,17 +24,27 @@ import org.smartfrog.sfcore.common.SmartFrogException;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
-public class MockClusterFarmerImpl extends AbstractClusterFarmer {
+/**
+ * This is a mock cluster, very inefficent, but as it is only for testing, there is no point adding any form of speedup,
+ * not even a little bit. Simpler is best.
+ */
+public class MockClusterFarmerImpl extends AbstractClusterFarmer implements ClusterFarmer {
 
-    int clusterLimit=1000;
-    int counter;
-    String domain = "internal";
-    String externalDomain = "external";
+    private int clusterLimit = 1000;
+    private int counter;
+    private String domain = "internal";
+    private String externalDomain = "external";
 
-    List<ClusterInstance> nodes = new ArrayList<ClusterInstance>();
+    private Map<String, String> roles = new HashMap<String, String>();
+
+    private List<ClusterNode> nodes = new LinkedList<ClusterNode>();
+
 
     public MockClusterFarmerImpl() throws RemoteException {
     }
@@ -50,49 +60,42 @@ public class MockClusterFarmerImpl extends AbstractClusterFarmer {
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public synchronized ClusterInstance[] create(String role, int min, int max) throws IOException, SmartFrogException {
+    public synchronized ClusterNode[] create(String role, int min, int max) throws IOException, SmartFrogException {
         validateClusterRange(min, max);
         if (clusterSpace() < min) {
             throw new NoClusterSpaceException();
         }
-        List<ClusterInstance> newnodes = new ArrayList<ClusterInstance>(max);
+        List<ClusterNode> newnodes = new ArrayList<ClusterNode>(max);
         for (int i = 0; i < max; i++) {
-            ClusterInstance clusterInstance = createOneInstance(role);
+            ClusterNode clusterInstance = createOneInstance(role);
             if (clusterInstance == null) {
                 break;
             }
             newnodes.add(clusterInstance);
         }
-        return newnodes.toArray(new ClusterInstance[newnodes.size()]);
-    }
-
-    private void validateClusterRange(int min, int max) throws SmartFrogException {
-        if (max < min) {
-            throw new SmartFrogException("The maximum number of machines requested was less than the minimum");
-        }
+        return newnodes.toArray(new ClusterNode[newnodes.size()]);
     }
 
     /**
      * Create a node and add it to the list
      *
-     * @param role
-     *
+     * @param role role to create
      * @return the instance, or null if there is no room
      */
-    private synchronized ClusterInstance createOneInstance(String role) {
-        if (clusterSpace()>0) {
-            ClusterInstance node = new ClusterInstance();
+    private synchronized ClusterNode createOneInstance(String role) {
+        if (clusterSpace() > 0) {
+            ClusterNode node = new ClusterNode();
             nodes.add(node);
             counter++;
             String machinename = "host" + counter;
-            node.id = machinename;
-            node.hostname = machinename + "." + domain;
-            node.externalHostname = machinename + "." + externalDomain;
-            node.role = role;
+            node.setId(machinename);
+            node.setHostname(machinename + "." + domain);
+            node.setExternalHostname(machinename + "." + externalDomain);
+            node.setRole(role);
             return node;
         } else {
             return null;
@@ -100,95 +103,160 @@ public class MockClusterFarmerImpl extends AbstractClusterFarmer {
     }
 
     /**
+     * Add a role to the list of allowed roles. No way to remove them.
+     * @param role role to add
+     */
+    public void addRole(String role) {
+        roles.put(role, role);
+    }
+
+    /**
+     * Is the role allowed
+     * @param role to ask for
+     * @return true iff the role is supported
+     */
+    public boolean roleAllowed(String role) {
+        return roles.size()==0 || roles.containsKey(role);
+    }
+
+    /**
      * Get the current cluster space
-     * @return the number of machines between the current size and that of the cluster limit.
-     * This may be less than zero, if the cluster limit was reduced
+     *
+     * @return the number of machines between the current size and that of the cluster limit. This may be less than
+     *         zero, if the cluster limit was reduced
      */
     private synchronized int clusterSpace() {
-        return clusterLimit-nodes.size();
+        return clusterLimit - nodes.size();
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public ClusterInstance lookup(String id) throws IOException, SmartFrogException {
+    public ClusterNode lookup(String id) throws IOException, SmartFrogException {
+        for (ClusterNode node : nodes) {
+            if (id.equals(node.getId())) {
+                return node;
+            }
+        }
         return null;
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public ClusterInstance lookupByHostname(String hostname) throws IOException, SmartFrogException {
+    public ClusterNode lookupByHostname(String hostname) throws IOException, SmartFrogException {
+        for (ClusterNode node : nodes) {
+            if (hostname.equals(node.getHostname())) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Find a node in a role
+     *
+     * @param role role to look for
+     * @return a node in this role, or null
+     */
+    protected ClusterNode nodeInRole(String role) {
+        for (ClusterNode node : nodes) {
+            if (role.equals(node.getRole())) {
+                return node;
+            }
+        }
         return null;
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
     public void delete(String id) throws IOException, SmartFrogException {
+        ClusterNode node = lookup(id);
+        nodes.remove(node);
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
     public void delete(String[] nodesToDelete) throws IOException, SmartFrogException {
+        for (String nodeID : nodesToDelete) {
+            delete(nodeID);
+        }
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public void delete(ClusterInstance[] nodesToDelete) throws IOException, SmartFrogException {
+    public void delete(ClusterNode[] nodesToDelete) throws IOException, SmartFrogException {
+        for (ClusterNode node : nodesToDelete) {
+            nodes.remove(node);
+        }
+    }
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IOException IO/network problems
+     * @throws SmartFrogException other problems
+     */
+    @Override
+    public int deleteAllInRole(String role) throws IOException, SmartFrogException {
+        ClusterNode node;
+        int count = 0;
+        while ((node = nodeInRole(role)) != null) {
+            nodes.remove(node);
+            count++;
+        }
+        return count;
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public void deleteAllInRole(String role) throws IOException, SmartFrogException {
+    public ClusterNode[] list() throws IOException, SmartFrogException {
+        return nodes.toArray(new ClusterNode[nodes.size()]);
     }
 
     /**
      * {@inheritDoc}
      *
-     * @throws IOException        IO/network problems
+     * @throws IOException IO/network problems
      * @throws SmartFrogException other problems
      */
     @Override
-    public ClusterInstance[] list() throws IOException, SmartFrogException {
-        return new ClusterInstance[0];
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws IOException        IO/network problems
-     * @throws SmartFrogException other problems
-     */
-    @Override
-    public ClusterInstance[] list(String role) throws IOException, SmartFrogException {
-        return new ClusterInstance[0];
+    public ClusterNode[] list(String role) throws IOException, SmartFrogException {
+        List<ClusterNode> result = new ArrayList<ClusterNode>(nodes.size());
+        for (ClusterNode node: nodes) {
+            if (role.equals(node.getRole())) {
+                result.add(node);
+            }
+        }
+        return result.toArray(new ClusterNode[result.size()]);
     }
 }
