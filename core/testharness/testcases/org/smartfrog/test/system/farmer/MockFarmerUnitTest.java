d4c0d7dc74e33fa21a65ad7ab6198a4ee937010e
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2009-09-16 15:29:56 +0000

    SFOS-1279 	 Create "Farmer" interface for talking (remotely) to a cluster farmer -moving to sfServices

---
+++ core/testharness/testcases/org/smartfrog/test/system/farmer/MockFarmerUnitTest.java
@@ -0,0 +1,85 @@
+package org.smartfrog.test.system.farmer;
+
+import junit.framework.TestCase;
+import org.smartfrog.services.farmer.ClusterNode;
+import org.smartfrog.services.farmer.MockClusterFarmerImpl;
+import org.smartfrog.services.farmer.NoClusterSpaceException;
+import org.smartfrog.sfcore.common.SmartFrogException;
+
+import java.io.IOException;
+
+/**
+ * This walks the mock farmer through its life
+ */
+public class MockFarmerUnitTest extends TestCase {
+
+    private MockClusterFarmerImpl farmer;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        farmer = new MockClusterFarmerImpl();
+    }
+
+    public void testAddListRemove() throws Throwable {
+        ClusterNode[] first = farmer.create("first", 1, 1);
+        assertEquals(1, first.length);
+        ClusterNode[] nodes = farmer.create("test", 1, 1);
+        assertEquals(1, nodes.length);
+        ClusterNode node = nodes[0];
+        ClusterNode[] listed = listByRole("test", 1);
+        assertSame(listed[0], nodes[0]);
+        listByRole("other", 0);
+        ClusterNode node2 = farmer.lookup(node.getId());
+        assertSame(node, node2);
+        node2 = farmer.lookupByHostname(node.getHostname());
+        assertSame(node, node2);
+    }
+
+    public void testBulkCreate() throws Throwable {
+        ClusterNode[] third = farmer.create("third", 4, 4);
+        assertEquals(4, third.length);
+        listByRole("third", 4);
+    }
+
+    public void testNoNegatives() throws Throwable {
+        try {
+            farmer.create("master", -10, 1);
+            fail("should not have reached here");
+        } catch (SmartFrogException e) {
+            //expected
+        }
+    }
+
+    public void testNoRoomAtAll() throws Throwable {
+        farmer.setClusterLimit(2);
+        try {
+            farmer.create("master", 7, 20);
+            fail("should not have reached here");
+        } catch (NoClusterSpaceException e) {
+            //expected
+        }
+    }
+
+    public void testNotFullySatisfied() throws Throwable {
+        farmer.setClusterLimit(2);
+        ClusterNode[] nodes = farmer.create("master", 1, 20);
+        assertEquals(2, nodes.length);
+        //and now there should be no room
+        try {
+            farmer.create("master", 1, 1);
+            fail("should not have reached here");
+        } catch (NoClusterSpaceException e) {
+            //expected
+        }
+    }
+
+    private ClusterNode[] listByRole(String role, int expected) throws IOException, SmartFrogException {
+        ClusterNode[] listed;
+        listed = farmer.list(role);
+        assertEquals("Expected to find " + expected + " nodes of role '" + role + "' but got" + listed.length,
+                expected, listed.length);
+        return listed;
+    }
+
+}
