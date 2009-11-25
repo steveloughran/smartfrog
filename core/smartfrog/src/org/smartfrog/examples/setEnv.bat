2bab9b81827d7312e98f1e37d34fe4d8d234cac8
julgui <julgui@users.noreply.github.com>
2009-11-25 10:55:02 +0000

    Added "" to avoid problems with paths using spaces in windows.

--- core/smartfrog/src/org/smartfrog/examples/setEnv.bat
+++ core/smartfrog/src/org/smartfrog/examples/setEnv.bat
@@ -4,6 +4,6 @@ rem Batch file for setting SFHOME & PATH variables for executing the examples.
 rem ------------------------------------------------------------------------
 
 set SFHOME=..\..\..\..\..\dist
-set PATH=%SFHOME%\bin;%PATH%
+set PATH="%SFHOME%\bin";%PATH%
 set CLASSPATH=.;%CLASSPATH%;
 
