68a73ca6ba8063f01be8acd3fce9b07e72d7f9fe
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2009-11-25 16:35:49 +0000

    SFOS-1385 Add FarmCheckRoles component to check both the role listing methods, and to verify the exact set of roles provided

---
+++ trunk/core/components/cloudfarmer/src/org/smartfrog/services/cloudfarmer/client/components/FarmCheckRoles.java
@@ -0,0 +1,122 @@
+/* (C) Copyright 2009 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+package org.smartfrog.services.cloudfarmer.client.components;
+
+import org.smartfrog.sfcore.common.SmartFrogException;
+import org.smartfrog.sfcore.utils.ListUtils;
+import org.smartfrog.sfcore.reference.Reference;
+import org.smartfrog.services.cloudfarmer.api.ClusterRoleInfo;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created 25-Nov-2009 14:41:35
+ */
+
+public class FarmCheckRoles extends AbstractFarmWorkflowClient implements FarmCustomer {
+
+    private String successText;
+    public static final String ATTR_ROLES = "roles";
+    public static final String ATTR_USE_LIST_CLUSTER_ROLES = "useListClusterRoles";
+
+    public FarmCheckRoles() throws RemoteException {
+    }
+
+    @Override
+    public String getSuccessText() {
+        return successText;
+    }
+
+    public void setSuccessText(String successText) {
+        this.successText = successText;
+    }
+
+    @Override
+    protected void startupAction() throws IOException, SmartFrogException {
+        boolean useListClusterRoles = sfResolve(ATTR_USE_LIST_CLUSTER_ROLES, true, true);
+        List<String> roles = ListUtils.resolveStringList(this, new Reference(ATTR_ROLES),true);
+        List<String> actual;
+        if(useListClusterRoles) {
+            actual = checkListClusterRoles(roles);
+        } else {
+            actual = checkListRoles(roles);
+        }
+        checkRoles(roles, actual);
+        setSuccessText("Role list as expected");
+    }
+
+    private List<String> checkListClusterRoles(List<String> roles) throws IOException, SmartFrogException {
+        ClusterRoleInfo[] clusterRoles = getFarmer().listClusterRoles();
+        List<String> actual = new ArrayList<String>(clusterRoles.length);
+        for (ClusterRoleInfo role:clusterRoles) {
+            actual.add(role.getName());
+        }
+        return actual;
+
+    }
+
+    private List<String> checkListRoles(List<String> roles) throws IOException, SmartFrogException {
+        String[] availableRoles = getFarmer().listAvailableRoles();
+        return Arrays.asList(availableRoles);
+    }
+
+
+    /**
+     * Check the roles
+     * @param roles expected
+     * @param actual actual
+     * @throws SmartFrogException
+     */
+    private void checkRoles(List<String> roles, List<String> actual) throws SmartFrogException {
+        Collections.sort(roles);
+        Collections.sort(actual);
+        String expectedString = stringify(roles);
+        String actualString = stringify(actual);
+        String expectedVsActual = "Expected " + expectedString + " actual " + actualString;
+        if(roles.size() != actual.size()) {
+            throw new SmartFrogException(" Mismatch in role count."
+                    + expectedVsActual);
+        }
+        Iterator<String> rolesIt = roles.iterator();
+        Iterator<String> actualIt = actual.iterator();
+        for (String role: roles) {
+            if (!(role.equals(actualIt.next()))) {            
+                throw new SmartFrogException("Different roles than expected. " + expectedVsActual);
+            }
+        }
+    }
+    
+    String stringify(List<String> roles) {
+        StringBuilder result = new StringBuilder("[ ");
+        for (String role:roles) {
+            result.append(role);
+            result.append(" ");
+        }
+        result.append("]");
+        return result.toString();
+    }
+
+}
