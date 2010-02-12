b75fb198344200478d611f935a37ec53788c5674
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2010-02-12 14:50:31 +0000

    SFOS-1435 csh/tcsh scripts build the path up wrong

--- core/release/scripts/etc/profile.d/smartfrog.sh
+++ core/release/scripts/etc/profile.d/smartfrog.sh
@@ -7,7 +7,7 @@
 export SFHOME="${rpm.install.dir}"
 
 # Add the directory of the SmartFrog executables to the path
-if echo ":$${PATH-}:" | grep ":$${SFHOME}:" > /dev/null 2>&1
+if echo ":$${PATH-}:" | grep ":$${SFHOME}" > /dev/null 2>&1
 then
 	:
 else
