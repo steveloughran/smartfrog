cf1fa21e01ef73ad8518cd81ad05f5896f4b78fd
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2010-12-03 15:02:53 +0000

    SFOS-1510 JavascriptTest failing - race condition in TestCompoundImpl

--- core/smartfrog/src/org/smartfrog/services/filesystem/files/DeleteFilesImpl.java
+++ core/smartfrog/src/org/smartfrog/services/filesystem/files/DeleteFilesImpl.java
@@ -20,14 +20,19 @@ For more information: www.smartfrog.org
 
 package org.smartfrog.services.filesystem.files;
 
+import org.smartfrog.sfcore.common.SmartFrogDeploymentException;
 import org.smartfrog.sfcore.common.SmartFrogException;
 
+import java.io.File;
 import java.rmi.RemoteException;
+import java.util.Set;
 
 /**
  * Delete a set of files
  */
-public class DeleteFilesImpl extends FilesCompoundImpl {
+public class DeleteFilesImpl extends FilesCompoundImpl implements Files {
+
+    public static final String ATTR_CONTINUE_AFTER_DELETE_FAILURES = "continueAfterDeleteFailures";
 
     public DeleteFilesImpl() throws RemoteException {
     }
@@ -35,5 +40,27 @@ public class DeleteFilesImpl extends FilesCompoundImpl {
     @Override
     public void sfStart() throws SmartFrogException, RemoteException {
         super.sfStart();
+        //now delete the files
+        Set<File> files = getFileset();
+        execute(files, sfResolve(ATTR_CONTINUE_AFTER_DELETE_FAILURES, true, true));
+    }
+
+    private void execute(Set<File> files, boolean continueAfterFailures)
+            throws SmartFrogException, RemoteException {
+        for (File file : files) {
+            executeSingleFile(file, continueAfterFailures);
+        }
+    }
+
+    private void executeSingleFile(final File file, final boolean continueAfterFailures)
+            throws SmartFrogException, RemoteException {
+        if (!file.delete()) {
+            String message = "Failed to delete " + file;
+            if (continueAfterFailures) {
+                sfLog().info(message);
+            } else {
+                throw new SmartFrogDeploymentException(message);
+            }
+        }
     }
 }
