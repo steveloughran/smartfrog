3d5f9ec501a9e2f5ef675b20a4c10d2d27389181
anfarr <anfarr@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2011-03-17 18:07:08 +0000

    SFOS-1573: orchcomponent migration

--- trunk/core/smartfrog/src/org/smartfrog/services/orchcomponent/desiredstate/DesiredEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.smartfrog.services.orchcomponent.desiredstate;
-
-import org.smartfrog.sfcore.componentdescription.ComponentDescription;
-
-
-public interface DesiredEventHandler {
-	public void handleDesiredEvent(ComponentDescription base);
-}
