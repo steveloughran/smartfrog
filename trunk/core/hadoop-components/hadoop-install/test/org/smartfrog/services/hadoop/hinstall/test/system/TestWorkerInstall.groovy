01ee236a68aa8040725789cc9937af9e231422aa
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2011-06-26 18:23:52 +0000

    SFOS-1608 	create hadoop-installer component to untar and set up hadoop worker or master nodes

---
+++ trunk/core/hadoop-components/hadoop-install/test/org/smartfrog/services/hadoop/hinstall/test/system/TestWorkerInstall.groovy
@@ -0,0 +1,19 @@
+package org.smartfrog.services.hadoop.hinstall.test.system
+
+import org.smartfrog.services.scripting.groovy.GroovyTestBase
+
+/**
+ *
+ */
+class TestWorkerInstall extends GroovyTestBase {
+
+    final String PACKAGE = "/org/smartfrog/services/hadoop/hinstall/test/system/"
+    /**
+     * This test verifies that the worker can be installed without having to put in the files
+     */
+    
+    void testWorkerFiles() {
+     //   expectSuccessfulTestRunOrSkip(PACKAGE, "")
+    }
+    
+}
