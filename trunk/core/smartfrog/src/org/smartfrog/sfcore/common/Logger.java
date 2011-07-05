16696db977196c531e06dfcdd50eab060a08137d
steve_l <steve_l@9868f95a-be1e-0410-b3e3-a02e98b909e6>
2011-07-05 10:19:55 +0000

    SFOS-1626 format first

--- trunk/core/smartfrog/src/org/smartfrog/sfcore/common/Logger.java
+++ trunk/core/smartfrog/src/org/smartfrog/sfcore/common/Logger.java
@@ -1,22 +1,22 @@
 /** (C) Copyright 1998-2004 Hewlett-Packard Development Company, LP
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-For more information: www.smartfrog.org
+ For more information: www.smartfrog.org
 
-*/
+ */
 
 package org.smartfrog.sfcore.common;
 
@@ -59,7 +59,7 @@ public class Logger implements MessageKeys {
     /** String name for optional attribute "{@value}". */
     public static final String ATR_TEST_URI = "testURI";
     /** String name for optional attribute "{@value}". */
-    public static final String ATR_TEST_SFDAEMON_NETWORK_PORT="testSFDaemonNetworkPort";
+    public static final String ATR_TEST_SFDAEMON_NETWORK_PORT = "testSFDaemonNetworkPort";
 
     /** String name for optional attribute "{@value}". */
     public static final String ATR_TEST_JAR_REPEAT = "testJarRepeat";
@@ -84,8 +84,8 @@ public class Logger implements MessageKeys {
     public static boolean testNetwork = true;
 
     /** Property to define a list of remote hosts for remote network test . The default value can be overridden by the
-      * value specified in default.ini file.
-      */
+     * value specified in default.ini file.
+     */
     public static String[] testURI = {SMARTFROG_URL};
 
     /** Property to enable initial daemon network port test. The default value can be overridden by the
@@ -94,56 +94,58 @@ public class Logger implements MessageKeys {
     public static boolean testSFDaemonNetworkPort = false;
 
     /** Property to define a list of remote hosts for remote network test . The default value can be overridden by the
-      * value specified in default.ini file.
-      */
+     * value specified in default.ini file.
+     */
     public static String[] testJarRepeat = {SMARTFROG_JAR, SFSERVICES_JAR};
 
     private static volatile boolean initialized = false;
 
     /** Property to define if to log classpaht during startup. The default value can be overridden by the
-      * value specified in default.ini file.
-      */
+     * value specified in default.ini file.
+     */
     public static boolean logClasspath = false;
 
     /**
      * empty constructor
      */
-    private Logger(){
+    private Logger() {
     }
 
     /**
      * Init method
      */
-    public static synchronized void  init() {
+    public static synchronized void init() {
         if (initialized) return;
         ComponentDescription configuration = null;
         //Check Class and read configuration...including system.properties
         try {
             configuration = ComponentDescriptionImpl.getClassComponentDescription(loggerPropertyBase, true, null);
-            if (configuration!=null){
-               logStackTrace = configuration.sfResolve(ATR_LOG_STACK_TRACE,logStackTrace,false);
-               logLiveness = configuration.sfResolve(ATR_LOG_LIVENESS,logLiveness,false);
-               logClasspath = configuration.sfResolve(ATR_LOG_CLASSPATH,logClasspath,false);
-               processCompoundDiagReport = configuration.sfResolve(ATR_LOG_PC_DIAG_REPORT,processCompoundDiagReport,false);
-               testNetwork = configuration.sfResolve(ATR_TEST_NETWORK,testNetwork,false);
-               testURI = configuration.sfResolve(ATR_TEST_URI,testURI,false);
-               testSFDaemonNetworkPort = configuration.sfResolve(ATR_TEST_SFDAEMON_NETWORK_PORT,testSFDaemonNetworkPort,false);
-               testJarRepeat = configuration.sfResolve(ATR_TEST_JAR_REPEAT,testJarRepeat,false);
+            if (configuration != null) {
+                logStackTrace = configuration.sfResolve(ATR_LOG_STACK_TRACE, logStackTrace, false);
+                logLiveness = configuration.sfResolve(ATR_LOG_LIVENESS, logLiveness, false);
+                logClasspath = configuration.sfResolve(ATR_LOG_CLASSPATH, logClasspath, false);
+                processCompoundDiagReport = configuration.sfResolve(ATR_LOG_PC_DIAG_REPORT, processCompoundDiagReport, false);
+                testNetwork = configuration.sfResolve(ATR_TEST_NETWORK, testNetwork, false);
+                testURI = configuration.sfResolve(ATR_TEST_URI, testURI, false);
+                testSFDaemonNetworkPort = configuration.sfResolve(ATR_TEST_SFDAEMON_NETWORK_PORT, testSFDaemonNetworkPort, false);
+                testJarRepeat = configuration.sfResolve(ATR_TEST_JAR_REPEAT, testJarRepeat, false);
             }
-        } catch (Exception ex){
-            if (SFSystem.sfLog().isErrorEnabled()) { SFSystem.sfLog().error(ex, ex); }
+        } catch (Exception ex) {
+            if (SFSystem.sfLog().isErrorEnabled()) {
+                SFSystem.sfLog().error(ex, ex);
+            }
         }
-        
+
         initialized = true;
     }
 
     public static void logStatus() {
-        if (logStackTrace&&(SFSystem.sfLog().isWarnEnabled())) {
-              SFSystem.sfLog().warn(MessageUtil.formatMessage(MSG_WARNING_STACKTRACE_ENABLED));
+        if (logStackTrace && (SFSystem.sfLog().isWarnEnabled())) {
+            SFSystem.sfLog().warn(MessageUtil.formatMessage(MSG_WARNING_STACKTRACE_ENABLED));
         }
 
         if (logLiveness && (SFSystem.sfLog().isWarnEnabled())) {
-          SFSystem.sfLog().warn(MessageUtil.formatMessage(MSG_WARNING_LIVENESS_ENABLED));
+            SFSystem.sfLog().warn(MessageUtil.formatMessage(MSG_WARNING_LIVENESS_ENABLED));
         }
         reportRepeatedJars();
 
@@ -158,8 +160,8 @@ public class Logger implements MessageKeys {
             String[] words = Logger.testJarRepeat;
             String[] classpath = (System.getProperty("java.class.path")).split(System.getProperty("path.separator"));
             StringBuffer message = Logger.getRepeatsMessage(words, classpath);
-            if (message !=null) {
-                SFSystem.sfLog().warn("Possible problem with classpath: \n"+message.toString());
+            if (message != null) {
+                SFSystem.sfLog().warn("Possible problem with classpath: \n" + message.toString());
             }
             // Check for repeated Jar files in code base
             String codebaseproperty = System.getProperty(SFClassLoader.SF_CODEBASE_PROPERTY);
@@ -179,7 +181,7 @@ public class Logger implements MessageKeys {
      * Return the intializaed status of Logger
      * @return boolean
      */
-    public static boolean initialized(){
+    public static boolean initialized() {
         return initialized;
     }
 
@@ -205,10 +207,10 @@ public class Logger implements MessageKeys {
      *  Method to search for repeted number of words in a String
      *
      * @param word Word to match
-    *  @param content array of strings to search
+     *  @param content array of strings to search
      * @return resultMessage  list of lines where the word was found  or NULL 1 or less found.
      */
-    public static String checkRepeatedWords (String word, String[] content) {
+    public static String checkRepeatedWords(String word, String[] content) {
         String regex = word;
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher("");         // Create Matcher
@@ -218,12 +220,12 @@ public class Logger implements MessageKeys {
         for (String line : content) {
             m.reset(line);
             while (m.find()) {
-                strb.append("\n    "+line);
+                strb.append("\n    " + line);
                 count++;
             }
         }
-        if (count >1) {
-           return (count + " occurrences for "+word + strb.toString()+"\n");
+        if (count > 1) {
+            return (count + " occurrences for " + word + strb.toString() + "\n");
         } else {
             return null;
         }
