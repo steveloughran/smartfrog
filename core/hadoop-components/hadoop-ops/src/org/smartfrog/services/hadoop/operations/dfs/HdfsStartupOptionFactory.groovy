236e287449299976536aef1443ce942ed60fd7d5
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2011-07-19 17:22:22 +0000

    SFOS-1633 	SFOS-1630 add sf component to bring up MiniDFS cluster and MiniMRCluster

---
+++ core/hadoop-components/hadoop-ops/src/org/smartfrog/services/hadoop/operations/dfs/HdfsStartupOptionFactory.groovy
@@ -0,0 +1,41 @@
+package org.smartfrog.services.hadoop.operations.dfs
+
+import org.apache.hadoop.hdfs.server.common.HdfsConstants.StartupOption
+
+/**
+ *   static public enum StartupOption{
+ *   FORMAT  ("-format"),
+ REGULAR ("-regular"),
+ UPGRADE ("-upgrade"),
+ ROLLBACK("-rollback"),
+ FINALIZE("-finalize"),
+ IMPORT  ("-importCheckpoint");
+
+ private String name = null;
+ private StartupOption(String arg) {this.name = arg;}public String getName() {return name;}}
+ */
+class HdfsStartupOptionFactory {
+
+    /**
+     * Create a startup option from a string value.
+     * @param option the option as a command-line string
+     * @return the action, or null for no match
+     */
+
+    public static StartupOption createStartupOption(String option) {
+        switch(option) {
+            case "-regular":
+                return StartupOption.REGULAR;
+            case "-format":
+                return StartupOption.FORMAT;
+            case "-rollback":
+                return StartupOption.ROLLBACK;
+            case "-finalize":
+                return StartupOption.FINALIZE;
+            case "-importCheckpoint":
+                return StartupOption.IMPORT;
+            default:
+                return null;
+        }
+    }
+}
---
+++ core/hadoop-components/hadoop-ops/test/org/smartfrog/services/hadoop/operations/test/system/instances/InstanceTestBase.groovy
@@ -0,0 +1,9 @@
+package org.smartfrog.services.hadoop.operations.test.system.instances
+
+import org.smartfrog.services.scripting.groovy.GroovyTestBase
+
+class InstanceTestBase extends GroovyTestBase {
+
+    protected String PACKAGE = "/org/smartfrog/services/hadoop/operations/test/system/instances/";
+
+}
