be3381d2710eefa7d1becd712572d957a2feefbf
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2011-09-15 14:48:58 +0000

    SFOS-1640 	SFOS-1630 test DFS operations against the MiniDFS cluster

---
+++ core/hadoop-components/hadoop-ops/src/org/smartfrog/services/hadoop/mapreduce/terasort/TeraGenJob.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.smartfrog.services.hadoop.mapreduce.terasort;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RunningJob;
+import org.smartfrog.services.hadoop.mapreduce.submitter.JobRunningTool;
+
+/**
+ * Generate the official terasort input data set.
+ * The user specifies the number of rows and the output directory and this
+ * class runs a map/reduce program to generate the data.
+ * The format of the data is:
+ * <ul>
+ * <li>(10 bytes key) (10 bytes rowid) (78 bytes filler) \r \n
+ * <li>The keys are random characters from the set ' ' .. '~'.
+ * <li>The rowid is the right justified row id as a int.
+ * <li>The filler consists of 7 runs of 10 characters from 'A' to 'Z'.
+ * </ul>
+ *
+ * <p>
+ * To run the program: 
+ * <b>bin/hadoop jar hadoop-examples-*.jar teragen 10000000000 in-dir</b>
+ */
+@SuppressWarnings({"deprecation"})
+
+public class TeraGenJob extends JobRunningTool {
+
+    static long getNumberOfRows(Configuration job) {
+        return job.getLong(TeraConstants.TERASORT_NUM_ROWS, 0);
+    }
+
+    static void setNumberOfRows(Configuration job, long numRows) {
+        job.setLong("terasort.num-rows", numRows);
+    }
+
+    @Override
+    public RunningJob runJob(String[] args) throws Exception {
+        JobConf job = (JobConf) getConf();
+        setNumberOfRows(job, Long.parseLong(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setJobName("TeraGen");
+        job.setJarByClass(TeraGenJob.class);
+        job.setMapperClass(TeraGenMapper.class);
+        job.setNumReduceTasks(0);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormat(TeraGenRangeInputFormat.class);
+        job.setOutputFormat(TeraOutputFormat.class);
+        return JobClient.runJob(job);
+    }
+
+
+}
---
+++ core/hadoop-components/hadoop-ops/src/org/smartfrog/services/hadoop/mapreduce/terasort/TeraGenMapper.java
@@ -0,0 +1,116 @@
+/* (C) Copyright 2010 Hewlett-Packard Development Company, LP
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For more information: www.smartfrog.org
+
+*/
+
+package org.smartfrog.services.hadoop.mapreduce.terasort;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+
+/**
+* The Mapper class that given a row number, will generate the appropriate 
+* output line.
+*/
+@SuppressWarnings({"deprecation"})
+
+public class TeraGenMapper extends MapReduceBase 
+  implements Mapper<LongWritable, NullWritable, Text, Text> {
+
+private Text key = new Text();
+private Text value = new Text();
+private TeraGenRandomGenerator rand;
+private byte[] keyBytes = new byte[12];
+private byte[] spaces = "          ".getBytes();
+private byte[][] filler = new byte[26][];
+{
+  for(int i=0; i < 26; ++i) {
+    filler[i] = new byte[10];
+    for(int j=0; j<10; ++j) {
+      filler[i][j] = (byte) ('A' + i);
+    }
+  }
+}
+
+/**
+ * Add a random key to the text
+ * @param rowId
+ */
+private void addKey() {
+  for(int i=0; i<3; i++) {
+    long temp = rand.next() / 52;
+    keyBytes[3 + 4*i] = (byte) (' ' + (temp % 95));
+    temp /= 95;
+    keyBytes[2 + 4*i] = (byte) (' ' + (temp % 95));
+    temp /= 95;
+    keyBytes[1 + 4*i] = (byte) (' ' + (temp % 95));
+    temp /= 95;
+    keyBytes[4*i] = (byte) (' ' + (temp % 95));
+  }
+  key.set(keyBytes, 0, 10);
+}
+
+/**
+ * Add the rowid to the row.
+ * @param rowId
+ */
+private void addRowId(long rowId) {
+  byte[] rowid = Integer.toString((int) rowId).getBytes();
+  int padSpace = 10 - rowid.length;
+  if (padSpace > 0) {
+    value.append(spaces, 0, 10 - rowid.length);
+  }
+  value.append(rowid, 0, Math.min(rowid.length, 10));
+}
+
+/**
+ * Add the required filler bytes. Each row consists of 7 blocks of
+ * 10 characters and 1 block of 8 characters.
+ * @param rowId the current row number
+ */
+private void addFiller(long rowId) {
+  int base = (int) ((rowId * 8) % 26);
+  for(int i=0; i<7; ++i) {
+    value.append(filler[(base+i) % 26], 0, 10);
+  }
+  value.append(filler[(base+7) % 26], 0, 8);
+}
+
+public void map(LongWritable row, NullWritable ignored,
+                OutputCollector<Text, Text> output,
+                Reporter reporter) throws IOException {
+  long rowId = row.get();
+  if (rand == null) {
+    // we use 3 random numbers per a row
+    rand = new TeraGenRandomGenerator(rowId*3);
+  }
+  addKey();
+  value.clear();
+  addRowId(rowId);
+  addFiller(rowId);
+  output.collect(key, value);
+}
+
+}
