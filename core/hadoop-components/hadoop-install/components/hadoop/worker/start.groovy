49cebc33c2a3c653957626425edf0dd474433be2
Steve Loughran <162090+steveloughran@users.noreply.github.com>
2011-12-06 16:26:11 +0000

    SFOS-1608 	create hadoop-installer component to untar and set up hadoop worker or master nodes

--- core/hadoop-components/hadoop-install/components/hadoop/worker/start.groovy
+++ core/hadoop-components/hadoop-install/components/hadoop/worker/start.groovy
@@ -7,8 +7,7 @@ def cmd = "bash $binDir/worker.sh $binDir "
 exec("$cmd stop tasktracker", binDir)
 exec("$cmd stop datanode", binDir)
 
-failIf(
-        exec("$cmd start datanode", binDir)
+failIf(exec("$cmd start datanode", binDir)
         , "could not start datanode")
 failIf(exec("$cmd start tasktracker", binDir), "could not start tasktracker")
 log("Started Hadoop Worker in ${binDir}")
